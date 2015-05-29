a9522298411831a9e251670a9e75500b17c0e08d
DMTfarc <1037414277@qq.com>
2015-05-30 07:58:47 +0800

    重新翻译

--- BukkitApi/org/bukkit/scheduler/BukkitTask.java
+++ BukkitApi/org/bukkit/scheduler/BukkitTask.java
@@ -3,33 +3,37 @@ package org.bukkit.scheduler;
 import org.bukkit.plugin.Plugin;
 
 /**
- * 代表一个被调度器执行的任务.
+ * 代表一个将被调度系统执行的任务/Represents a task being executed by the scheduler
  */
 public interface BukkitTask {
 
     /**
-     * 返回该任务id.
+     * 返回任务的taskID.
+	 * 原文:Returns the taskId for the task.
      *
-     * @return 任务id
+     * @return 任务的taskID
      */
     public int getTaskId();
 
     /**
-     * 返回拥有该任务的插件.
+     * 返回拥有这个任务的插件
+	 * 原文:Returns the Plugin that owns this task.
      *
-     * @return 拥有该任务的插件
+     * @return 返回拥有这个任务的插件
      */
     public Plugin getOwner();
 
     /**
-     * 返回true如果该任务是同步执行.
+     * 任务为同步执行，返回true
+	 * 原文:Returns true if the Task is a sync task.
      *
-     * @return true 如果有主线程执行
+     * @return true 任务在主线程被执行，返回true/if the task is run by main thread
      */
     public boolean isSync();
 
     /**
-     * 尝试关闭该任务.
+     * 此方法将尝试关闭这个任务.
+	 * 原文:This method will attempt to cancel this task.
      */
     public void cancel();
 }
