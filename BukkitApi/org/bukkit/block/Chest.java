f0776c572470dfc0ead4b7c7156a5d3e70ca8b0b
hcrgm <hcrgm@qq.com>
2015-06-08 15:04:34 +0800

    翻译 Chest.java

--- BukkitApi/org/bukkit/block/Chest.java
+++ BukkitApi/org/bukkit/block/Chest.java
@@ -3,15 +3,19 @@ package org.bukkit.block;
 import org.bukkit.inventory.Inventory;
 
 /**
- * Represents a chest.
+ * 代表一个箱子.
  */
 public interface Chest extends BlockState, ContainerBlock {
 
     /**
+     * 返回箱子的储存空间。如果这是大型箱子，也将返回此储存空间.
+     * 就在这个箱子的储存空间的一部分.
+     * 原文:
      * Returns the chest's inventory. If this is a double chest, it returns
      * just the portion of the inventory linked to this half of the chest.
      *
-     * @return The inventory.
+     * @return 箱子界面.
      */
     Inventory getBlockInventory();
 }
+
