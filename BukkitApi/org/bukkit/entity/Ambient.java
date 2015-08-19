e988e3507a55710a3a7f0c0ee749a45266a8e86f
hcrgm <hcrgm@qq.com>
2015-08-19 19:49:58 +0800

    2

--- BukkitApi/org/bukkit/entity/Ambient.java
+++ BukkitApi/org/bukkit/entity/Ambient.java
@@ -1,6 +1,11 @@
 package org.bukkit.entity;
 
 /**
+ * 代表环境生物.
+ * <p>
+ * 译注:玩这么久mc还不知道ambient是什么鬼.
+ * <p>
+ * 原文:
  * Represents an ambient mob
  */
 public interface Ambient extends LivingEntity {}
--- BukkitApi/org/bukkit/entity/AnimalTamer.java
+++ BukkitApi/org/bukkit/entity/AnimalTamer.java
@@ -5,16 +5,22 @@ import java.util.UUID;
 public interface AnimalTamer {
 
     /**
+     * 这是指定的AnimalTamer的名称.
+     * <p>
+     * 原文:
      * This is the name of the specified AnimalTamer.
      *
-     * @return The name to reference on tamed animals or null if a name cannot be obtained
+     * @return 如果名称不能被引用，改名称可以引用被驯养的动物或为
      */
     public String getName();
 
     /**
+     * 这是指定的AnimalTamer的UUID.
+     * <p>
+     * 原文:
      * This is the UUID of the specified AnimalTamer.
      *
-     * @return The UUID to reference on tamed animals
+     * @return AnimalTamer引用的UUID
      */
     public UUID getUniqueId();
 }
