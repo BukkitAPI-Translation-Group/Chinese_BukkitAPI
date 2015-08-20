c527eb1736a9303ea268cb01be0bb37353524c1e
Py2001 <Py2001@DESKTOP-A5FJBFQ.lan>
2015-08-20 22:28:09 +0800

    润笔,顺便试试netbeans自带的git的提交功能..

--- BukkitApi/org/bukkit/event/Cancellable.java
+++ BukkitApi/org/bukkit/event/Cancellable.java
@@ -3,7 +3,7 @@ package org.bukkit.event;
 public interface Cancellable {
 
     /**
-     * 获取一个事件取消的状态. 一个被取消的事件不能在
+     * 获取这个事件是否被取消. 一个被取消的事件不会在
      * 服务器里被执行，但是仍然会传递事件到其他插件
      *
      * @return true 如果事件已经被取消
@@ -11,7 +11,7 @@ public interface Cancellable {
     public boolean isCancelled();
 
     /**
-     * 设置一个事件取消的状态. 一个被取消的事件不能在
+     * 取消这个事件. 一个被取消的事件不会在
      * 服务器里被执行，但是仍然会传递事件到其他插件
      *
      * @param cancel true 如果你想取消这个事件
