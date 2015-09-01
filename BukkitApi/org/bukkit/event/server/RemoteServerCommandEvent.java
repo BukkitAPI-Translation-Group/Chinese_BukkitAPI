f32e8d60434363fc943658b8675f0c01b1b7a1a2
zhouhaha <mjx1627025175@126.com>
2015-09-01 13:19:22 +0800

    修正&校验完成

--- BukkitApi/org/bukkit/event/server/RemoteServerCommandEvent.java
+++ BukkitApi/org/bukkit/event/server/RemoteServerCommandEvent.java
@@ -4,11 +4,8 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.event.HandlerList;
 
 /**
- * 这个事件当RCON收到指令时调用.
- * {@link ServerCommandEvent查看更多信息.
- * <p>
- * 原文:This event is called when a command is recieved over RCON. See the javadocs
- * of {@link ServerCommandEvent} for more information.
+ * 这个事件当服务器RCON收到指令时调用. <p>
+ * {@link ServerCommandEvent} 查看更多信息.
  */
 public class RemoteServerCommandEvent extends ServerCommandEvent {
     private static final HandlerList handlers = new HandlerList();
