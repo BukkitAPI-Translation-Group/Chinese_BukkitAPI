ba92fb40ffa3b598532e01f2b5f5dc7d95be37da
zhouhaha <mjx1627025175@126.com>
2015-09-01 18:25:19 +0800

    修正

--- BukkitApi/org/bukkit/event/block/BlockPistonRetractEvent.java
+++ BukkitApi/org/bukkit/event/block/BlockPistonRetractEvent.java
@@ -7,7 +7,7 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.event.HandlerList;
 
 /**
- * 活塞缩回事件
+ * 活塞臂缩回事件
  */
 public class BlockPistonRetractEvent extends BlockPistonEvent {
     private static final HandlerList handlers = new HandlerList();
@@ -20,7 +20,7 @@ public class BlockPistonRetractEvent extends BlockPistonEvent {
     }
 
     /**
-     * 如果缩回的活塞是粘性的，获取这个活塞粘住的方块的位置
+     * 如果缩回的活塞是粘性的,获取被这个活塞粘住的方块的位置. 
      *
      * @return 这个活塞粘住的方块的位置
      */
