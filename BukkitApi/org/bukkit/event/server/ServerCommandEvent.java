70e8e3cfde71d0cdc4b983e9d599aaf291f3d50a
zhouhaha <mjx1627025175@126.com>
2015-09-01 13:21:13 +0800

    修正&校验完成

--- BukkitApi/org/bukkit/event/server/ServerCommandEvent.java
+++ BukkitApi/org/bukkit/event/server/ServerCommandEvent.java
@@ -5,10 +5,9 @@ import org.bukkit.event.HandlerList;
 
 /**
  * 这个事件当服务器后台发送指令时调用.
- * 这是命令处理过程早期方法.
- * 修改在这个事件({@link #setCommand(String)})所示的方法.
+ * 这是命令开始处理过程之前被触发的.
  * <p>
- * 很多插件<b>都不使用此事件</b>.如果这个事件不是必要的,你应该尽量避免适用它！
+ * 很多插件<b>都不使用此事件</b>.如果这个事件不是必要的,你应该尽量避免使用它!
  * <p>
  * 正确使用该事件的事例如下:
  * <ul>
@@ -31,39 +30,6 @@ import org.bukkit.event.HandlerList;
  * </ul>
  * <p>
  * 如果事件被取消,处理命令将停止.
- * <p>
- * 原文:This event is called when a command is run from the server console. It is
- * called early in the command handling process, and modifications in this
- * event (via {@link #setCommand(String)}) will be shown in the behavior.
- * <p>
- * Many plugins will have <b>no use for this event</b>, and you should
- * attempt to avoid using it if it is not necessary.
- * <p>
- * Some examples of valid uses for this event are:
- * <ul>
- * <li>Logging executed commands to a separate file
- * <li>Variable substitution. For example, replacing <code>${ip:Steve}</code>
- *     with the connection IP of the player named Steve, or simulating the
- *     <code>@a</code> and <code>@p</code> decorators used by Command Blocks
- *     for plugins that do not handle it.
- * <li>Conditionally blocking commands belonging to other plugins.
- * <li>Per-sender command aliases. For example, after the console runs the
- *     command <code>/calias cr gamemode creative</code>, the next time they
- *     run <code>/cr</code>, it gets replaced into
- *     <code>/gamemode creative</code>. (Global command aliases should be
- *     done by registering the alias.)
- * </ul>
- * <p>
- * Examples of incorrect uses are:
- * <ul>
- * <li>Using this event to run command logic
- * </ul>
- * <p>
- * If the event is cancelled, processing of the command will halt.
- * <p>
- * The state of whether or not there is a slash (<code>/</code>) at the
- * beginning of the message should be preserved. If a slash is added or
- * removed, unexpected behavior may result.
  */
 public class ServerCommandEvent extends ServerEvent {
     private static final HandlerList handlers = new HandlerList();
@@ -76,7 +42,7 @@ public class ServerCommandEvent extends ServerEvent {
     }
 
     /**
-     * 得到从控制台执行的命令.
+     * 得到从控制台执行的命令(触发这个事件的命令).
      * <p>
      * 原文:Gets the command that the user is attempting to execute from the
      * console
@@ -88,18 +54,18 @@ public class ServerCommandEvent extends ServerEvent {
     }
 
     /**
-     * 设置将执行的命令.
+     * 设置将要执行的命令.
      * <p>
      * 原文:Sets the command that the server will execute
      *
-     * @param message 控制台执行的命令
+     * @param message 控制台将会执行的命令
      */
     public void setCommand(String message) {
         this.command = message;
     }
 
     /**
-     * 得到命令发送者.
+     * 得到命令发送者(后台).
      * <p>
      * 原文:Get the command sender.
      *
