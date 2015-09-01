c5bc600afe7e09756bc3973d7ccfc1468d73ee47
zhouhaha <mjx1627025175@126.com>
2015-09-01 18:37:43 +0800

    修正

--- BukkitApi/org/bukkit/event/block/LeavesDecayEvent.java
+++ BukkitApi/org/bukkit/event/block/LeavesDecayEvent.java
@@ -5,9 +5,9 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * 当树叶腐烂时调用本事件
+ * 当树叶消失时触发此事件
  * <p>
- * 如果树叶腐烂事件被取消，树叶就不会腐烂哦。
+ * 如果此事件被取消则树叶就不会小时.
  */
 public class LeavesDecayEvent extends BlockEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
