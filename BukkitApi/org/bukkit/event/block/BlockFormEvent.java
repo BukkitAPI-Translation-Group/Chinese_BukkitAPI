33ba1d09673f795188ba705c02c93aed377d1ec3
zhouhaha <mjx1627025175@126.com>
2015-09-01 18:00:12 +0800

    修正

--- BukkitApi/org/bukkit/event/block/BlockFormEvent.java
+++ BukkitApi/org/bukkit/event/block/BlockFormEvent.java
@@ -6,18 +6,19 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * 当一个方块基于世界的条件被构成或者蔓延时，调用此事件.
+ * 当一个方块因为自然变化被放置、更改或者蔓延时(比如下雪),触发此事件.
  * <p>
  * 使用 {@link BlockSpreadEvent} 来捕获方块，事实上在方块蔓延的时候，不只是
  * "随意地" 构成。
  * <p>
  * 例子:
  * <ul>
- * <li>雪在下雪的时候形成
- * <li>冰在有雪的生物群系中构成
+ * <li>雪在下雪的时候形成.
+ * <li>冰在有雪的生物群系中构成.
  * </ul>
  * <p>
- * 如果本事件被取消，方块将不会构成
+ * 如果本事件被取消，方块将不会蔓延等.
+ * 
  * @see BlockSpreadEvent
  */
 public class BlockFormEvent extends BlockGrowEvent implements Cancellable {
