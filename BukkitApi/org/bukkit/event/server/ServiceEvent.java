bf7c58cbf86e880086e4341f4dbae79dab39e7e9
zhouhaha <mjx1627025175@126.com>
2015-09-01 13:21:59 +0800

    修正&校验完成

--- BukkitApi/org/bukkit/event/server/ServiceEvent.java
+++ BukkitApi/org/bukkit/event/server/ServiceEvent.java
@@ -3,10 +3,7 @@ package org.bukkit.event.server;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 /**
- * 事件相关的注册服务器.这就相当于一个{@link org.bukkit.plugin.ServicesManager}.
- * <p>
- * 原文:An event relating to a registered service. This is called in a {@link
- * org.bukkit.plugin.ServicesManager}
+ * 事件关联的注册服务器.这就相当于一个{@link org.bukkit.plugin.ServicesManager}.
  */
 public abstract class ServiceEvent extends ServerEvent {
     private final RegisteredServiceProvider<?> provider;
