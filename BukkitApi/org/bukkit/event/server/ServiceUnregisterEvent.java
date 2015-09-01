50048e7fb6cae752c6bac8f803881f7e2c3f7740
zhouhaha <mjx1627025175@126.com>
2015-09-01 13:22:30 +0800

    修正&校验完成

--- BukkitApi/org/bukkit/event/server/ServiceUnregisterEvent.java
+++ BukkitApi/org/bukkit/event/server/ServiceUnregisterEvent.java
@@ -7,11 +7,6 @@ import org.bukkit.plugin.RegisteredServiceProvider;
  * 当一个服务注销时调用.
  * <p>
  * 注意:注册和注销的事件顺序不互相依赖.
- * <p>
- * 原文:This event is called when a service is unregistered.
- * <p>
- * Warning: The order in which register and unregister events are called
- * should not be relied upon.
  */
 public class ServiceUnregisterEvent extends ServiceEvent {
     private static final HandlerList handlers = new HandlerList();
