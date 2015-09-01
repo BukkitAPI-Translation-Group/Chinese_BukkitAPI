bc6ff8ccbc43ce893231b8b249ec2a34c703a874
zhouhaha <mjx1627025175@126.com>
2015-09-01 13:22:15 +0800

    修正&校验完成

--- BukkitApi/org/bukkit/event/server/ServiceRegisterEvent.java
+++ BukkitApi/org/bukkit/event/server/ServiceRegisterEvent.java
@@ -4,14 +4,9 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.RegisteredServiceProvider;
 
 /**
- * 当一个服务注册时调用.
+ * 当一个服务注册时调用. 
  * <p>
  * 注意:注册和注销的事件顺序不互相依赖.
- * <p>
- * 原文:This event is called when a service is registered.
- * <p>
- * Warning: The order in which register and unregister events are called
- * should not be relied upon.
  */
 public class ServiceRegisterEvent extends ServiceEvent {
     private static final HandlerList handlers = new HandlerList();
