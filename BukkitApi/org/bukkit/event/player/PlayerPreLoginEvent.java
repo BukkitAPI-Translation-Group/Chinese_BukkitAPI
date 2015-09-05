6006512113e7c13de69052e6bcc551173859f361
Bai_Shi <1546625638@qq.com>
2015-09-05 19:49:46 +0800

    update PlayerPreLoginEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerPreLoginEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerPreLoginEvent.java
@@ -8,11 +8,10 @@ import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 /**
- * Stores details for players attempting to log in
+ * 玩家尝试登录服务器事件.
  *
- * @deprecated This event causes synchronization from the login thread; {@link
- *     AsyncPlayerPreLoginEvent} is preferred to keep the secondary threads
- *     asynchronous.
+ * @deprecated 这个事件使登录线程同步;
+ * {@link AsyncPlayerPreLoginEvent}是首选的辅助线程.
  */
 @Deprecated
 @Warning(reason="This event causes a login thread to synchronize with the main thread")
@@ -38,44 +37,48 @@ public class PlayerPreLoginEvent extends Event {
     }
 
     /**
-     * Gets the current result of the login, as an enum
-     *
-     * @return Current Result of the login
+     * 获取当前登录的结果,作为枚举常量. <p>
+     * 原文:Gets the current result of the login, as an enum
+     * @return 当前登录的结果
      */
     public Result getResult() {
         return result;
     }
 
     /**
-     * Sets the new result of the login, as an enum
+     * 设置新的登录的结果,作为枚举常量. <p>
+     * 原文:Sets the new result of the login, as an enum
      *
-     * @param result New result to set
+     * @param 设置新的结果
      */
     public void setResult(final Result result) {
         this.result = result;
     }
 
     /**
-     * Gets the current kick message that will be used if getResult() !=
+     * 如果getResult() !=Result.ALLOWED，那么获取将要踢出的信息. <p>
+     * 原文:Gets the current kick message that will be used if getResult() !=
      * Result.ALLOWED
      *
-     * @return Current kick message
+     * @return 踢出的信息
      */
     public String getKickMessage() {
         return message;
     }
 
     /**
-     * Sets the kick message to display if getResult() != Result.ALLOWED
+     * 设置如果getResult() !=Result.ALLOWED，那么将要踢出的消息. <p>
+     * 原文:Sets the kick message to display if getResult() != Result.ALLOWED
      *
-     * @param message New kick message
+     * @param 设置踢出的消息
      */
     public void setKickMessage(final String message) {
         this.message = message;
     }
 
     /**
-     * Allows the player to log in
+     * 允许玩家登录. <p>
+     * 原文:Allows the player to log in
      */
     public void allow() {
         result = Result.ALLOWED;
@@ -83,10 +86,11 @@ public class PlayerPreLoginEvent extends Event {
     }
 
     /**
-     * Disallows the player from logging in, with the given reason
+     * 不允许玩家登录. <p>
+     * 原文:Disallows the player from logging in, with the given reason
      *
-     * @param result New result for disallowing the player
-     * @param message Kick message to display to the user
+     * @param result 结果
+     * @param message 理由
      */
     public void disallow(final Result result, final String message) {
         this.result = result;
@@ -94,18 +98,20 @@ public class PlayerPreLoginEvent extends Event {
     }
 
     /**
-     * Gets the player's name.
+     * 获得此玩家的名字. <p>
+     * 原文:Gets the player's name.
      *
-     * @return the player's name
+     * @return 玩家名字
      */
     public String getName() {
         return name;
     }
 
     /**
-     * Gets the player IP address.
+     * 获得此玩家的IP地址. <p>
+     * 原文:Gets the player IP address.
      *
-     * @return The IP address
+     * @return 玩家IP地址
      */
     public InetAddress getAddress() {
         return ipAddress;
@@ -117,9 +123,10 @@ public class PlayerPreLoginEvent extends Event {
     }
 
     /**
-     * Gets the player's unique ID.
+     * 获得此玩家的唯一标识. <p>
+     * 原文:Gets the player's unique ID.
      *
-     * @return The unique ID
+     * @return 唯一标识
      */
     public UUID getUniqueId() {
         return uniqueId;
@@ -130,29 +137,34 @@ public class PlayerPreLoginEvent extends Event {
     }
 
     /**
-     * Basic kick reasons for communicating to plugins
+     * 踢出玩家基本字段.
      */
     public enum Result {
 
         /**
-         * The player is allowed to log in
+         * 玩家需要重新登陆. <p>
+         * 原文:The player is allowed to log in
          */
         ALLOWED,
         /**
-         * The player is not allowed to log in, due to the server being full
+         * 由于服务器限制，玩家不允许登陆. <p>
+         * 原文:The player is not allowed to log in, due to the server being full
          */
         KICK_FULL,
         /**
-         * The player is not allowed to log in, due to them being banned
+         * 玩家被禁止登录到此服务器. <p>
+         * 原文:The player is not allowed to log in, due to them being banned
          */
         KICK_BANNED,
         /**
-         * The player is not allowed to log in, due to them not being on the
+         * 玩家不在服务器白名单之内. <p>
+         * 原文:The player is not allowed to log in, due to them not being on the
          * white list
          */
         KICK_WHITELIST,
         /**
-         * The player is not allowed to log in, for reasons undefined
+         * 未知原因导致玩家无法进入服务器. <p>
+         * 原文:The player is not allowed to log in, for reasons undefined
          */
         KICK_OTHER
     }
