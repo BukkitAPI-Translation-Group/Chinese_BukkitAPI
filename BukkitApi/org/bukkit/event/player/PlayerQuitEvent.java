89cdc9d6b0c379d035103edad5d8b08cd0355712
zhouhaha <mjx1627025175@126.com>
2015-09-05 20:25:22 +0800

    修正&校对

--- BukkitApi/org/bukkit/event/player/PlayerQuitEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerQuitEvent.java
@@ -16,20 +16,20 @@ public class PlayerQuitEvent extends PlayerEvent {
     }
 
     /**
-     * 获取某玩家离开游戏后，发送给全体玩家的留言. <p>
+     * 获取某玩家离开游戏后，发送给全体玩家的离开消息. <p>
      * 原文:Gets the quit message to send to all online players
      *
-     * @return string 玩家退出消息
+     * @return string 玩家离开消息
      */
     public String getQuitMessage() {
         return quitMessage;
     }
 
     /**
-     * 设置某玩家离开游戏后，发送给全体玩家的留言. <p>
+     * 设置某玩家离开游戏后，发送给全体玩家的离开消息. <p>
      * 原文:Sets the quit message to send to all online players
      *
-     * @param quitMessage 玩家退出消息
+     * @param quitMessage 玩家离开消息
      */
     public void setQuitMessage(String quitMessage) {
         this.quitMessage = quitMessage;
