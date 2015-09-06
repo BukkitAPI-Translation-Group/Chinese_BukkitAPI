a5f275b740dd3c8da44eff023d517245bf2c22a0
hcrgm <hcrgm@qq.com>
2015-09-06 19:20:21 +0800

    LightningStrike

--- BukkitApi/org/bukkit/entity/LightningStrike.java
+++ BukkitApi/org/bukkit/entity/LightningStrike.java
@@ -1,14 +1,16 @@
 package org.bukkit.entity;
 
 /**
- * Represents an instance of a lightning strike. May or may not do damage.
+ * 代表雷击的实例，可能会或者不会造成伤害。
  */
 public interface LightningStrike extends Weather {
 
     /**
-     * Returns whether the strike is an effect that does no damage.
+     * 返回雷击是否是没有伤害的效果。
+     * <p>
+     * 原文：Returns whether the strike is an effect that does no damage.
      *
-     * @return whether the strike is an effect
+     * @return 雷击是否为效果
      */
     public boolean isEffect();
 
