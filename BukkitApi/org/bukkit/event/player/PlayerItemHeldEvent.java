e46346f9a3a6b44e9b53b1189bdf62bf49c1f82c
Bai_Shi <1546625638@qq.com>
2015-09-11 21:38:18 +0800

    update PlayerItemHeldEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerItemHeldEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerItemHeldEvent.java
@@ -5,7 +5,7 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * Fired when a player changes their currently held item
+ * 玩家手持某物品事件.
  */
 public class PlayerItemHeldEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -20,18 +20,20 @@ public class PlayerItemHeldEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
-     * Gets the previous held slot index
+     * 获得玩家上一次手持物品的物品槽位置. <p>
+     * 原文:Gets the previous held slot index
      *
-     * @return Previous slot index
+     * @return 上次手持物品的物品槽的位置
      */
     public int getPreviousSlot() {
         return previous;
     }
 
     /**
-     * Gets the new held slot index
+     * 获得玩家此次手持物品的物品槽位置. <p>
+     * 原文:Gets the new held slot index
      *
-     * @return New slot index
+     * @return 此次物品的物品槽位置
      */
     public int getNewSlot() {
         return current;
