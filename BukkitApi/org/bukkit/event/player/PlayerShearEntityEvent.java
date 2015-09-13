b18bd5067ad6757035bd8753a5831901be71316c
Bai_Shi <1546625638@qq.com>
2015-09-13 15:11:36 +0800

    update PlayerShearEntityEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerShearEntityEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerShearEntityEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
- * Called when a player shears an entity
+ * 玩家剪羊毛则调用此事件.
  */
 public class PlayerShearEntityEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -28,9 +28,11 @@ public class PlayerShearEntityEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
-     * Gets the entity the player is shearing
+     * 获得正在被玩家剪羊毛的实体对象.
+     * <p>
+     * 原文:Gets the entity the player is shearing
      *
-     * @return the entity the player is shearing
+     * @return 被剪实体
      */
     public Entity getEntity() {
         return what;
