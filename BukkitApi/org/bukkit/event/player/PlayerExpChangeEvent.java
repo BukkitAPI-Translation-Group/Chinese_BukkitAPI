4a6b0783214252ff0dd0ad8270201883fbc7a419
Bai_Shi <1546625638@qq.com>
2015-09-13 15:42:11 +0800

    update PlayerExpChangeEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerExpChangeEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerExpChangeEvent.java
@@ -4,7 +4,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
 /**
- * Called when a players experience changes naturally
+ * 当玩家经验值发生变化时调用此事件.
  */
 public class PlayerExpChangeEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
@@ -16,18 +16,22 @@ public class PlayerExpChangeEvent extends PlayerEvent {
     }
 
     /**
-     * Get the amount of experience the player will receive
+     * 获得玩家将要获得的经验.
+     * <p>
+     * 原文:Get the amount of experience the player will receive
      *
-     * @return The amount of experience
+     * @return 将要获得的经验
      */
     public int getAmount() {
         return exp;
     }
 
     /**
-     * Set the amount of experience the player will receive
+     * 设置玩家将要获得的经验.
+     * <p>
+     * 原文:Set the amount of experience the player will receive
      *
-     * @param amount The amount of experience to set
+     * @param amount 设置的经验
      */
     public void setAmount(int amount) {
         exp = amount;
