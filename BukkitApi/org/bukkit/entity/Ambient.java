6df86f3b4b0e9cb6135b4c58c9e6c48db25d7ef7
Bai_Shi <1546625638@qq.com>
2015-09-19 18:58:55 +0800

    修正

--- BukkitApi/org/bukkit/entity/Ambient.java
+++ BukkitApi/org/bukkit/entity/Ambient.java
@@ -3,7 +3,8 @@ package org.bukkit.entity;
 /**
  * 代表环境生物.
  * <p>
- * 原文:
- * Represents an ambient mob
+ * 代表周围的环境生物.
+ * <p>
+ * 原文:Represents an ambient mob
  */
 public interface Ambient extends LivingEntity {}
