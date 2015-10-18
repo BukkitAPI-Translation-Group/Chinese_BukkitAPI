d2612bbd667c2809c081306eadd17caf842d1394
hcrgm <hcrgm@qq.com>
2015-10-18 20:54:34 +0800

    改改

--- BukkitApi/org/bukkit/Rotation.java
+++ BukkitApi/org/bukkit/Rotation.java
@@ -2,8 +2,6 @@ package org.bukkit;
 
 /**
  * 指定旋转方向的一个枚举。
- * <p>
- * 它代表了一些物品是如何展示的，而不是基本方向。
  */
 public enum Rotation {
 
@@ -50,7 +48,7 @@ public enum Rotation {
      * <p>
      * 原文：Rotate clockwise by 90 degrees.
      *
-     * @return 相对旋转
+     * @return 旋转
      */
     public Rotation rotateClockwise() {
         return rotations[(this.ordinal() + 1) & 0x7];
@@ -59,9 +57,9 @@ public enum Rotation {
     /**
      * 按逆时针方向旋转90度。
      * <p>
-     * Rotate counter-clockwise by 90 degrees.
+     * 原文：Rotate counter-clockwise by 90 degrees.
      *
-     * @return 相对旋转
+     * @return 旋转
      */
     public Rotation rotateCounterClockwise() {
         return rotations[(this.ordinal() - 1) & 0x7];
