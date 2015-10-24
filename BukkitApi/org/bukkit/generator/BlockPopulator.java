ec67cc716b7e9c624992c92a575677fc801629cf
Igniz <529134615@qq.com>
2015-10-24 20:36:16 +0800

    update BlockPopulator.java

--- BukkitApi/org/bukkit/generator/BlockPopulator.java
+++ BukkitApi/org/bukkit/generator/BlockPopulator.java
@@ -5,6 +5,11 @@ import org.bukkit.Chunk;
 import org.bukkit.World;
 
 /**
+ * 方块填充器用于生成一定小区域的方块。
+ * <p>
+ * 例如，在地狱中或生成的填满宝藏的地牢中生成萤石。
+ * <p>
+ * 原文：
  * A block populator is responsible for generating a small area of blocks.
  * <p>
  * For example, generating glowstone inside the nether or generating dungeons
@@ -13,6 +18,14 @@ import org.bukkit.World;
 public abstract class BlockPopulator {
 
     /**
+     * 在指定的区块附近或内部填充一定区域的方块。
+     * <p>
+     * 在指定区块四周的区块必须已经存在；就是说，指定方块的东南西北都必须有一个区块存在，
+     * 角落区块可以不存在。当准备好时填充器会记录那些区块所有的修改请求并执行修改。
+     * <p>
+     * 译注：存在的意思可能是加载。
+     * <p>
+     * 原文：
      * Populates an area of blocks at or around the given chunk.
      * <p>
      * The chunks on each side of the specified chunk must already exist; that
@@ -21,9 +34,9 @@ public abstract class BlockPopulator {
      * populator should record any changes required for those chunks and
      * perform the changes when they are ready.
      *
-     * @param world The world to generate in
-     * @param random The random generator to use
-     * @param source The chunk to generate for
+     * @param world 用于生成的世界
+     * @param random 使用的随机生成器
+     * @param source 用于生成的区块
      */
     public abstract void populate(World world, Random random, Chunk source);
 }
