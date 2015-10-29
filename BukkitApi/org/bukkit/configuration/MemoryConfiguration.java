74c00f20e782fe0bf840794fe689b59fbe1a6917
Py2001 <Py2001@794126318@qq.com>
2015-10-30 01:43:47 +0800

    重复方法汉化完毕,剩余一些独特方法未汉化.

--- BukkitApi/org/bukkit/configuration/MemoryConfiguration.java
+++ BukkitApi/org/bukkit/configuration/MemoryConfiguration.java
@@ -40,6 +40,7 @@ public class MemoryConfiguration extends MemorySection implements Configuration 
         defaults.set(path, value);
     }
 
+    @Override
     public void addDefaults(Map<String, Object> defaults) {
         Validate.notNull(defaults, "Defaults may not be null");
 
@@ -48,18 +49,21 @@ public class MemoryConfiguration extends MemorySection implements Configuration 
         }
     }
 
+    @Override
     public void addDefaults(Configuration defaults) {
         Validate.notNull(defaults, "Defaults may not be null");
 
         addDefaults(defaults.getValues(true));
     }
 
+    @Override
     public void setDefaults(Configuration defaults) {
         Validate.notNull(defaults, "Defaults may not be null");
 
         this.defaults = defaults;
     }
 
+    @Override
     public Configuration getDefaults() {
         return defaults;
     }
@@ -69,6 +73,7 @@ public class MemoryConfiguration extends MemorySection implements Configuration 
         return null;
     }
 
+    @Override
     public MemoryConfigurationOptions options() {
         if (options == null) {
             options = new MemoryConfigurationOptions(this);
