53bf411e13edb3ff876b21e114700011f75f2868
zhouhaha <mjx1627025175@126.com>
2015-10-31 18:12:07 +0800

    修正&校验完毕

--- BukkitApi/org/bukkit/projectiles/ProjectileSource.java
+++ BukkitApi/org/bukkit/projectiles/ProjectileSource.java
@@ -4,31 +4,29 @@ import org.bukkit.entity.Projectile;
 import org.bukkit.util.Vector;
 
 /**
- * 代表一个有效的弹射源。
+ * 代表一个有效的抛射物发射源。
  */
 public interface ProjectileSource {
 
     /**
-     * 让ProjectileSource弹射一个{@link Projectile}。
+     * 让ProjectileSource发射一个{@link Projectile}。
      * <p>
      * 原文：Launches a {@link Projectile} from the ProjectileSource.
      *
-     * @param <T> Projectile子类
-     * @param projectile 要让Projectile弹射的类
-     * @return 已弹射的Projectile
+     * @param projectile 要让ProjectileSource发射的抛射物类型
+     * @return 发射的抛射物实例对象
      */
     public <T extends Projectile> T launchProjectile(Class<? extends T> projectile);
 
     /**
-     * 让ProjectileSource以一个初始速度弹射{@link Prijectile}。
+     * 让ProjectileSource以一个初始速度向量发射{@link Prijectile}。
      * <p>
      * 原文：Launches a {@link Projectile} from the ProjectileSource with an
      * initial velocity.
      *
-     * @param <T> Projectile子类
-     * @param projectile 要让Projectile弹射的类
-     * @param velocity 弹射的速度
-     * @return 已弹射的Projectile
+     * @param projectile 要让ProjectileSource发射的抛射物类型
+     * @param velocity 抛射物的速度向量
+     * @return 发射的抛射物实例对象
      */
     public <T extends Projectile> T launchProjectile(Class<? extends T> projectile, Vector velocity);
 }
