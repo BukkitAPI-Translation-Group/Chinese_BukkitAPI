ee909d902c9af3b538d0367af2602c7097b66145
zhouhaha <mjx1627025175@126.com>
2015-10-31 18:07:15 +0800

    修正&效验

--- BukkitApi/org/bukkit/projectiles/BlockProjectileSource.java
+++ BukkitApi/org/bukkit/projectiles/BlockProjectileSource.java
@@ -5,11 +5,11 @@ import org.bukkit.block.Block;
 public interface BlockProjectileSource extends ProjectileSource {
 
     /**
-     * 获取这个弹射源所属的方块。
+     * 获取这个抛射物所属的方块。
      * <p>
      * 原文：Gets the block this projectile source belongs to.
      *
-     * @return 弹射源方块
+     * @return 抛射物所属方块
      */
     public Block getBlock();
 }
