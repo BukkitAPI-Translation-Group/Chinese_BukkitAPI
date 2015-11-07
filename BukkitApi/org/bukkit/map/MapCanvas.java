e1f23bed60bf89c3d50ef4f5e67c3fcc6f2894b6
哎呦哥哥 <1019507194@qq.com>
2015-11-07 17:48:25 +0800

    我有钻石剑翻译

--- BukkitApi/org/bukkit/map/MapCanvas.java
+++ BukkitApi/org/bukkit/map/MapCanvas.java
@@ -3,81 +3,102 @@ package org.bukkit.map;
 import java.awt.Image;
 
 /**
- * Represents a canvas for drawing to a map. Each canvas is associated with a
+ * 代表一个画地图的画布，每个画布都关联着一个特定的
+ * 地图渲染器{@link MapRenderer}并表现为地图上渲染器的一层.
+ * <p>
+ * 原文:Represents a canvas for drawing to a map. Each canvas is associated with a
  * specific {@link MapRenderer} and represents that renderer's layer on the
  * map.
  */
 public interface MapCanvas {
 
     /**
-     * Get the map this canvas is attached to.
+     * 获取画布依附的地图.
+     * <p>
+     * 原文:Get the map this canvas is attached to.
      *
-     * @return The MapView this canvas is attached to.
+     * @return 画布依附的地图控件.
      */
     public MapView getMapView();
 
     /**
-     * Get the cursor collection associated with this canvas.
+     * 获取与该画布相关联的游标集合.
+     * <P>
+     * 原文:Get the cursor collection associated with this canvas.
      *
-     * @return The MapCursorCollection associated with this canvas.
+     * @return 与该画布相关联的游标集合
      */
     public MapCursorCollection getCursors();
 
     /**
-     * Set the cursor collection associated with this canvas. This does not
+     * 设置与该画布相关联的游标集合,自从地图游标集合(MapCursorCollection)提供后这个方法通常不需要被调用.
+     * <p>
+     * 原文:Set the cursor collection associated with this canvas. This does not
      * usually need to be called since a MapCursorCollection is already
      * provided.
      *
-     * @param cursors The MapCursorCollection to associate with this canvas.
+     * @param cursors 与画布相关联的游标集合.
      */
     public void setCursors(MapCursorCollection cursors);
 
     /**
-     * Draw a pixel to the canvas.
+     * 画一个像素到画布上.
+     * <P>
+     * 原文:Draw a pixel to the canvas.
      *
-     * @param x The x coordinate, from 0 to 127.
-     * @param y The y coordinate, from 0 to 127.
-     * @param color The color. See {@link MapPalette}.
+     * @param x x坐标值,从0到127.
+     * @param y y坐标值,从0到127.
+     * @param color 颜色,参考{@link MapPalette}.
      */
     public void setPixel(int x, int y, byte color);
 
     /**
-     * Get a pixel from the canvas.
+     * 获取画布上的一个像素.
+     * <p>
+     * 原文:Get a pixel from the canvas.
      *
-     * @param x The x coordinate, from 0 to 127.
-     * @param y The y coordinate, from 0 to 127.
-     * @return The color. See {@link MapPalette}.
+     * @param x x坐标值,从0到127.
+     * @param y y坐标值,从0到127.
+     * @param color 颜色,参考{@link MapPalette}.
      */
     public byte getPixel(int x, int y);
 
     /**
-     * Get a pixel from the layers below this canvas.
+     * 获取画布下层的一个像素.
+     * <p>
+     * 原文:Get a pixel from the layers below this canvas.
      *
-     * @param x The x coordinate, from 0 to 127.
-     * @param y The y coordinate, from 0 to 127.
-     * @return The color. See {@link MapPalette}.
+     * @param x x坐标值,从0到127.
+     * @param y y坐标值,从0到127.
+     * @param color 颜色,参考{@link MapPalette}.
      */
     public byte getBasePixel(int x, int y);
 
     /**
-     * Draw an image to the map. The image will be clipped if necessary.
+     * 画一张图片到地图上,如果必要的话,图像将被裁剪.
+     * <p>
+     * 原文:Draw an image to the map. The image will be clipped if necessary.
      *
-     * @param x The x coordinate of the image.
-     * @param y The y coordinate of the image.
-     * @param image The Image to draw.
+     * @param x 图片的x坐标.
+     * @param y 图片的x坐标.
+     * @param image 要画的图片.
      */
     public void drawImage(int x, int y, Image image);
 
     /**
-     * Render text to the map using fancy formatting. Newline (\n) characters
+     * 使用你想要的格式渲染文字到地图上.换行符(\n)会移动到下一行并返回原来的列,
+     * 字体颜色可以使用带字符的数字来改变如"§12;",这会把字体颜色换为颜色表中
+     * 第12个颜色(参考 {@link MapPalette}).
+     * <P>
+     * 原文:Render text to the map using fancy formatting. Newline (\n) characters
      * will move down one line and return to the original column, and the text
      * color can be changed using sequences such as "§12;", replacing 12 with
      * the palette index of the color (see {@link MapPalette}).
      *
-     * @param x The column to start rendering on.
-     * @param y The row to start rendering on.
-     * @param font The font to use.
-     * @param text The formatted text to render.
+     * @param x 开始渲染的列.
+     * @param y 开始渲染的行.
+     * @param font 使用的字体.
+     * @param text 要渲染的格式化文字.
      */
     public void drawText(int x, int y, MapFont font, String text);
 
