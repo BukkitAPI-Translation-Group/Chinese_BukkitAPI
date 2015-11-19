6e3c13ad48d348d6f0f8ceafe5900c19a4f1b7b3
Igniz <529134615@qq.com>
2015-11-19 17:25:55 +0800

    update Utility.java

--- BukkitApi/org/bukkit/Utility.java
+++ BukkitApi/org/bukkit/Utility.java
@@ -6,6 +6,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
+ * 这个注释表示一个方法（有时是构造函数）会连接它的内部操作。
+ * <p>
+ * 这仅仅用于识别不需要手动重写或处理的方法。
+ * <p>
+ * 原文：
  * This annotation indicates a method (and sometimes constructor) will chain
  * its internal operations.
  * <p>
