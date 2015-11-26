c4a75a9ddf69f0a3d66e27774477e48de968ca3a
hcrgm <hcrgm@qq.com>
2015-11-26 20:16:51 +0800

    那个....校对员记得如果翻译正确的话就把原文去了

--- javadoc/org/bukkit/event/hanging/package-info.java
+++ javadoc/org/bukkit/event/hanging/package-info.java
@@ -1,6 +1,7 @@
 /**
- * {@link org.bukkit.event.Event Events} relating to {@link
+ * 与{@link org.bukkit.entity.Hanging 悬挂}有关的{@link org.bukkit.event.Event 事件}.
+ * <p>
+ * 原文：{@link org.bukkit.event.Event Events} relating to {@link
  * org.bukkit.entity.Hanging entities that hang}.
  */
 package org.bukkit.event.hanging;
-
--- javadoc/org/bukkit/scheduler/package-info.java
+++ javadoc/org/bukkit/scheduler/package-info.java
@@ -1,6 +1,7 @@
 /**
- * Classes dedicated to letting {@link org.bukkit.plugin.Plugin plugins} run
+ * 让{@link org.bukkit.plugin.Plugin 插件}在特定时间间隔运行(包括线程安全)代码。
+ * <p>
+ * 原文：Classes dedicated to letting {@link org.bukkit.plugin.Plugin plugins} run
  * code at specific time intervals, including thread safety.
  */
 package org.bukkit.scheduler;
-
