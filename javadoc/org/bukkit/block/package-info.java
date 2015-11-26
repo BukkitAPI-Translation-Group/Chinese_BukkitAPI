9a305ec15f8164a4d7efd897d78bcdef05f4b4e7
hcrgm <hcrgm@qq.com>
2015-11-26 19:47:26 +0800

    英语太差

--- javadoc/org/bukkit/block/package-info.java
+++ javadoc/org/bukkit/block/package-info.java
@@ -1,5 +1,5 @@
 /**
- * 用来操作在{@link org.bukkit.World world}里的包含特别状态和三维像素的类.
+ * 用来操作在{@link org.bukkit.World world}里的包含特别状态的三维像素的类.
  * <p>
  * 原文：Classes used to manipulate the voxels in a {@link org.bukkit.World world},
  * including special states.
