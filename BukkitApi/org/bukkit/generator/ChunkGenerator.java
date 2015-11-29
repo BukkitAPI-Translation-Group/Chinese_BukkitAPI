3f4c4ca777e5887066d50767c04670ce1496fc2a
hcrgm <hcrgm@qq.com>
2015-11-29 17:14:02 +0800

    修正

--- BukkitApi/org/bukkit/generator/ChunkGenerator.java
+++ BukkitApi/org/bukkit/generator/ChunkGenerator.java
@@ -10,22 +10,17 @@ import org.bukkit.block.Biome;
 import org.bukkit.block.Block;
 
 /**
- * 区块生成器用于初始化一个整体的区块。比如说，地狱的区块生成器用于生成地狱岩和灵魂沙。
- * <p>
- * 原文：
- * A chunk generator is responsible for the initial shaping of an entire
- * chunk. For example, the nether chunk generator should shape netherrack and
- * soulsand
+ * 用于初始化一个整体的区块的区块生成器。比如说，地狱的区块生成器用于生成地狱岩和灵魂沙。
  */
 public abstract class ChunkGenerator {
 
     /**
      * 被生成区块的生物群系数据的接口：根据世界类型和种子的默认值初始化。
-	 * <p>
-	 * 自定义生成器使用以下接口可以自由地访问和调整数值：generateBlockSections() 或 generateExtBlockSections()
-	 * <p>
-	 * 原文：
-	 * Interface to biome data for chunk to be generated: initialized with
+     * <p>
+     * 自定义生成器使用以下接口可以自由地访问和调整数值：generateBlockSections() 或 generateExtBlockSections()
+     * <p>
+     * 原文：
+     * Interface to biome data for chunk to be generated: initialized with
      * default values for world type and seed.
      * <p>
      * Custom generator is free to access and tailor values during
@@ -34,9 +29,9 @@ public abstract class ChunkGenerator {
     public interface BiomeGrid {
 
         /**
-		 * 根据被生成区块的X，Z坐标获取生物群系。
-		 * <p>
-		 * 原文：
+         * 根据被生成区块的X，Z坐标获取生物群系。
+         * <p>
+         * 原文：
          * Get biome at x, z within chunk being generated
          *
          * @param x - 0-15
@@ -46,9 +41,9 @@ public abstract class ChunkGenerator {
         Biome getBiome(int x, int z);
 
         /**
-		 * 根据被生成区块的X，Z坐标设置生物群系。
-		 * <p>
-		 * 原文：
+         * 根据被生成区块的X，Z坐标设置生物群系。
+         * <p>
+         * 原文：
          * Set biome at x, z within chunk being generated
          *
          * @param x - 0-15
@@ -59,9 +54,9 @@ public abstract class ChunkGenerator {
     }
     @Deprecated
     /**
-	 * 生成指定坐标的区块。
+     * 生成指定坐标的区块。
      * <p>
-	 * 这个方法会按照下面的格式返回一个byte[32768]类型的数据。
+     * 这个方法会按照下面的格式返回一个byte[32768]类型的数据。
      * <pre>
      * for (int x = 0; x &lt; 16; x++) {
      *     for (int z = 0; z &lt; 16; z++) {
@@ -72,11 +67,11 @@ public abstract class ChunkGenerator {
      * }
      * </pre>
      * <p>
-	 * 注意这个方法<b>永远不要</b>试图去获取已经通过的坐标，不然就可能陷入死循环。
-	 * <p>
-	 * 注意这个过时的方法只有在 generateExtBlockSections() 和 generateBlockSections() 都失效并且返回null时才能被调用。
-	 * <p>
-   	 * 原文：
+     * 注意这个方法<b>永远不要</b>试图去获取已经通过的坐标，不然就可能陷入死循环。
+     * <p>
+     * 注意这个过时的方法只有在 generateExtBlockSections() 和 generateBlockSections() 都失效并且返回null时才能被调用。
+     * <p>
+     * 原文：
      * Shapes the chunk for the given coordinates.
      * <p>
      * This method should return a byte[32768] in the following format:
@@ -108,21 +103,21 @@ public abstract class ChunkGenerator {
     }
 
     /**
-	 * 使用拓展方块的ID(0-4095)生成指定坐标的区块。
-	 * <p>
-	 * 截至1.2，区块被表示为一个三维数组,每个区块都由16*16*16个方块组成。如果一部分是空的(都是ID为0的方块，即空气)，那么这个部分就不再需要被维持以减少内存占用
-	 * <p>
-	 * 这个方法会按照下面的格式返回一个short[][]类型的数据。
+     * 使用拓展方块的ID(0-4095)生成指定坐标的区块。
+     * <p>
+     * 截至1.2，区块被表示为一个三维数组,每个区块都由16*16*16个方块组成。如果一部分是空的(都是ID为0的方块，即空气)，那么这个部分就不再需要被维持以减少内存占用。
+     * <p>
+     * 这个方法会按照下面的格式返回一个short[][]类型的数据。
      * <pre>
      *     short[][] result = new short[world-height / 16][];
      * </pre>
-	 * 每个拥有方块的部分 {@code (sectionID = (Y>>4))} 需要为这部分中的4096个方块分配空间：
+     * 每个拥有方块的部分 {@code (sectionID = (Y>>4))} 需要为这部分中的4096个方块分配空间：
      * <pre>
      *     result[sectionID] = new short[4096];
      * </pre>
      * 没有内容的部分可以被保留为空。
-	 * <p>
-	 * 使用下面的映射函数可以在X,Y,Z坐标放置一个在区块内的方块：
+     * <p>
+     * 使用下面的映射函数可以在X,Y,Z坐标放置一个在区块内的方块：
      * <pre>
      *    void setBlock(short[][] result, int x, int y, int z, short blkid) {
      *        {@code if (result[y >> 4] == null) {}
@@ -131,7 +126,7 @@ public abstract class ChunkGenerator {
      *        {@code result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;}
      *    }
      * </pre>
-	 * 使用下面的映射函数可以读取一个方块的ID：
+     * 使用下面的映射函数可以读取一个方块的ID：
      * <pre>
      *    short getBlock(short[][] result, int x, int y, int z) {
      *        {@code if (result[y >> 4] == null) {}
@@ -140,12 +135,12 @@ public abstract class ChunkGenerator {
      *        {@code return result[y >> 4][((y & 0xF) << 8) | (z << 4) | x];}
      *    }
      * </pre>
-	 * <p>
-	 * 注意这个方法<b>永远不要</b>试图去获取已经通过的坐标，不然就可能陷入死循环。
-	 * <p>
-	 * 注意不能返回255以上方块ID的生成器不应该执行此方法，否则会返回空(generateBlockSections()方法被调用时的结果)。
-	 * <p>
-	 * 原文：
+     * <p>
+     * 注意这个方法<b>永远不要</b>试图去获取已经通过的坐标，不然就可能陷入死循环。
+     * <p>
+     * 注意不能返回255以上方块ID的生成器不应该执行此方法，否则会返回空(generateBlockSections()方法被调用时的结果)。
+     * <p>
+     * 原文：
      * Shapes the chunk for the given coordinates, with extended block IDs
      * supported (0-4095).
      * <p>
@@ -207,20 +202,20 @@ public abstract class ChunkGenerator {
     }
 
     /**
-	 * 在指定的坐标生成区块。
-	 * <p>
-	 * 截至1.2，区块被表示为一个三维数组,每个区块都由16*16*16个方块组成。如果一部分是空的(都是ID为0的方块，即空气)，那么这个部分就不再需要被维持以减少内存占用
-	 * <p>
-	 * 这个方法会按照下面的格式返回一个byte[][]类型的数据。
+     * 在指定的坐标生成区块。
+     * <p>
+     * 截至1.2，区块被表示为一个三维数组,每个区块都由16*16*16个方块组成。如果一部分是空的(都是ID为0的方块，即空气)，那么这个部分就不再需要被维持以减少内存占用。
+     * <p>
+     * 这个方法会按照下面的格式返回一个byte[][]类型的数据。
      * <pre>
      *     byte[][] result = new byte[world-height / 16][];
      * </pre>
-	 * 每个拥有方块的部分 {@code (sectionID = (Y>>4))} 需要为这部分中的4096个方块分配空间：
+     * 每个拥有方块的部分 {@code (sectionID = (Y>>4))} 需要为这部分中的4096个方块分配空间：
      * <pre>
      *     result[sectionID] = new byte[4096];
      * </pre>
      * 没有内容的部分可以被保留为空。
-	 * 使用下面的映射函数可以在X,Y,Z坐标放置一个在区块内的方块：
+     * 使用下面的映射函数可以在X,Y,Z坐标放置一个在区块内的方块：
      * <pre>
      *    void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
      *        {@code if (result[y >> 4) == null) {}
@@ -229,7 +224,7 @@ public abstract class ChunkGenerator {
      *        {@code result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;}
      *    }
      * </pre>
-	 * 使用下面的映射函数可以读取一个方块的ID：
+     * 使用下面的映射函数可以读取一个方块的ID：
      * <pre>
      *    byte getBlock(byte[][] result, int x, int y, int z) {
      *        {@code if (result[y >> 4) == null) {}
@@ -239,8 +234,8 @@ public abstract class ChunkGenerator {
      *    }
      * </pre>
      * 注意这个方法<b>永远不要</b>试图去获取已经通过的坐标，不然就可能陷入死循环。
-	 * <p>
-	 * 原文：
+     * <p>
+     * 原文：
      * Shapes the chunk for the given coordinates.
      * <p>
      * As of 1.2, chunks are represented by a vertical array of chunk
@@ -297,9 +292,9 @@ public abstract class ChunkGenerator {
     }
 
     /**
-	 * 测试指定方位是否对自然生成的方位有效。
-	 * <p>
-	 * 原文：
+	    * 测试指定方位是否对自然生成的方位有效。
+	    * <p>
+	    * 原文：
      * Tests if the specified location is valid for a natural spawn position
      *
      * @param world 用于测试的世界
@@ -322,9 +317,9 @@ public abstract class ChunkGenerator {
     }
 
     /**
-	 * 得到一个用于提供指定世界的默认的{@link BlockPopulator}列表
-	 * <p>
-	 * 原文：
+	    * 得到一个用于提供指定世界的默认的{@link BlockPopulator}列表。
+     * <p>
+     * 原文：
      * Gets a list of default {@link BlockPopulator}s to apply to a given
      * world
      *
@@ -336,11 +331,11 @@ public abstract class ChunkGenerator {
     }
 
     /**
-	 * 获取一个固定出生方位用于一个指定的世界。
-	 * <p>
-	 * 如果一个世界没有使用一个固定出生点就会返回空值，并且会试图随机寻找一个以代替。
-	 * <p>
-	 * 原文：
+	    * 获取一个固定出生方位用于一个指定的世界。
+	    * <p>
+	    * 如果一个世界没有使用一个固定出生点就会返回空值，并且会试图随机寻找一个以代替。
+	    * <p>
+	    * 原文：
      * Gets a fixed spawn location to use for a given world.
      * <p>
      * A null value is returned if a world should not use a fixed spawn point,
