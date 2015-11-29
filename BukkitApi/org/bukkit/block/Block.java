e1e0252464c4708691bc21fcf8a535d8bf7c498e
hcrgm <hcrgm@qq.com>
2015-11-29 15:48:39 +0800

    那么多..

--- BukkitApi/org/bukkit/block/Block.java
+++ BukkitApi/org/bukkit/block/Block.java
@@ -15,7 +15,8 @@ import org.bukkit.metadata.Metadatable;
 public interface Block extends Metadatable {
 
     /**
-     * 得到该方块的属性. <p>
+     * 得到该方块的属性.
+     * <p>
      * 原文: Gets the metadata for this block
      *
      * @return block 方块属性
@@ -25,7 +26,9 @@ public interface Block extends Metadatable {
     byte getData();
 
     /**
-     * 得到该方块的坐标偏移后的方块. <p>
+     * 得到该方块的坐标偏移后的方块.
+     * <p>
+     * 原文：Gets the block at the given offsets
      *
      * @param modX X坐标偏移
      * @param modY Y坐标偏移
@@ -35,12 +38,13 @@ public interface Block extends Metadatable {
     Block getRelative(int modX, int modY, int modZ);
 
     /**
-     * 得到这个方块某个方向最近的一个方块. <p>
+     * 得到这个方块某个方向最近的一个方块.
+     * <p>
      * 原文:Gets the block at the given face
      * <p>
      * This method is equal to getRelative(face, 1)
      *
-     * @param face 方向  {@link BlockFace}}
+     * @param face 方块的方向
      * @return Block 在该方块给定的方向的最近的一个方块
      * @see #getRelative(BlockFace, int)
      */
@@ -65,7 +69,8 @@ public interface Block extends Metadatable {
     Block getRelative(BlockFace face, int distance);
 
     /**
-     * 得到该方块的类型. <p>
+     * 得到该方块的类型.
+     * <p>
      * 原文:Gets the type of this block
      *
      * @return 一个Material类型.
@@ -73,7 +78,8 @@ public interface Block extends Metadatable {
     Material getType();
 
     /**
-     * 得到该方块的ID. <p>
+     * 得到该方块的ID.
+     * <p>
      * 原文: Gets the typeid of this block
      * 
      * @return 方块ID
@@ -83,7 +89,8 @@ public interface Block extends Metadatable {
     int getTypeId();
 
     /**
-     * 得到1~15之间的亮度. <p>
+     * 得到1~15之间的亮度.
+     * <p>
      * 原文: Gets the light level between 0-15
      *
      * @return 亮度
@@ -91,8 +98,10 @@ public interface Block extends Metadatable {
     byte getLightLevel();
 
     /**
-     * 获取从天空传来该方块上的亮度. <p>
-     * 仅仅是天空(太阳,月亮)的,火把萤石等的亮度都会被忽略. <p>
+     * 获取从天空传来该方块上的亮度.
+     * <p>
+     * 仅仅是天空(太阳,月亮)的,火把萤石等的亮度都会被忽略.
+     * <p>
      * 原文:Get the amount of light at this block from the sky.
      * <p>
      * Any light given from other sources (such as blocks like torches) will
@@ -103,8 +112,10 @@ public interface Block extends Metadatable {
     byte getLightFromSky();
 
     /**
-     * 获取从其他方块传来该方块的亮度. <p>
-     * 仅仅是方块(火把萤石等),天空中传来的将会被忽略. <p>
+     * 获取从其他方块传来该方块的亮度.
+     * <p>
+     * 仅仅是方块(火把萤石等),天空中传来的将会被忽略.
+     * <p>
      * 原文:Get the amount of light at this block from nearby blocks.
      * <p>
      * Any light given from other sources (such as the sun) will be ignored.
@@ -114,7 +125,8 @@ public interface Block extends Metadatable {
     byte getLightFromBlocks();
 
     /**
-     * 获取这个方块所在的世界. <p>
+     * 获取这个方块所在的世界.
+     * <p>
      * 原文:Gets the world which contains this Block
      *
      * @return 该方块所在的世界.
@@ -122,7 +134,8 @@ public interface Block extends Metadatable {
     World getWorld();
 
     /**
-     * 得到该方块的X坐标. <p>
+     * 得到该方块的X坐标.
+     * <p>
      * 原文:Gets the x-coordinate of this block
      *
      * @return X坐标
@@ -130,7 +143,8 @@ public interface Block extends Metadatable {
     int getX();
 
     /**
-     * 得到该方块的Y坐标. <p>
+     * 得到该方块的Y坐标.
+     * <p>
      * 原文:Gets the y-coordinate of this block
      *
      * @return Y坐标
@@ -138,7 +152,8 @@ public interface Block extends Metadatable {
     int getY();
 
     /**
-     * 得到该方块的Z坐标. <p>
+     * 得到该方块的Z坐标.
+     * <p>
      * 原文:Gets the z-coordinate of this block
      *
      * @return Z坐标
@@ -146,7 +161,8 @@ public interface Block extends Metadatable {
     int getZ();
 
     /**
-     * 得到该方块的位置实例. <p>
+     * 得到该方块的位置实例.
+     * <p>
      * 原文:Gets the Location of the block
      *
      * @return 该方块的位置实例.
@@ -165,7 +181,8 @@ public interface Block extends Metadatable {
     Location getLocation(Location loc);
 
     /**
-     * 得到该方块所在的区块. <p>
+     * 得到该方块所在的区块.
+     * <p>
      * 原文:Gets the chunk which contains this block
      *
      * @return 所在区块.
@@ -173,17 +190,19 @@ public interface Block extends Metadatable {
     Chunk getChunk();
 
     /**
-     * 设置该方块的属性. <p>
+     * 设置该方块的属性.
+     * <p>
      * 原文:Sets the metadata for this block
      *
-     * @param data 新的方块属性.
-     * @deprecated 不安全的参数.
+     * @param data 新的方块属性
+     * @deprecated 不安全的参数
      */
     @Deprecated
     void setData(byte data);
 
     /**
-     * 设置该方块的属性. <p>
+     * 设置该方块的属性.
+     * <p>
      * 原文: Sets the metadata for this block
      *
      * @param data 新的方块属性
@@ -194,7 +213,8 @@ public interface Block extends Metadatable {
     void setData(byte data, boolean applyPhysics);
 
     /**
-     * 设置方块类型. <p>
+     * 设置方块类型.
+     * <p>
      * 原文: Sets the type of this block
      *
      * @param type 方块类型.
@@ -202,7 +222,8 @@ public interface Block extends Metadatable {
     void setType(Material type);
 
     /**
-     * 设置方块类型. <p>
+     * 设置方块类型.
+     * <p>
      * 原文: Sets the type of this block
      *
      * @param type 方块类型.
@@ -211,37 +232,40 @@ public interface Block extends Metadatable {
     void setType(Material type, boolean applyPhysics);
 
     /**
-     * 设置方块的类型ID. <p>
+     * 设置方块的类型ID.
+     * <p>
      * 原文: Sets the type-id of this block
      *
-     * @param type 类型ID.
-     * @return 方块是否被改变.
-     * @deprecated 不安全的参数.
+     * @param type 类型ID
+     * @return 方块是否被改变
+     * @deprecated 不安全的参数
      */
     @Deprecated
     boolean setTypeId(int type);
 
     /**
-     * 设置方块的类型ID. <p>
+     * 设置方块的类型ID.
+     * <p>
      * 原文: Sets the type-id of this block
      *
-     * @param type 类型ID.
-     * @param applyPhysics 为false时取消物理效果.
-     * @return 方块是否被改变.
-     * @deprecated 不安全的参数.
+     * @param type 类型ID
+     * @param applyPhysics 为false时取消物理效果
+     * @return 方块是否被改变
+     * @deprecated 不安全的参数
      */
     @Deprecated
     boolean setTypeId(int type, boolean applyPhysics);
 
     /**
-     * 设置方块ID,属性. <p>
+     * 设置方块ID,属性.
+     * <p>
      * 原文: Sets the type-id and the data of this block
      *
-     * @param type 类型ID.
-     * @param data 方块属性.
-     * @param applyPhysics 为false时取消物理效果.
-     * @return 方块是否被改变.
-     * @deprecated 不安全的参数.
+     * @param type 类型ID
+     * @param data 方块属性
+     * @param applyPhysics 为false时取消物理效果
+     * @return 方块是否被改变
+     * @deprecated 不安全的参数
      */
     @Deprecated
     boolean setTypeIdAndData(int type, byte data, boolean applyPhysics);
@@ -265,9 +289,12 @@ public interface Block extends Metadatable {
     BlockFace getFace(Block block);
 
     /**
-     * 得到方块的状态. <p> 
-     * 可以用来得到一个木牌对象. <p>
-     * 如: Sign sign = (Sign)block.getState(); <p>
+     * 得到方块的状态.
+     * <p>
+     * 可以用来得到一个木牌对象.
+     * <p>
+     * 如: Sign sign = (Sign)block.getState();
+     * <p>
      * 原文: Captures the current state of this block. You may then cast that state
      * into any accepted type, such as Furnace or Sign.
      * <p>
@@ -279,18 +306,20 @@ public interface Block extends Metadatable {
     BlockState getState();
 
     /**
-     * 得到方块的生物群系. <p>.
+     * 得到方块的生物群系.
+     * <p>
      * 原文:Returns the biome that this block resides in
      *
-     * @return 生物群系.
+     * @return 生物群系
      */
     Biome getBiome();
 
     /**
      * 设置方块的生物群系.
+     * <p>
      * 原文:Sets the biome that this block resides in
      *
-     * @param bio 新生物群系.
+     * @param bio 新生物群系
      */
     void setBiome(Biome bio);
 
@@ -341,25 +370,27 @@ public interface Block extends Metadatable {
     int getBlockPower();
 
     /**
-     * 检查方块是否为空气. <p>
+     * 检查方块是否为空气.
+     * <p>
      * 原文:Checks if this block is empty.
      * <p>
      * A block is considered empty when {@link #getType()} returns {@link
      * Material#AIR}.
      *
-     * @return 为空气时返回true.
+     * @return 为空气时返回true
      */
     boolean isEmpty();
 
     /**
-     * 检查方块是否是液体. <p>
+     * 检查方块是否是液体.
+     * <p>
      * 原文:Checks if this block is liquid.
      * <p>
      * A block is considered liquid when {@link #getType()} returns {@link
      * Material#WATER}, {@link Material#STATIONARY_WATER}, {@link
      * Material#LAVA} or {@link Material#STATIONARY_LAVA}.
      *
-     * @return 为液体时返回true.
+     * @return 为液体时返回true
      */
     boolean isLiquid();
 
@@ -378,10 +409,11 @@ public interface Block extends Metadatable {
     double getHumidity();
 
     /**
-     * 返回方块在被活塞推动时的反应. <p>
+     * 返回方块在被活塞推动时的反应.
+     * <p>
      * 原文:Returns the reaction of the block when moved by a piston
      *
-     * @return 反应.
+     * @return 反应
      */
     PistonMoveReaction getPistonMoveReaction();
 
@@ -402,20 +434,22 @@ public interface Block extends Metadatable {
     boolean breakNaturally(ItemStack tool);
 
     /**
-     * 返回该方块破坏后会掉落的物品列表. <p>
+     * 返回该方块破坏后会掉落的物品列表.
+     * <p>
      * 原文: Returns a list of items which would drop by destroying this block
      *
-     * @return 方块会掉落的物品列表.
+     * @return 方块会掉落的物品列表
      */
     Collection<ItemStack> getDrops();
 
     /**
-     * 返回该方块使用的特定的工具破坏后会掉落的物品列表. <p>
+     * 返回该方块使用的特定的工具破坏后会掉落的物品列表.
+     * <p>
      * 原文:Returns a list of items which would drop by destroying this block with
      * a specific tool
      *
-     * @param tool 特定的工具.
-     * @return 会掉落的物品列表.
+     * @param tool 特定的工具
+     * @return 会掉落的物品列表
      */
     Collection<ItemStack> getDrops(ItemStack tool);
 
