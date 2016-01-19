87dd4b956c716d6c0cbf75a5b4e5ad60f1aaa342
dengyu <657399256@qq.com>
2016-01-19 21:24:56 +0800

    待校验

--- BukkitApi/org/bukkit/metadata/MetadataStore.java
+++ BukkitApi/org/bukkit/metadata/MetadataStore.java
@@ -6,55 +6,56 @@ import java.util.List;
 
 public interface MetadataStore<T> {
     /**
-     * Adds a metadata value to an object.
+     * 向对象添加元数据值。
+     * 原文：Adds a metadata value to an object.
      *
-     * @param subject The object receiving the metadata.
-     * @param metadataKey A unique key to identify this metadata.
-     * @param newMetadataValue The metadata value to apply.
-     * @throws IllegalArgumentException If value is null, or the owning plugin
-     *     is null
+     * @param subject 接收元数据的对象.
+     * @param metadataKey 确定这个元数据的唯一关键值。
+     * @param newMetadataValue 应用元数据。
+     * @throws IllegalArgumentException 如果值为空，或该插件是空的，抛出此异常。
      */
     public void setMetadata(T subject, String metadataKey, MetadataValue newMetadataValue);
 
     /**
-     * Returns all metadata values attached to an object. If multiple plugins
+     * 返回连接到对象的所有元数据值。如果多个插件附加元数据，每一个都将被包括在内。
+     * 原文：Returns all metadata values attached to an object. If multiple plugins
      * have attached metadata, each will value will be included.
      *
-     * @param subject the object being interrogated.
-     * @param metadataKey the unique metadata key being sought.
-     * @return A list of values, one for each plugin that has set the
-     *     requested value.
+     * @param subject 被访问的对象。
+     * @param metadataKey 正在访问这个元数据的唯一关键值。
+     * @return 一个为每个插件设置请求的值的列表。
      */
     public List<MetadataValue> getMetadata(T subject, String metadataKey);
 
     /**
-     * Tests to see if a metadata attribute has been set on an object.
+     * 测试是否已将元数据的属性设置为对象。
+     * 原文：Tests to see if a metadata attribute has been set on an object.
      *
-     * @param subject the object upon which the has-metadata test is
-     *     performed.
-     * @param metadataKey the unique metadata key being queried.
-     * @return the existence of the metadataKey within subject.
+     * @param subject 进行元数据测试的对象。
+     * @param metadataKey 正在访问这个元数据的唯一关键值。
+     * @return the existence of the metadataKey within subject.在主体内metadataKey是否存在。
      */
     public boolean hasMetadata(T subject, String metadataKey);
 
     /**
-     * Removes a metadata item owned by a plugin from a subject.
+     * 删除由一个主体所拥有的一个插件所拥有的元数据项。
+     * 原文：Removes a metadata item owned by a plugin from a subject.
      *
-     * @param subject the object to remove the metadata from.
-     * @param metadataKey the unique metadata key identifying the metadata to
-     *     remove.
-     * @param owningPlugin the plugin attempting to remove a metadata item.
-     * @throws IllegalArgumentException If plugin is null
+     * @param subject 删除元数据的对象。
+     * @param metadataKey 识别元数据的唯一关键值并删除。
+     * @param owningPlugin 从插件内试图删除元数据项。
+     * @throws IllegalArgumentException 如果插件不存在，抛出该异常。
      */
     public void removeMetadata(T subject, String metadataKey, Plugin owningPlugin);
 
     /**
-     * Invalidates all metadata in the metadata store that originates from the
+     * 无效的元数据会存储于特定插件的所有元数据。这样做会使得当下次访问它时，每一个无效的元数据将重新计算。
+     * 原文：Invalidates all metadata in the metadata store that originates from the
      * given plugin. Doing this will force each invalidated metadata item to
      * be recalculated the next time it is accessed.
      *
-     * @param owningPlugin the plugin requesting the invalidation.
-     * @throws IllegalArgumentException If plugin is null
+     * @param owningPlugin 访问无效的插件。
+     * @throws IllegalArgumentException 如果插件不存在，抛出该异常。
      */
     public void invalidateAll(Plugin owningPlugin);
 }
