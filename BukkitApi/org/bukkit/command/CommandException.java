58bea25ce9db1a1a1b5468312d8bd1a3a84dabe5
dengyu <657399256@qq.com>
2016-01-20 08:54:18 +0800

    待校验

--- BukkitApi/org/bukkit/command/CommandException.java
+++ BukkitApi/org/bukkit/command/CommandException.java
@@ -1,22 +1,29 @@
 package org.bukkit.command;
 
 /**
- * Thrown when an unhandled exception occurs during the execution of a Command
+ * 当一个命令的执行过程中发生未处理的异常时，抛出该异常。
+ * 原文：Thrown when an unhandled exception occurs during the execution of a Command
+ * <p>
+ * 译注：比如/setblock a b c 这样x,y,z值非数字，则会抛出异常。
  */
 @SuppressWarnings("serial")
 public class CommandException extends RuntimeException {
 
     /**
-     * Creates a new instance of <code>CommandException</code> without detail
+     * 新建一个<code>CommandException</code>的空实例。
+     * 原文：Creates a new instance of <code>CommandException</code> without detail
      * message.
+     * <p>
+     * 译注：without detail message.指“无详细信息”，instance指“实例”，在这翻译为“空实例”更为准确一些。
      */
     public CommandException() {}
 
     /**
-     * Constructs an instance of <code>CommandException</code> with the
+     * 创建一个新的实例<code>CommandException</code>，该实例中包含详细信息。
+     * 原文：Constructs an instance of <code>CommandException</code> with the
      * specified detail message.
      *
-     * @param msg the detail message.
+     * @param msg 详细信息。
      */
     public CommandException(String msg) {
         super(msg);
