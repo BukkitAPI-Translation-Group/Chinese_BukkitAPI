71eb4ec14333c7328f2a812d9b28f333236c4bd4
dengyu <657399256@qq.com>
2016-01-20 11:10:01 +0800

    待校验

--- BukkitApi/org/bukkit/command/TabExecutor.java
+++ BukkitApi/org/bukkit/command/TabExecutor.java
@@ -1,7 +1,8 @@
 package org.bukkit.command;
 
 /**
- * This class is provided as a convenience to implement both TabCompleter and
+ * 这个类提供了TabCompleter和CommandExecutor两种功能
+ * 原文：This class is provided as a convenience to implement both TabCompleter and
  * CommandExecutor.
  */
 public interface TabExecutor extends TabCompleter, CommandExecutor {
