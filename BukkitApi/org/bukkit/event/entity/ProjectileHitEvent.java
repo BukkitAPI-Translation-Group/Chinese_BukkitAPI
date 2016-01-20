01b018fd345a880e6983b62421c9a4c1e3506d0b
dengyu <657399256@qq.com>
2016-01-20 09:08:30 +0800

    待校验

--- BukkitApi/org/bukkit/event/entity/ProjectileHitEvent.java
+++ BukkitApi/org/bukkit/event/entity/ProjectileHitEvent.java
@@ -4,7 +4,8 @@ import org.bukkit.entity.Projectile;
 import org.bukkit.event.HandlerList;
 
 /**
- * Called when a projectile hits an object
+ * 当弹射物伤害实体时触发
+ * 原文：Called when a projectile hits an object
  */
 public class ProjectileHitEvent extends EntityEvent {
     private static final HandlerList handlers = new HandlerList();
