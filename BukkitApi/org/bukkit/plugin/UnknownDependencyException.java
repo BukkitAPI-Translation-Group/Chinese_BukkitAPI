e4111ce0043383824fe15722d382d84310261f21
dengyu <657399256@qq.com>
2016-01-20 13:52:34 +0800

    待校验...好累，暂时不翻了

--- BukkitApi/org/bukkit/plugin/UnknownDependencyException.java
+++ BukkitApi/org/bukkit/plugin/UnknownDependencyException.java
@@ -1,6 +1,7 @@
 package org.bukkit.plugin;
 
 /**
+ * 在试图加载无效的插件文件时将抛出异常
  * Thrown when attempting to load an invalid Plugin file
  */
 public class UnknownDependencyException extends RuntimeException {
@@ -8,36 +9,40 @@ public class UnknownDependencyException extends RuntimeException {
     private static final long serialVersionUID = 5721389371901775895L;
 
     /**
-     * Constructs a new UnknownDependencyException based on the given
+     * 新建一个新的异常类型UnknownDependencyException。
+     * 原文：Constructs a new UnknownDependencyException based on the given
      * Exception
      *
-     * @param throwable Exception that triggered this Exception
+     * @param throwable 导致抛出此异常的异常。
      */
     public UnknownDependencyException(final Throwable throwable) {
         super(throwable);
     }
 
     /**
-     * Constructs a new UnknownDependencyException with the given message
+     * 抛出一个新的UnknownDependencyException异常及它给定的消息。
+     * 原文：Constructs a new UnknownDependencyException with the given message
      *
-     * @param message Brief message explaining the cause of the exception
+     * @param message 发短消息来解释异常的原因。
      */
     public UnknownDependencyException(final String message) {
         super(message);
     }
 
     /**
-     * Constructs a new UnknownDependencyException based on the given
+     * 基于给定的异常，抛出一个新的UnknownDependencyException异常
+     * 原文：Constructs a new UnknownDependencyException based on the given
      * Exception
      *
-     * @param message Brief message explaining the cause of the exception
-     * @param throwable Exception that triggered this Exception
+     * @param message 发短消息来解释异常的原因
+     * @param throwable 导致抛出此异常的异常。
      */
     public UnknownDependencyException(final Throwable throwable, final String message) {
         super(message, throwable);
     }
 
     /**
+     * 抛出一个新的UnknownDependencyException异常
      * Constructs a new UnknownDependencyException
      */
     public UnknownDependencyException() {
