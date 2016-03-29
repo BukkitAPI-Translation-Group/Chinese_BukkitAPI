1ce7e593a094811c71712bb164aa2a090a431fb9
hcrgm <hcrgm@qq.com>
2016-03-29 18:42:10 +0800

    翻译CommandExecutor类

--- BukkitApi/org/bukkit/command/CommandExecutor.java
+++ BukkitApi/org/bukkit/command/CommandExecutor.java
@@ -1,18 +1,20 @@
 package org.bukkit.command;
 
 /**
- * Represents a class which contains a single method for executing commands
+ * 包含了一个执行命令的方法的类.
  */
 public interface CommandExecutor {
 
     /**
-     * Executes the given command, returning its success
+     * 执行给定的命令，成功时返回.
+     * <p>
+     * 原文:Executes the given command, returning its success
      *
-     * @param sender Source of the command
-     * @param command Command which was executed
-     * @param label Alias of the command which was used
-     * @param args Passed command arguments
-     * @return true if a valid command, otherwise false
+     * @param sender 命令执行的来源
+     * @param command 被执行的命令
+     * @param label 使用的命令别名
+     * @param args 传递的命令参数
+     * @return 如果是合法的命令则为true，否则为false
      */
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args);
 }
