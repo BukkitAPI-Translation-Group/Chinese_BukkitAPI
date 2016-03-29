e458b60df5f84662b00868ca1f5ab42d988f62a8
hcrgm <hcrgm@qq.com>
2016-03-29 17:38:50 +0800

    Update ProxiedCommandSender.java

--- BukkitApi/org/bukkit/command/ProxiedCommandSender.java
+++ BukkitApi/org/bukkit/command/ProxiedCommandSender.java
@@ -1,19 +1,22 @@
-
 package org.bukkit.command;
 
 public interface ProxiedCommandSender extends CommandSender {
 
     /**
-     * Returns the CommandSender which triggered this proxied command
+     * 返回触发这个代理命令的CommandSender.
+     * <p>
+     * 原文:Returns the CommandSender which triggered this proxied command
      *
-     * @return the caller which triggered the command
+     * @return 命令调用者
      */
     CommandSender getCaller();
 
     /**
-     * Returns the CommandSender which is being used to call the command
+     * 返回CommandSender正在运行的命令.
+     * <p>
+     * 原文:Returns the CommandSender which is being used to call the command
      *
-     * @return the caller which the command is being run as
+     * @return 调用者正在运行的命令
      */
     CommandSender getCallee();
 
