f9d8e8f43b67a250aaef8a13a827ba7cd7c85283
hcrgm <hcrgm@qq.com>
2016-05-27 20:26:40 +0800

    额

--- BukkitApi/org/bukkit/material/Attachable.java
+++ BukkitApi/org/bukkit/material/Attachable.java
@@ -3,14 +3,19 @@ package org.bukkit.material;
 import org.bukkit.block.BlockFace;
 
 /**
- * Indicates that a block can be attached to another block
+ * 表示可以与另一个方块连接的方块(比如箱子、门).
  */
 public interface Attachable extends Directional {
 
     /**
+     * 获取这个方块的朝向.
+     * <p>
+     * 译注：比如箱子，箱子可以有东西南北四个朝向。
+     * <p>
+     * 原文：
      * Gets the face that this block is attached on
      *
-     * @return BlockFace attached to
+     * @return 朝向
      */
     public BlockFace getAttachedFace();
 }
