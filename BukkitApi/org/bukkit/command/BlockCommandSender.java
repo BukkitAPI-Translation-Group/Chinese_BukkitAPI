5bd615d4adc7e339d6bd9ef5be3976ef7d0df8df
22284625 <22284625@qq.com>
2016-06-08 13:05:30 +0800

    Update BlockCommandSender.java

--- BukkitApi/org/bukkit/command/BlockCommandSender.java
+++ BukkitApi/org/bukkit/command/BlockCommandSender.java
@@ -5,7 +5,7 @@ import org.bukkit.block.Block;
 public interface BlockCommandSender extends CommandSender {
 
     /**
-     * 返回方命令发送者的方块.
+     * 返回命令发送者的方块.
      * <p>
      * 原文:
      * Returns the block this command sender belongs to
