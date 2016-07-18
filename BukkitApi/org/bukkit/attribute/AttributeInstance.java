9ab45c7d3f4f5aa59a85532016aadd940e9ddafc
hcrgm <hcrgm@qq.com>
2016-07-18 09:43:36 +0800

    Update AttributeInstance.java

--- BukkitApi/org/bukkit/attribute/AttributeInstance.java
+++ BukkitApi/org/bukkit/attribute/AttributeInstance.java
@@ -3,13 +3,14 @@ package org.bukkit.attribute;
 import java.util.Collection;
 
 /**
- * Represents a mutable instance of an attribute and its associated modifiers
- * and values.
+ * 代表一个属性的可变实例以及它的修饰符和数据值.
  */
 public interface AttributeInstance {
 
     /**
-     * 有关该实例的属性
+     * 属于该实例的属性.
+     * <p>
+     * 原文:The attribute pertaining to this instance.
      *
      * @return 返回对应实例的属性
      */
@@ -17,43 +18,56 @@ public interface AttributeInstance {
 
     /**
      * 获取所有属性的基准值(也就是未更改之前的默认值)
+     * <p>
+     * 原文:Base value of this instance before modifiers are applied.
      *
      * @return 默认属性值
      */
     double getBaseValue();
 
     /**
-     * 设置默认值为某个属性
+     * 设置基准值为某个属性.
+     * <p>
+     * 原文:Set the base value of this instance.
      *
-     * @param 为了给某个值设定他的基础值
+     * @param value 基准值
      */
     void setBaseValue(double value);
 
     /**
-     *获取该实例上面所有被修改过的值
+     * 获取该实例上的所有修饰符(属性).
+     * <p>
+     * 原文:Get all modifiers present on this instance.
      *
-     * @return 将会返回一个包含所有修改过的值的集合
+     * @return 所有修饰符的副本
      */
     Collection<AttributeModifier> getModifiers();
 
     /**
-     * 向次实例添加要修改的属性
+     * 向次实例添加要修改的修饰符(属性).
+     * <p>
+     * 原文:Add a modifier to this instance.
      *
-     * @param 用于添加属性
+     * @param modifier 添加的属性
      */
     void addModifier(AttributeModifier modifier);
 
     /**
-     * 从此实例内移除一个属性值
+     * 从此实例内移除一个修饰符(属性).
+     * <p>
+     * 原文:Remove a modifier from this instance.
      *
-     * @param 用于移除一个属性值
+     * @param modifier 移除的属性
      */
     void removeModifier(AttributeModifier modifier);
 
     /**
      * 获取当前实例的值,（所有已经应用的值）
+     * <p>
+     * 原文:Get the value of this instance after all associated modifiers have been
+     * applied.
      *
-     * @return 总的属性值
+     * @return 总属性值
      */
     double getValue();
 }
