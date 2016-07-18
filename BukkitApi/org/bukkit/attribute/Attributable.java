d04034d8d5786244a26c6a933faaaace04e1c4be
SkyStardust <961552712@qq.com>
2016-07-18 08:41:47 +0800

    Update Attributable.java

--- BukkitApi/org/bukkit/attribute/Attributable.java
+++ BukkitApi/org/bukkit/attribute/Attributable.java
@@ -1,16 +1,17 @@
 package org.bukkit.attribute;
 
 /**
- * Represents an object which may contain attributes.
+ * 用于表示一个可能包含属性的对象
  */
 public interface Attributable {
 
     /**
-     * Gets the specified attribute instance from the object. This instance will
-     * be backed directly to the object and any changes will be visible at once.
+     * 从指定的一个对象内获取对应的属性
+     * 这个方法将直接会返回相对包含属性的对象
+     * 如果你更改了他们的属性,你将只能获取到一次对他们的更改
      *
-     * @param attribute the attribute to get
-     * @return the attribute instance or null if not applicable to this object
+     * @param 用于获取Attribute实例
+     * @return 这个方法会返回一个Attribute实例,如果对应对象不可用,将返回null
      */
     AttributeInstance getAttribute(Attribute attribute);
 }
