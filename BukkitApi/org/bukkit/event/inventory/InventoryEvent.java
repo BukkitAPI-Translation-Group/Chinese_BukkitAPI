22cf2b55baed37a43b2070503bf226fdc9931289
hcrgm <hcrgm@qq.com>
2016-07-31 21:02:28 +0800

    Update InventoryEvent.java

--- BukkitApi/org/bukkit/event/inventory/InventoryEvent.java
+++ BukkitApi/org/bukkit/event/inventory/InventoryEvent.java
@@ -1,4 +1,3 @@
-
 package org.bukkit.event.inventory;
 
 import java.util.List;
@@ -10,7 +9,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 
 /**
- * Represents a player related inventory event
+ * 代表与玩家相关的背包事件
  */
 public class InventoryEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
@@ -21,26 +20,32 @@ public class InventoryEvent extends Event {
     }
 
     /**
-     * Gets the primary Inventory involved in this transaction
+     * 获取此事件涉及的主要背包.
+     * <p>
+     * 原文:Gets the primary Inventory involved in this transaction
      *
-     * @return The upper inventory.
+     * @return 顶部背包
      */
     public Inventory getInventory() {
         return transaction.getTopInventory();
     }
 
     /**
-     * Gets the list of players viewing the primary (upper) inventory involved
+     * 获取正在查看此主要/顶部背包的玩家列表.
+     * <p>
+     * 原文:Gets the list of players viewing the primary (upper) inventory involved
      * in this event
      *
-     * @return A list of people viewing.
+     * @return 正在查看此背包的玩家
      */
     public List<HumanEntity> getViewers() {
         return transaction.getTopInventory().getViewers();
     }
 
     /**
-     * Gets the view object itself
+     * 获取背包本身的 InventoryView 对象。
+     * <p>
+     * 原文:Gets the view object itself
      *
      * @return InventoryView
      */
