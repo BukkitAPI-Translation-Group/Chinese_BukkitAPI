12602c2c1b16256273f565d4720d2a81c99b014c
hcrgm <hcrgm@qq.com>
2016-07-31 20:46:28 +0800

    首先由我开始吧^_^

--- BukkitApi/org/bukkit/event/inventory/FurnaceExtractEvent.java
+++ BukkitApi/org/bukkit/event/inventory/FurnaceExtractEvent.java
@@ -6,7 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockExpEvent;
 
 /**
- * This event is called when a player takes items out of the furnace
+ * 当玩家从熔炉取出产物时触发本事件.
  */
 public class FurnaceExtractEvent extends BlockExpEvent {
     private final Player player;
@@ -21,27 +21,33 @@ public class FurnaceExtractEvent extends BlockExpEvent {
     }
 
     /**
-     * Get the player that triggered the event
+     * 获取触发这个事件的玩家.
+     * <p>
+     * 原文:Get the player that triggered the event
      *
-     * @return the relevant player
+     * @return 相关玩家
      */
     public Player getPlayer() {
         return player;
     }
 
     /**
-     * Get the Material of the item being retrieved
+     * 获取被取出的物品.
+     * <p>
+     * 原文：Get the Material of the item being retrieved
      *
-     * @return the material of the item
+     * @return 物品
      */
     public Material getItemType() {
         return itemType;
     }
 
     /**
-     * Get the item count being retrieved
+     * 获取被取出的物品的数量.
+     * <p>
+     * 原文:Get the item count being retrieved
      *
-     * @return the amount of the item
+     * @return 物品数量
      */
     public int getItemAmount() {
         return itemAmount;
