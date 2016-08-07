18d1c02ba81eba432f0705f254e7457365210c7e
100 <1005002050@qq.com>
2016-08-07 22:09:27 +0800

    Update PlayerEditBookEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerEditBookEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerEditBookEvent.java
@@ -8,8 +8,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.meta.BookMeta;
 
 /**
- * Called when a player edits or signs a book and quill item. If the event is
- * cancelled, no changes are made to the BookMeta
+ *  当玩家编辑或签名书与笔时触发。如果事件中断取消，书与笔的元数据不会改变。
  */
 public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -37,47 +36,43 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
-     * Gets the book meta currently on the book.
+     * 获取当前书本元数据。
      * <p>
-     * Note: this is a copy of the book meta. You cannot use this object to
-     * change the existing book meta.
+     * 注意：获取书本的元数据副本。你无法使用该对象来修改书本元数据。
      *
-     * @return the book meta currently on the book
+     * @return 当前书本的元数据
      */
     public BookMeta getPreviousBookMeta() {
         return previousBookMeta.clone();
     }
 
     /**
-     * Gets the book meta that the player is attempting to add to the book.
+     * 获取玩家试图新增的书本元数据。 
      * <p>
-     * Note: this is a copy of the proposed new book meta. Use {@link
-     * #setNewBookMeta(BookMeta)} to change what will actually be added to the
-     * book.
+     * 注意：获取的元数据为副本。使用 {@link setNewBookMeta(BookMeta)}修改实际想要更新的书本元数据
      *
-     * @return the book meta that the player is attempting to add
+     * @return 玩家试图新增的书本元数据
      */
     public BookMeta getNewBookMeta() {
         return newBookMeta.clone();
     }
 
     /**
-     * Gets the inventory slot number for the book item that triggered this
-     * event.
+     * 获取触发事件时，书本在物品栏所在的格子序号。
      * <p>
-     * This is a slot number on the player's hotbar in the range 0-8.
+     * 对应为玩家快捷操作栏，取值范围 0-8。
      *
-     * @return the inventory slot number that the book item occupies
+     * @return 事件中书本在玩家物品栏所占用的格子序号
      */
     public int getSlot() {
         return slot;
     }
 
     /**
-     * Sets the book meta that will actually be added to the book.
+     * 设置新增的书本元数据。
      *
-     * @param newBookMeta new book meta
-     * @throws IllegalArgumentException if the new book meta is null
+     * @param newBookMeta 构造书本元数据
+     * @throws IllegalArgumentException 当构造的书本元数据不为空值时抛出异常
      */
     public void setNewBookMeta(BookMeta newBookMeta) throws IllegalArgumentException {
         Validate.notNull(newBookMeta, "New book meta must not be null");
@@ -86,20 +81,19 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
-     * Gets whether or not the book is being signed. If a book is signed the
-     * Material changes from BOOK_AND_QUILL to WRITTEN_BOOK.
-     *
-     * @return true if the book is being signed
+     * 检测书本是否正在被签名。如果正在签名，书与笔将转变为成书。
+     * 
+     * 
+     * @return 当书本正在被签名时，返回true。
      */
     public boolean isSigning() {
         return isSigning;
     }
 
     /**
-     * Sets whether or not the book is being signed. If a book is signed the
-     * Material changes from BOOK_AND_QUILL to WRITTEN_BOOK.
+     * 设置书本是否正在被签名。如果正在签名，书与笔将转变为成书。
      *
-     * @param signing whether or not the book is being signed.
+     * @param signing 书本是否正在被签名
      */
     public void setSigning(boolean signing) {
         isSigning = signing;
