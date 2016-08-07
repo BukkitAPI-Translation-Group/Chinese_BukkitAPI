abfb219b05d4c10e424f9a8e71dc602c5b0e3f9d
hcrgm <hcrgm@qq.com>
2016-08-07 22:19:46 +0800

    先加原文

--- BukkitApi/org/bukkit/event/player/PlayerEditBookEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerEditBookEvent.java
@@ -8,7 +8,7 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.meta.BookMeta;
 
 /**
- *  当玩家编辑或签名书与笔时触发。如果事件中断取消，书与笔的元数据不会改变。
+ * 当玩家编辑或签名书与笔时触发。如果事件中断取消，书与笔的元数据不会改变。
  */
 public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
@@ -39,6 +39,11 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
      * 获取当前书本元数据。
      * <p>
      * 注意：获取书本的元数据副本。你无法使用该对象来修改书本元数据。
+     * <p>
+     * 原文:Gets the book meta currently on the book.
+     * <p>
+     * Note: this is a copy of the book meta. You cannot use this object to
+     * change the existing book meta.
      *
      * @return 当前书本的元数据
      */
@@ -50,6 +55,11 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
      * 获取玩家试图新增的书本元数据。 
      * <p>
      * 注意：获取的元数据为副本。使用 {@link setNewBookMeta(BookMeta)}修改实际想要更新的书本元数据
+     * <p>
+     * 原文:Gets the book meta that the player is attempting to add to the book.
+     * <p>
+     * Note: this is a copy of the proposed new book meta. Use {@link
+     * #setNewBookMeta(BookMeta)} to change what will actually be added to the
      *
      * @return 玩家试图新增的书本元数据
      */
@@ -61,6 +71,11 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
      * 获取触发事件时，书本在物品栏所在的格子序号。
      * <p>
      * 对应为玩家快捷操作栏，取值范围 0-8。
+     * <p>
+     * 原文:Gets the inventory slot number for the book item that triggered this
+     * event.
+     * <p>
+     * This is a slot number on the player's hotbar in the range 0-8.
      *
      * @return 事件中书本在玩家物品栏所占用的格子序号
      */
@@ -70,6 +85,8 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
 
     /**
      * 设置新增的书本元数据。
+     * <p>
+     * 原文:Sets the book meta that will actually be added to the book.
      *
      * @param newBookMeta 构造书本元数据
      * @throws IllegalArgumentException 当构造的书本元数据不为空值时抛出异常
@@ -82,8 +99,10 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
 
     /**
      * 检测书本是否正在被签名。如果正在签名，书与笔将转变为成书。
-     * 
-     * 
+     * <p>
+     * 原文:Gets whether or not the book is being signed. If a book is signed the
+     * Material changes from BOOK_AND_QUILL to WRITTEN_BOOK.
+     *
      * @return 当书本正在被签名时，返回true。
      */
     public boolean isSigning() {
@@ -92,6 +111,9 @@ public class PlayerEditBookEvent extends PlayerEvent implements Cancellable {
 
     /**
      * 设置书本是否正在被签名。如果正在签名，书与笔将转变为成书。
+     * <p>
+     * 原文:Sets whether or not the book is being signed. If a book is signed the 
+     * Material changes from BOOK_AND_QUILL to WRITTEN_BOOK.
      *
      * @param signing 书本是否正在被签名
      */
