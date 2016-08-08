7090c478e4e2c1815380dcbab29aa0e3b4386b26
100 <1005002050@qq.com>
2016-08-08 12:54:38 +0800

    Update PlayerEggThrowEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerEggThrowEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerEggThrowEvent.java
@@ -26,7 +26,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 获取事件中的鸡蛋
      * <p>
-     * 原文:
+     * 原文:Gets the egg involved in this event.
      *
      * @return 事件中的鸡蛋
      */
@@ -37,9 +37,10 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 检测鸡蛋是否将被孵化。服务器可能设置无互动。
      * <p>
-     * 原文:
-     *
-     * @return 布尔值 鸡蛋是否将被孵化
+     * 原文:Gets whether the egg is hatching or not. Will be what the server
+     * would've done without interaction.
+     * 
+     * @return 鸡蛋是否将被孵化
      */
     public boolean isHatching() {
         return hatching;
@@ -48,7 +49,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 设置鸡蛋是否将被孵化。
      * <p>
-     * 原文：
+     * 原文：Sets whether the egg will hatch or not.
      *
      * @param hatching 布尔值，你是否希望鸡蛋孵化。
      */
@@ -59,7 +60,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 获取将被孵化的生物类型 (默认为EntityType.CHICKEN )
      * <p>
-     * 原文:
+     * 原文:Get the type of the mob being hatched (EntityType.CHICKEN by default)
      *
      * @return 将被孵化的生物类型
      */
@@ -70,7 +71,7 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 修改将被孵化生物的类型
      * <p>
-     * 原文:
+     * 原文:Change the type of mob being hatched by the egg
      *
      * @param hatchType 将被孵化生物的类型
      */
@@ -87,7 +88,13 @@ public class PlayerEggThrowEvent extends PlayerEvent {
      * <li>1/256 的几率生成 4 只
      * </ul>
      * <p>
-     * 原文:
+     * 原文:Get the number of mob hatches from the egg. By default the number will
+     * be the number the server would've done
+     * <ul>
+     * <li>7/8 chance of being 0
+     * <li>31/256 ~= 1/8 chance to be 1
+     * <li>1/256 chance to be 4
+     * </ul>
      *
      * @return 将被孵化生物的数量
      */
@@ -98,11 +105,13 @@ public class PlayerEggThrowEvent extends PlayerEvent {
     /**
      * 改变将被孵化生物的数量
      * <p>
-     * 布尔值hatching(你是否希望鸡蛋孵化)的值将覆盖该数值的作用。
-     * 当 hatching=false ，该数值的设定不生效。
+     * {@Link hatching}的值将覆盖该数值的作用。
+     * 如果{@Link hatching}被设为false, 则本数值不会生效。
      * <p>
-     * 原文:
-     * 
+     * 原文:Change the number of mobs coming out of the hatched egg
+     * <p>
+     * The boolean hatching will override this number. Ie. If hatching =
+     * false, this number will not matter
      *
      * @param numHatches 将被孵化生物的数量
      */
