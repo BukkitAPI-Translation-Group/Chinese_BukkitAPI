c37fe7665ee6f30274c10a69f66813f8eaacd13a
SkyStardust <961552712@qq.com>
2016-08-11 10:00:39 +0800

    血条翻译完成

--- BukkitApi/org/bukkit/boss/BossBar.java
+++ BukkitApi/org/bukkit/boss/BossBar.java
@@ -7,136 +7,135 @@ import java.util.List;
 public interface BossBar {
 
     /**
-     * Returns the title of this boss bar
+     *  用于获取该Boos血量条的标题
      *
-     * @return the title of the bar
+     * @return 当前的Boss血条的标题
      */
     String getTitle();
 
     /**
-     * Sets the title of this boss bar
+     *  设置当前boss血条的标题
+     * 比如凋零这个boss的血条标题就叫凋零
      *
-     * @param title the title of the bar
+     * @param 要设置的boss血条的标题.
      */
     void setTitle(String title);
 
     /**
-     * Returns the color of this boss bar
+     * 获取这个boss的血条的颜色（一个BossColor的实例）
      *
-     * @return the color of the bar
+     * @return 返回一个BossColor的实例
      */
     BarColor getColor();
 
     /**
-     * Sets the color of this boss bar.
+     *  设置这个Boss的血条的颜色
      *
-     * @param color the color of the bar
+     * @param 要设置的血条颜色
      */
     void setColor(BarColor color);
 
     /**
-     * Returns the style of this boss bar
+     * 获取该Boss血条的样式
      *
-     * @return the style of the bar
+     * @return 这个boss血条的样式
      */
     BarStyle getStyle();
 
     /**
-     * Sets the bar style of this boss bar
+     * 设置这个boss血条的样式
      *
-     * @param style the style of the bar
+     * @param style:要设置的boss血条的样式
      */
     void setStyle(BarStyle style);
 
     /**
-     * Remove an existing flag on this boss bar
+     * 从该boss血条内移除一个指定的属性
      *
-     * @param flag the existing flag to remove
+     * @param 一个已有的指定boss血条内属性,参见BossFlag类
      */
     void removeFlag(BarFlag flag);
 
     /**
-     * Add an optional flag to this boss bar
+     * 向该血条添加一个血条属性
      *
-     * @param flag an optional flag to set on the boss bar
+     * @param 要添加的属性
      */
     void addFlag(BarFlag flag);
 
     /**
-     * Returns whether this boss bar as the passed flag set
+     * 获取某个boss血条属性是否在该血条上存在
      *
-     * @param flag the flag to check
-     * @return whether it has the flag
+     * @param 要检查是否存在的属性
+     * @return 是否拥有该属性
      */
     boolean hasFlag(BarFlag flag);
 
     /**
-     * Sets the progress of the bar. Values should be between 0.0 (empty) and
-     * 1.0 (full)
+     * 设置该血条的进度.请记住,这个参数必须是1.0（最大值）到0.0（血条空）这两个数字之间的
      *
-     * @param progress the progress of the bar
+     * @param 要设置该血条的进度
      */
     void setProgress(double progress);
 
     /**
-     * Returns the progress of the bar between 0.0 and 1.0
+     * 返回该血条的进度（只会返回1.00到0.00之间的数字）
      *
-     * @return the progress of the bar
+     * @return 这个血条的进度（怎么解释呢,就是血条剩下的血量占总血量的比值）
      */
     double getProgress();
 
     /**
-     * Adds the player to this boss bar causing it to display on their screen.
+     * 向该boss血条内添加一个玩家（也就是使该玩家能看见这个血条）
      *
-     * @param player the player to add
+     * @param 要添加的玩家
      */
     void addPlayer(Player player);
 
     /**
-     * Removes the player from this boss bar causing it to be removed from their
-     * screen.
+     * 从一个boss血条内移除某个玩家（就是使指定玩家看不见该BOSS血条）
      *
-     * @param player the player to remove
+     * @param 要移除的玩家
      */
     void removePlayer(Player player);
 
     /**
-     * Removes all players from this boss bar
+     * 从该boss血条内移除所有玩家
      *
      * @see #removePlayer(Player)
      */
     void removeAll();
 
     /**
-     * Returns all players viewing this boss bar
+     * 返回所有能看到这个boss血条的玩家
      *
-     * @return a immutable list of players
+     * @return 一个包含所有能看到这个血条的玩家的List
      */
     List<Player> getPlayers();
 
     /**
-     * Set if the boss bar is displayed to attached players.
+     * 设置这个boss血条是否可见
      *
-     * @param visible visible status
+     * @param 该血条的可见状态
      */
     void setVisible(boolean visible);
 
     /**
-     * Return if the boss bar is displayed to attached players.
+     * 获取该血条是否可见
      *
-     * @return visible status
+     * @return 可见状态
      */
     boolean isVisible();
 
     /**
-     * Shows the previously hidden boss bar to all attached players
+     * 将该boss血条设置为可见状态
      * @deprecated {@link #setVisible(boolean)}
      */
     @Deprecated
     void show();
 
     /**
-     * Hides this boss bar from all attached players
+     * 从所有能看见的玩家屏幕上隐藏该血条
      * @deprecated {@link #setVisible(boolean)}
      */
     @Deprecated
