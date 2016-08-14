6cc874bfaa2600c0a9949e0878983e66ec8286d9
hcrgm <hcrgm@qq.com>
2016-08-14 14:09:33 +0800

    挺完美的翻译

--- BukkitApi/org/bukkit/TravelAgent.java
+++ BukkitApi/org/bukkit/TravelAgent.java
@@ -16,6 +16,7 @@ public interface TravelAgent {
 
     /**
      * 设置搜索有效传送门的方块半径。
+     * <p>
      * 原文：
      * Set the Block radius to search in for available portals.
      *
@@ -26,6 +27,7 @@ public interface TravelAgent {
 
     /**
      * 获取寻找有效传送门的搜索半径数值。
+     * <p>
      * 原文：
      * Gets the search radius value for finding an available portal.
      *
@@ -35,6 +37,7 @@ public interface TravelAgent {
 
     /**
      * 设置以指定方位为圆心生成传送门的最大半径。
+     * <p>
      * 原文：
      * Sets the maximum radius from the given location to create a portal.
      *
@@ -45,6 +48,7 @@ public interface TravelAgent {
 
     /**
      * 获取以指定方位为圆心生成传送门的最大半径。
+     * <p>
      * 原文：
      * Gets the maximum radius from the given location to create a portal.
      *
@@ -54,6 +58,7 @@ public interface TravelAgent {
 
     /**
      * 返回TravelAgent是否尝试生成目的传送门。
+     * <p>
      * 原文：
      * Returns whether the TravelAgent will attempt to create a destination
      * portal or not.
@@ -64,6 +69,7 @@ public interface TravelAgent {
 
     /**
      * 设置TravelAgent是否尝试生成目的传送门。
+     * <p>
      * 原文：
      * Sets whether the TravelAgent should attempt to create a destination
      * portal or not.
@@ -74,6 +80,7 @@ public interface TravelAgent {
 
     /**
      * 尝试寻找指定方位附近的传送门，如果找不到则会尝试生成一个。
+     * <p>
      * 原文：
      * Attempt to find a portal near the given location, if a portal is not
      * found it will attempt to create one.
@@ -86,6 +93,7 @@ public interface TravelAgent {
 
     /**
      * 尝试寻找指定方位附近的传送门。
+     * <p>
      * 原文：
      * Attempt to find a portal near the given location.
      *
@@ -100,6 +108,7 @@ public interface TravelAgent {
      * 如果传送到下界将会尝试生成一个下界传送门。
      * <p>
      * 如果传送到末地将会（重新）生成黑曜石平台并清除上方的方块。
+     * <p>
      * 原文：
      * Attempt to create a portal near the given location.
      * <p>
