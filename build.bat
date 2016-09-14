cdc8f335eba72e6549cbe9a698a5137b24200462
zhouhaha <MJX1627025175@126.com>
2016-09-14 22:26:16 +0800

    added build script

---
+++ build.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Building javadoc...
+title Building javadoc...
+javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi;javadoc" -d docs -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -windowtitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -subpackages org.bukkit
+title Build Completed
+echo.
+echo BUILD COMPLETED
+echo.
+pause
