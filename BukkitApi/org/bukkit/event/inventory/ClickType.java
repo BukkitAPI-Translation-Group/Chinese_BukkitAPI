1861b59d481e5cae5f779a6700bdba6d2b33a3c1
Yun-Shan <1773460341@qq.com>
2016-09-14 10:39:50 +0800

    修正一些注释
    标点符号统一使用英文标点符号
    修正了一些表述
    ClickType中增加了一堆译注

--- BukkitApi/org/bukkit/event/inventory/ClickType.java
+++ BukkitApi/org/bukkit/event/inventory/ClickType.java
@@ -41,6 +41,8 @@ public enum ClickType {
     MIDDLE,
     /**
      * 数字键1-9，对应快捷栏的物品槽
+     * <p>
+     * 译注: 该数字键指主键盘的数字键, 而非小键盘的数字键
      */
     NUMBER_KEY,
     /**
@@ -60,22 +62,29 @@ public enum ClickType {
      */
     CREATIVE,
     /**
-     * 无法被Bukkit解析的物品栏点击类型
+     * 无法被Bukkit解析的物品栏点击类型. 
      * <p>
-     * 这个按键类型仅出现在Minecraft的版本过渡期，且不应该被依赖
+     * 这个按键类型仅出现在Minecraft的版本过渡期, 且不应该被依赖. <br>
+     * 任何对 ClickType.UNKNOWN 的调用都是在最努力的基础上的.
      * <p>
-     * 任何对 ClickType.UNKNOWN 的调用都是在最努力的基础上的
-     * 译注:一般只有可能在支持最新快照版本的插件中才可能需要使用
+     * 译注:一般只在支持最新快照版本的插件中才可能需要使用
      */
     UNKNOWN,
     ;
 
     /**
-     * 判断这个按键类型是否为键盘按键.
+     * 判断这个按键类型是否为键盘按键. 
+     * <p>
+     * 译注: 键盘按键包括:
+     * <ul>
+     * <li>{@link #NUMBER_KEY 数字键}</li>
+     * <li>{@link #DROP 丢弃物品键}</li>
+     * <li>{@link #CONTROL_DROP Ctrl+丢弃物品键}</li>
+     * </ul>
      * <p>
      * 原文：Gets whether this ClickType represents the pressing of a key on a
      * keyboard.
-     *
+     * 
      * @return 如果这个按键类型为键盘按键则返回true，否则返回false
      */
     public boolean isKeyboardClick() {
@@ -83,35 +92,56 @@ public enum ClickType {
     }
 
     /**
-     * 判断这个按键类型是否只能在创造模式被执行.
+     * 判断这个按键类型所定义的操作是否只能在创造模式被执行. 
+     * <p>
+     * 译注: 只有创造模式有效的按键类型包括:
+     * <ul>
+     * <li>{@link #MIDDLE 鼠标中键}</li>
+     * <li>{@link #CREATIVE 创造模式物品栏的任何点击操作}</li>
+     * </ul>
      * <p>
      * 原文：Gets whether this ClickType represents an action that can only be
      * performed by a Player in creative mode.
-     *
+     * 
      * @return 如果这个按键类型要求玩家处在创造模式则返回true，否则返回false
      */
     public boolean isCreativeAction() {
         // Why use middle click?
+        // <s>译注</s>译回答: 创造模式可以用鼠标中键可以将玩家所对的方块对应的物品复制到快捷栏.
         return (this == ClickType.MIDDLE) || (this == ClickType.CREATIVE);
     }
 
     /**
-     * 判断这个按键类型是否使用了右键.
+     * 判断这个按键类型是否使用了鼠标右键. 
+     * <p>
+     * 译注: 使用了鼠标右键的按键类型包括:
+     * <ul>
+     * <li>{@link #RIGHT 鼠标右键}</li>
+     * <li>{@link #SHIFT_RIGHT Shift+鼠标右键}</li>
+     * </ul>
      * <p>
      * 原文：Gets whether this ClickType represents a right click.
-     *
-     * @return 如果这个按键类型使用了右键则返回true，否则返回false
+     * 
+     * @return 如果这个按键类型使用了鼠标右键则返回true，否则返回false
      */
     public boolean isRightClick() {
         return (this == ClickType.RIGHT) || (this == ClickType.SHIFT_RIGHT);
     }
 
     /**
-     * 判断这个按键类型是否使用了左键.
+     * 判断这个按键类型是否使用了鼠标左键.
+     * <p>
+     * 译注: 使用了鼠标左键的按键类型包括:
+     * <ul>
+     * <li>{@link #LEFT 鼠标左键}</li>
+     * <li>{@link #SHIFT_LEFT Shift+鼠标左键}</li>
+     * <li>{@link #DOUBLE_CLICK 双击鼠标左键}</li>
+     * <li>{@link #CREATIVE 创造模式物品栏的任何点击操作}</li>
+     * </ul>
      * <p>
      * 原文：Gets whether this ClickType represents a left click.
-     *
-     * @return 如果这个按键类型使用了左键则返回true，否则返回false
+     * 
+     * @return 如果这个按键类型使用了鼠标左键则返回true，否则返回false
      */
     public boolean isLeftClick() {
         return (this == ClickType.LEFT) || (this == ClickType.SHIFT_LEFT) || (this == ClickType.DOUBLE_CLICK) || (this == ClickType.CREATIVE);
@@ -119,6 +149,13 @@ public enum ClickType {
 
     /**
      * 判断这个按键类型是否使用了Shift键.
+     * <p>
+     * 译注: 使用了Shift键的按键类型包括:
+     * <ul>
+     * <li>{@link #SHIFT_LEFT Shift+鼠标左键}</li>
+     * <li>{@link #SHIFT_RIGHT Shift+鼠标右键}</li>
+     * <li><b>{@link #CONTROL_DROP Ctrl+丢弃物品键}(<i>暂不知为何要将此按键类型判断为使用了Shift键</i>)</b></li>
+     * </ul>>
      * <p>
      * 原文：Gets whether this ClickType indicates that the shift key was pressed
      * down when the click was made.
--- BukkitApi/org/bukkit/event/inventory/CraftItemEvent.java
+++ BukkitApi/org/bukkit/event/inventory/CraftItemEvent.java
@@ -6,8 +6,9 @@ import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.Recipe;
 
 /**
- * 当一个物品被合成的时候触发这个事件。
- * 译注:触发这个事件的时候，物品已经被合成出来。
+ * 当一个物品被合成的时候触发这个事件. 
+ * <p>
+ * 译注: 发生这个事件的时候, 物品已经被合成出来.
  */
 public class CraftItemEvent extends InventoryClickEvent {
     private Recipe recipe;
@@ -29,6 +30,7 @@ public class CraftItemEvent extends InventoryClickEvent {
 
     /**
      * 获取合成出该物品所用的合成公式.
+     * 
      * @return 被合成出来的物品所用的合成公式
      */
     public Recipe getRecipe() {
