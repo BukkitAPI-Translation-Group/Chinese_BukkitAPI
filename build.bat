3b05cd7645e166f0ac7ef5132852e82baa5aacdd
hcrgm <hcrgm@qq.com>
2016-09-15 14:37:02 +0800

    change the charset

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Building javadoc...
 title Building javadoc...
-javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi;javadoc" -d target -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -windowtitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -subpackages org.bukkit
+javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi;javadoc" -d target -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 涓枃鏂囨。" -windowtitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 涓枃鏂囨。" -subpackages org.bukkit
 title Build Completed
 echo.
 echo BUILD COMPLETED
