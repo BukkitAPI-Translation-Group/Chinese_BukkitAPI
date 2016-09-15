f027b6e9570283c038a50f16bae0d912266e0649
hcrgm <hcrgm@qq.com>
2016-09-15 14:13:15 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ -d "target" ]; then
+	echo "Deleting old javadoc"
+	rm -rf target
+fi
 echo "Building javadoc..."
 javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi:javadoc" -d target -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -windowtitle "Bukkit 1.10.2-R0.1-SNAPSHOT API 中文文档" -subpackages org.bukkit
 echo 
