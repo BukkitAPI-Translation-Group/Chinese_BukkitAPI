055c70c9d20b7b71a8dfc9219617a04a73cea56c
hcrgm <hcrgm@qq.com>
2016-09-27 19:01:30 +0800

    移除侮辱智商的译注

--- BukkitApi/org/bukkit/entity/Hanging.java
+++ BukkitApi/org/bukkit/entity/Hanging.java
@@ -4,14 +4,12 @@ import org.bukkit.block.BlockFace;
 import org.bukkit.material.Attachable;
 
 /**
- * 代表悬挂实体。
- * <p>
- * 译注：什么是悬挂实体？比如画，它属于实体，而且可以悬挂，那么我们就称那些实体为悬挂实体。
+ * 代表悬挂实体.
  */
 public interface Hanging extends Entity, Attachable {
 
     /**
-     * 设置悬挂实体的方向，这可能会重写所有规则。注意：如果结果是无效对象，该对象会作为一个物品而掉落.
+     * 设置悬挂实体的方向，这可能会重写所有规则. 注意：如果结果无效，实体可能会掉落为物品.
      * <p>
      * 原文：Sets the direction of the hanging entity, potentially overriding rules
      * of placement. Note that if the result is not valid the object would
