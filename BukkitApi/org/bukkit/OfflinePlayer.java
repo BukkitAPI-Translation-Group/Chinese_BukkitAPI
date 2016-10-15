89bf4822c088756f5fcb23e27b5a28c109c071b6
hcrgm <hcrgm@qq.com>
2016-10-15 10:57:00 +0800

    Update OfflinePlayer.java

--- BukkitApi/org/bukkit/OfflinePlayer.java
+++ BukkitApi/org/bukkit/OfflinePlayer.java
@@ -20,15 +20,15 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
     public boolean isOnline();
 
     /**
-     * 返回该玩家的游戏name
+     * 返回该玩家的游戏名
      * <p>
-     * 游戏name并不再是唯一的游戏标识,如果你希望长时间储存,建议使用{@link #getUniqueId()}来替代
+     * 游戏名并不再是唯一的游戏标识,如果你希望长时间储存,建议使用{@link #getUniqueId()}来替代
      * <p>
      * 原文:Names are no longer unique past a single game session. For persistent storage
      * <p>
      * it is recommended that you use {@link #getUniqueId()} instead.
      *
-     * @return 返回玩家游戏name,如果玩家不存在返回null
+     * @return 返回玩家游戏名,如果玩家不存在返回null
      */
     public String getName();
 
@@ -42,7 +42,7 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
     /**
      * 检测该玩家是否被封禁
      *
-     * @return 返回true说明被封禁,反之返回false
+     * @return 是否被封禁
      */
     public boolean isBanned();
 
@@ -50,9 +50,8 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
      * 对玩家进行封禁或解封
      *
      * @param banned 如果要对其封禁使用true
-     * @deprecated Use {@link org.bukkit.BanList#addBan(String, String, Date,
-     *     String)} or {@link org.bukkit.BanList#pardon(String)} to enhance
-     *     functionality
+     * @deprecated 可使用 {@link org.bukkit.BanList#addBan(String, String, Date,
+     *     String)} 或 {@link org.bukkit.BanList#pardon(String)} 来增强功能
      */
     @Deprecated
     public void setBanned(boolean banned);
@@ -60,7 +59,7 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
     /**
      * 检测玩家是否存在白名单列表中
      *
-     * @return 存在返回true,反之返回false
+     * @return 是否加白
      */
     public boolean isWhitelisted();
 
@@ -72,7 +71,7 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
     public void setWhitelisted(boolean value);
 
     /**
-     * 获取一个 {@link Player}对象,如果存在?
+     * 获取一个 {@link Player}对象
      * <p>
      * 原文:Gets a {@link Player} object that this represents, if there is one
      * <p>
@@ -83,7 +82,7 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
     public Player getPlayer();
 
     /**
-     * 获取该玩家第一次进入服务器的日期时间
+     * 获取该玩家第一次进入服务器的时间
      * <p>
      * 原文:Gets the first date and time that this player was witnessed on this
      * server.
@@ -94,12 +93,12 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
      * it will be the amount of milliseconds since midnight, January 1, 1970
      * UTC.
      *
-     * @return 返回玩家第一次进入服务器的日期时间,或返回0
+     * @return 返回玩家第一次进入服务器的时间
      */
     public long getFirstPlayed();
 
     /**
-     * 获取该玩家最后一次登录服务器的日期时间
+     * 获取该玩家最后一次登录服务器的时间
      * <p>
      * 原文:Gets the last date and time that this player was witnessed on this
      * server.
@@ -110,23 +109,21 @@ public interface OfflinePlayer extends ServerOperator, AnimalTamer, Configuratio
      * it will be the amount of milliseconds since midnight, January 1, 1970
      * UTC.
      *
-     * @return 返回最后登录的日期时间,或返回0
+     * @return 返回最后登录的时间
      */
     public long getLastPlayed();
 
     /**
-     * 检测该玩家是否是服务器成员
+     * 检测该玩家是否在先前在此服游戏过
      * <p>
      * 原文:Checks if this player has played on this server before.
-     * <p>
-     * 译注:可以理解为是否进入过服务器
      *
      * @return 如果是返回true,反之返回false
      */
     public boolean hasPlayedBefore();
 
     /**
-     * 获取该玩家在床的重生点的Location对象,但如果该玩家从未使用过床或该重生点是无效的,则会返回null
+     * 获取该玩家在床的重生点的Location对象,但如果该玩家从未使用过床或该重生点无效,则会返回null
      * <p>
      * 原文:Gets the Location where the player will spawn at their bed, null if
      * they have not slept in one or their current bed spawn is invalid.
