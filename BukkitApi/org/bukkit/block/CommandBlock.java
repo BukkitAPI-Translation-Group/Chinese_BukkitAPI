02a48127353de719be2c21d2d644fbf68c41ae86
hcrgm <hcrgm@qq.com>
2016-11-12 06:45:04 -0600

    Update CommandBlock.java

--- BukkitApi/org/bukkit/block/CommandBlock.java
+++ BukkitApi/org/bukkit/block/CommandBlock.java
@@ -3,7 +3,7 @@ package org.bukkit.block;
 public interface CommandBlock extends BlockState {
 
     /**
-     * 获取这个命令方块激活时运行的命令。这将永远不会为空，反而会返回空的字符串。
+     * 获取这个命令方块激活时运行的命令。本方法不会返回null，若此命令方块还没有设置命令，会返回空字符.
      * <p>
      * 原文：Gets the command that this CommandBlock will run when powered.
      * This will never return null.  If the CommandBlock does not have a
@@ -14,7 +14,7 @@ public interface CommandBlock extends BlockState {
     public String getCommand();
 
     /**
-     * 设置这个命令方块激活时运行的命令。设置这个命令为null与设置这个命令为空字符串是一样的。
+     * 设置这个命令方块激活时运行的命令。设置这个命令为null等价于设置命令为空字符串.
      * <p>
      * 原文：Sets the command that this CommandBlock will run when powered.
      * Setting the command to null is the same as setting it to an empty
@@ -25,7 +25,7 @@ public interface CommandBlock extends BlockState {
     public void setCommand(String command);
 
     /**
-     * 获取这个命令方块的名字.这个名字用于命令方块执行命令。这个名字永远不会为空，默认为“@”.
+     * 获取这个命令方块的名字.这个名字被用于命令方块执行命令。这个名字永远不会为空，默认为“@”.
      * <p>
      * 原文：Gets the name of this CommandBlock.  The name is used with commands
      * that this CommandBlock executes.  This name will never be null, and
@@ -36,7 +36,7 @@ public interface CommandBlock extends BlockState {
     public String getName();
 
     /**
-     * 设置这个命令方块的名字。这个名字用于命令方块执行命令。设置名字为null与设置为“@”是一样的。
+     * 设置这个命令方块的名字。这个名字用于命令方块执行命令。设置名字为null等价于设置为“@”.
      * <p>
      * 原文：Sets the name of this CommandBlock.  The name is used with commands
      * that this CommandBlock executes.  Setting the name to null is the
