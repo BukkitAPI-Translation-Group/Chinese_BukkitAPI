d8dc4ee3290bc5c07a195b9939799bac2a3eae0b
hcrgm <hcrgm@qq.com>
2016-12-30 19:18:26 +0800

    接受反馈

--- BukkitApi/org/bukkit/inventory/meta/Repairable.java
+++ BukkitApi/org/bukkit/inventory/meta/Repairable.java
@@ -1,34 +1,34 @@
 package org.bukkit.inventory.meta;
 
 /**
- * 代表可以被修复的物品.
+ * 代表可以在铁砧上被修复的物品.
  */
 public interface Repairable {
 
     /**
-     * 检测这个物品是否损耗了耐久.
+     * 检测修复此物品是否需要消耗额外经验等级.
      * <p>
      * 原文：Checks to see if this has a repair penalty
      *
-     * @return 如果这个物品损耗了耐久则为true
+     * @return 修复此物品是否需要消耗额外经验等级
      */
     boolean hasRepairCost();
 
     /**
-     * 获取这个物品的耐久度.
+     * 获取修复此物品所需的额外经验等级.
      * <p>
      * 原文：Gets the repair penalty
      *
-     * @return 耐久度
+     * @return 修复此物品所需的额外经验等级
      */
     int getRepairCost();
 
     /**
-     * 设置这个物品的耐久度.
+     * 设置修复此物品所需的额外经验等级.
      * <p>
      * 原文：Sets the repair penalty
      *
-     * @param cost 耐久度
+     * @param cost 修复此物品所需的额外经验等级
      */
     void setRepairCost(int cost);
 
