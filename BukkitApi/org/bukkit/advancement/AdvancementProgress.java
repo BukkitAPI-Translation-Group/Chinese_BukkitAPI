9cf2dc2aa9f6cc9dc4e042e737f9536030c27217
hcrgm <hcrgm@qq.com>
2017-07-02 18:48:57 +0800

    Update AdvancementProgress.java

--- BukkitApi/org/bukkit/advancement/AdvancementProgress.java
+++ BukkitApi/org/bukkit/advancement/AdvancementProgress.java
@@ -4,61 +4,74 @@ import java.util.Collection;
 import java.util.Date;
 
 /**
- * The individual status of an advancement for a player. This class is not
- * reference safe as the underlying advancement may be reloaded.
+ * 玩家达成一个进度的状态. 这个类不是指针安全的，因为底层的进度可能会被重载.
  */
 public interface AdvancementProgress {
 
     /**
+     * 返回这个进度关于的进度 (前者指达成进度，后者指游戏中的进度，是不是搞混了？).
+     * <p>
+     * 原文:
      * The advancement this progress is concerning.
      *
-     * @return the relevant advancement
+     * @return 相应的进度
      */
     Advancement getAdvancement();
 
     /**
-     * Check if all criteria for this advancement have been met.
+     * 检查是否达到了完成该进度的所有要求 (即是否达成了该进度).
+     * <p>
+     * 原文:Check if all criteria for this advancement have been met.
      *
-     * @return true if this advancement is done
+     * @return 是否达成了该进度
      */
     boolean isDone();
 
     /**
-     * Mark the specified criteria as awarded at the current time.
+     * 标记指定的条件为已授予的条件.
+     * <p>
+     * 原文:Mark the specified criteria as awarded at the current time.
      *
-     * @param criteria the criteria to mark
-     * @return true if awarded, false if criteria does not exist or already
-     * awarded.
+     * @param criteria 要标记的条件
+     * @return 若操作成功返回true，若条件不存在或已经达到返回false
      */
     boolean awardCriteria(String criteria);
 
     /**
-     * Mark the specified criteria as uncompleted.
+     * 标记指定的条件为未完成的条件.
+     * <p>
+     * 原文:Mark the specified criteria as uncompleted.
      *
-     * @param criteria the criteria to mark
-     * @return true if removed, false if criteria does not exist or not awarded
+     * @param criteria 要标记的条件
+     * @return 若移除条件成功返回true，若条件不存在或还未达到返回false
      */
     boolean revokeCriteria(String criteria);
 
     /**
-     * Get the date the specified critera was awarded.
+     * 获取指定条件被授予的日期.
+     * <p>
+     * 原文:Get the date the specified critera was awarded.
      *
-     * @param criteria the criteria to check
-     * @return date awarded or null if unawarded or criteria does not exist
+     * @param criteria 要检查的条件
+     * @return 条件被授予的时间，如果条件不存在或还未达到则为null
      */
     Date getDateAwarded(String criteria);
 
     /**
-     * Get the criteria which have not been awarded.
+     * 获取达成某一进度还未达到的条件(剩余的条件).
+     * <p>
+     * 原文:Get the criteria which have not been awarded.
      *
-     * @return unmodifiable copy of criteria remaining
+     * @return 剩余的条件的不可编辑的副本
      */
     Collection<String> getRemainingCriteria();
 
     /**
-     * Gets the criteria which have been awarded.
+     * 获取达成某一进度已经达到的条件.
+     * <p>
+     * 原文:Gets the criteria which have been awarded.
      *
-     * @return unmodifiable copy of criteria awarded
+     * @return 已达到的条件的不可编辑的副本
      */
     Collection<String> getAwardedCriteria();
 }
