caa3abb85e63091f3124c1ba54a61bec9a96dacc
hcrgm <hcrgm@qq.com>
2017-08-02 21:25:24 +0800

    Update PlayerChangedWorldEvent.java

--- BukkitApi/org/bukkit/event/player/PlayerChangedWorldEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerChangedWorldEvent.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 
 /**
- * 玩家进行世界传送时触发此事件.
+ * 当玩家切换到另一个世界时触发此事件.
  */
 public class PlayerChangedWorldEvent extends PlayerEvent {
     private static final HandlerList handlers = new HandlerList();
@@ -17,11 +17,11 @@ public class PlayerChangedWorldEvent extends PlayerEvent {
     }
 
     /**
-     * 获得玩家传送到的世界.
+     * 获得玩家切换到目标世界之前所在的世界.
      * <p>
      * 原文:Gets the world the player is switching from.
      *
-     * @return 玩家传送到的世界
+     * @return 玩家切换到目标世界之前所在的世界
      */
     public World getFrom() {
         return from;
