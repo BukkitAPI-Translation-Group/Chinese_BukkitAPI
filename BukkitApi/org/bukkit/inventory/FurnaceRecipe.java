f3dc9c50031991d3c50e300cdbab888f3d48a445
hcrgm <hcrgm@qq.com>
2017-11-03 23:59:11 -0500

    FurnaceRecipe翻译改善

--- BukkitApi/org/bukkit/inventory/FurnaceRecipe.java
+++ BukkitApi/org/bukkit/inventory/FurnaceRecipe.java
@@ -4,7 +4,7 @@ import org.bukkit.Material;
 import org.bukkit.material.MaterialData;
 
 /**
- * 熔炉冶炼公式.
+ * 熔炉冶炼配方.
  * <p>
  * 原文:Represents a smelting recipe.
  */
@@ -14,11 +14,11 @@ public class FurnaceRecipe implements Recipe {
     private float experience;
 
     /**
-     * 创建一个熔炉冶炼公式.
+     * 创建一个熔炉冶炼配方.
      * <p>
      * 原文:Create a furnace recipe to craft the specified ItemStack.
      *
-     * @param result 填入该冶炼公式产生的生成物.
+     * @param result 填入该冶炼配方产生的生成物.
      * @param source 冶炼原料.
      */
     public FurnaceRecipe(ItemStack result, Material source) {
@@ -26,11 +26,11 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 创建一个熔炉冶炼公式.
+     * 创建一个熔炉冶炼配方.
      * <p>
      * 原文:Create a furnace recipe to craft the specified ItemStack.
      *
-     * @param result 填入该冶炼公式产生的生成物.
+     * @param result 填入该冶炼配方产生的生成物.
      * @param source 冶炼原料.
      */
     public FurnaceRecipe(ItemStack result, MaterialData source) {
@@ -38,11 +38,11 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 创建一个熔炉冶炼公式.
+     * 创建一个熔炉冶炼配方.
      *
-     * @param result 填入该冶炼公式产生的生成物.
+     * @param result 填入该冶炼配方产生的生成物.
      * @param source 冶炼原料.
-     * @param experience 冶炼公式所需经验
+     * @param experience 熔炼后玩家所得经验
      */
     public FurnaceRecipe(ItemStack result, MaterialData source, float experience) {
         this(result, source.getItemType(), source.getData(), experience);
@@ -53,10 +53,9 @@ public class FurnaceRecipe implements Recipe {
      * <p>
      * 原文:Create a furnace recipe to craft the specified ItemStack.
      *
-     * @param result 填入该冶炼公式产生的生成物.
+     * @param result 填入该冶炼配方产生的生成物.
      * @param source 冶炼原料.
-     * @param data The data value. (Note: This is currently ignored by the
-     *     CraftBukkit server.)
+     * @param data 数据值. (注意: 该值目前被CraftBukkit服务器忽略，也就是说没有作用)
      * @deprecated 不安全的参数
      */
     @Deprecated
@@ -65,13 +64,12 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 创建一个熔炉冶炼公式.
+     * 创建一个熔炉冶炼配方.
      *
      * @param result 填入该冶炼公式产生的生成物.
      * @param source 冶炼原料.
-     * @param data The data value. (Note: This is currently ignored by the
-     *     CraftBukkit server.)
-     * @param experience 冶炼公式所需经验
+     * @param data 数据值. (注意: 该值目前被CraftBukkit服务器忽略，也就是说没有作用)
+     * @param experience 冶炼后玩家所得经验
      * @deprecated 不安全的参数
      */
     @Deprecated
@@ -82,38 +80,37 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 设置此冶炼公式的原料.
+     * 设置此冶炼配方的原料.
      * <p>
      * 原文:Sets the input of this furnace recipe.
      *
      * @param input 冶炼原料.
-     * @return 返回新的冶炼公式.
+     * @return 返回新的冶炼配方.
      */
     public FurnaceRecipe setInput(MaterialData input) {
         return setInput(input.getItemType(), input.getData());
     }
 
     /**
-     * 设置此冶炼公式的原料.
+     * 设置此冶炼配方的原料.
      * <p>
      * 原文:Sets the input of this furnace recipe.
      *
      * @param input 冶炼原料.
-     * @return 返回新的冶炼公式.
+     * @return 返回新的冶炼配方
      */
     public FurnaceRecipe setInput(Material input) {
         return setInput(input, 0);
     }
 
     /**
-     * 设置此冶炼公式的原料.
+     * 设置此冶炼配方的原料.
      * <p>
      * 原文:Sets the input of this furnace recipe.
      *
      * @param input 冶炼原料.
-     * @param data The data value. (Note: This is currently ignored by the
-     *     CraftBukkit server.)
-     * @return 返回新的冶炼公式.
+     * @param data 数据值. (注意: 该值目前被CraftBukkit服务器忽略，也就是说没有作用)
+     * @return 返回新的冶炼配方.
      * @deprecated Magic value
      */
     @Deprecated
@@ -123,7 +120,7 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 获取该冶炼公式的冶炼原料.
+     * 获取该冶炼配方的冶炼原料.
      * <p>
      * 原文:Get the input material.
      *
@@ -134,29 +131,33 @@ public class FurnaceRecipe implements Recipe {
     }
 
     /**
-     * 获取该冶炼公式的冶炼生成物Item.
+     * 获取该冶炼配方的冶炼生成物.
      * <p>
      * 原料:Get the result of this recipe.
      *
-     * @return 冶炼生成物Item.
+     * @return 冶炼生成物
      */
     public ItemStack getResult() {
         return output.clone();
     }
 
     /**
-     * Sets the experience given by this recipe.
+     * 设置烧炼后给予玩家的经验.
+     * <p>
+     * 原文:Sets the experience given by this recipe.
      *
-     * @param experience the experience level
+     * @param experience 经验值
      */
     public void setExperience(float experience) {
         this.experience = experience;
     }
 
     /**
-     * Get the experience given by this recipe.
+     * 获得烧炼后给予玩家的经验.
+     * <p>
+     * 原文:Get the experience given by this recipe.
      *
-     * @return experience level
+     * @return 经验值
      */
     public float getExperience() {
         return experience;
