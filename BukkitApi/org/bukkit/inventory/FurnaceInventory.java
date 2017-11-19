55d14993c000acf3228979265425c0c9c8440443
hcrgm <hcrgm@qq.com>
2017-11-19 02:11:13 -0600

    每月更新

--- BukkitApi/org/bukkit/inventory/FurnaceInventory.java
+++ BukkitApi/org/bukkit/inventory/FurnaceInventory.java
@@ -3,59 +3,59 @@ package org.bukkit.inventory;
 import org.bukkit.block.Furnace;
 
 /**
- * 熔炉的.用户界面接口
+ * 熔炉用户界面接口
  */
 public interface FurnaceInventory extends Inventory {
 
     /**
-     * 获取当前熔炉生成的的Item项目.
+     * 获取当前熔炉生成的的物品.
      * <p>
      * 原文:Get the current item in the result slot.
      *
-     * @return Item项目
+     * @return ItemStack
      */
     ItemStack getResult();
 
     /**
-     * 获取当前熔炉的燃料Item项目.
+     * 获取当前熔炉的燃料物品.
      *
-     * @return 燃料Item项目
+     * @return ItemStack
      */
     ItemStack getFuel();
 
     /**
-     * 获取当前熔炉的被冶炼Item项目?
+     * 获取当前熔炉的被冶炼的物品.
      * <p>
      * 原文:Get the item currently smelting.
      *
-     * @return Item项目
+     * @return 物品
      */
     ItemStack getSmelting();
 
     /**
-     * 设置当前熔炉的燃料Item项目.
+     * 设置当前熔炉的燃料物品.
      * <p>
      * 原文:Set the current fuel.
      *
-     * @param stack 燃料Item
+     * @param stack 燃料物品
      */
     void setFuel(ItemStack stack);
 
     /**
-     * 设置当前熔炉的生成物Item项目.
+     * 设置当前熔炉的生成物.
      * <p>
      * 原文:Set the current item in the result slot.
      *
-     * @param stack Item项目
+     * @param stack 生成物
      */
     void setResult(ItemStack stack);
 
     /**
-     * 设置当前熔炉的被冶炼物Item项目?
+     * 设置当前熔炉的被冶炼物物品.
      * <p>
      * 原文:Set the item currently smelting.
      *
-     * @param stack Item项目
+     * @param stack 物品
      */
     void setSmelting(ItemStack stack);
 
