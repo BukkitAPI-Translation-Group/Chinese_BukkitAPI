0b6e994362d3178ac7ea31534d5c6e7d6d2440c1
Zoyn <602723113@qq.com>
2018-02-18 13:52:02 +0800

    :memo:update: 更新 AnvilInventory, EntityEquipment 的doc

--- BukkitApi/org/bukkit/inventory/EntityEquipment.java
+++ BukkitApi/org/bukkit/inventory/EntityEquipment.java
@@ -3,321 +3,389 @@ package org.bukkit.inventory;
 import org.bukkit.entity.Entity;
 
 /**
- * An interface to a creatures inventory
+ * 生物的背包界面接口
  */
 public interface EntityEquipment {
 
     /**
-     * Gets a copy of the item the entity is currently holding
+     * 获取实体当前主手中手持的物品的拷贝
+     * <p>
+     * 原文: Gets a copy of the item the entity is currently holding
      * in their main hand.
      *
-     * @return the currently held item
+     * @return 当前主手手持的物品
      */
     ItemStack getItemInMainHand();
 
     /**
-     * Sets the item the entity is holding in their main hand.
+     * 设置实体在主手中手持的物品
+     * <p>
+     * 原文: Sets the item the entity is holding in their main hand.
      *
-     * @param item The item to put into the entities hand
+     * @param item 需要设置的物品
      */
     void setItemInMainHand(ItemStack item);
 
     /**
-     * Gets a copy of the item the entity is currently holding
+     * 获取实体当前副手中手持的物品的拷贝
+     * <p>
+     * 原文: Gets a copy of the item the entity is currently holding
      * in their off hand.
      *
-     * @return the currently held item
+     * @return 当前副手手持的物品
      */
     ItemStack getItemInOffHand();
 
     /**
-     * Sets the item the entity is holding in their off hand.
+     * 设置实体在副手中手持的物品
+     * <p>
+     * 原文: Sets the item the entity is holding in their off hand.
      *
-     * @param item The item to put into the entities hand
+     * @param item 需要设置的物品
      */
     void setItemInOffHand(ItemStack item);
 
     /**
-     * Gets a copy of the item the entity is currently holding
+     * 获取实体当前所持物品的拷贝
+     * <p>
+     * 译注: 反编译其实现, 发现其也是获取主手的物品
+     * <p>
+     * 原文: Gets a copy of the item the entity is currently holding
      *
-     * @deprecated entities can duel wield now use the methods for the
-     *      specific hand instead
+     * @return the currently held item
      * @see #getItemInMainHand()
      * @see #getItemInOffHand()
-     * @return the currently held item
+     * @deprecated 现在实体可以双持, 所以需要特定的方法来获取指定的手
      */
     @Deprecated
     ItemStack getItemInHand();
 
     /**
-     * Sets the item the entity is holding
+     * 设置实体当前所持的物品
+     * <p>
+     * 译注: 反编译其实现, 发现其也是设置主手的物品
+     * <p>
+     * 原文: Sets the item the entity is holding
      *
-     * @deprecated entities can duel wield now use the methods for the
-     *      specific hand instead
+     * @param stack 需要设置的物品
      * @see #setItemInMainHand(ItemStack)
      * @see #setItemInOffHand(ItemStack)
-     * @param stack The item to put into the entities hand
+     * @deprecated 现在实体可以双持, 所以需要特定的方法来获取指定的手
      */
     @Deprecated
     void setItemInHand(ItemStack stack);
 
     /**
-     * Gets a copy of the helmet currently being worn by the entity
+     * 获取该实体当前正在佩戴的头盔的拷贝
+     * <p>
+     * 原文: Gets a copy of the helmet currently being worn by the entity
      *
-     * @return The helmet being worn
+     * @return 所佩戴的头盔
      */
     ItemStack getHelmet();
 
     /**
-     * Sets the helmet worn by the entity
+     * 设置该实体当前正在佩戴的头盔
+     * <p>
+     * 原文: Sets the helmet worn by the entity
      *
-     * @param helmet The helmet to put on the entity
+     * @param helmet 给定的头盔
      */
     void setHelmet(ItemStack helmet);
 
     /**
-     * Gets a copy of the chest plate currently being worn by the entity
+     * 获取该实体当前正在穿戴的胸甲的拷贝
+     * <p>
+     * 原文: Gets a copy of the chest plate currently being worn by the entity
      *
-     * @return The chest plate being worn
+     * @return 实体所穿戴的胸甲
      */
     ItemStack getChestplate();
 
     /**
-     * Sets the chest plate worn by the entity
+     * 设置该实体当前正在穿戴的胸甲
+     * <p>
+     * 原文: Sets the chest plate worn by the entity
      *
-     * @param chestplate The chest plate to put on the entity
+     * @param chestplate 给定的胸甲
      */
     void setChestplate(ItemStack chestplate);
 
     /**
-     * Gets a copy of the leggings currently being worn by the entity
+     * 获取该实体当前正在穿戴的护腿的拷贝
+     * <p>
+     * 原文: Gets a copy of the leggings currently being worn by the entity
      *
-     * @return The leggings being worn
+     * @return 实体所穿戴的护腿
      */
     ItemStack getLeggings();
 
     /**
-     * Sets the leggings worn by the entity
+     * 设置该实体当前正在穿戴的护腿
+     * <p>
+     * 原文: Sets the leggings worn by the entity
      *
-     * @param leggings The leggings to put on the entity
+     * @param leggings 给定的护腿
      */
     void setLeggings(ItemStack leggings);
 
     /**
-     * Gets a copy of the boots currently being worn by the entity
+     * 获取该实体当前正在穿戴的鞋子的拷贝
+     * <p>
+     * 原文: Gets a copy of the boots currently being worn by the entity
      *
-     * @return The boots being worn
+     * @return 实体所穿戴的鞋子
      */
     ItemStack getBoots();
 
     /**
-     * Sets the boots worn by the entity
+     * 设置该实体当前正在穿戴的鞋子
+     * <p>
+     * 原文: Sets the boots worn by the entity
      *
-     * @param boots The boots to put on the entity
+     * @param boots 给定的鞋子
      */
     void setBoots(ItemStack boots);
 
     /**
-     * Gets a copy of all worn armor
+     * 获得实体当前所有穿着盔甲的拷贝
+     * <p>
+     * 译注: 其顺序为  鞋子、护腿、胸甲、头盔
+     * <p>
+     * 原文: Gets a copy of all worn armor
      *
-     * @return The array of worn armor
+     * @return 一个关于实体当前所有穿着的盔甲的数组
      */
     ItemStack[] getArmorContents();
 
     /**
-     * Sets the entities armor to the provided array of ItemStacks
+     * 将实体所穿戴的盔甲设置为提供的ItemStacks数组
+     * <p>
+     * 译注:
+     * <ul>
+     * <li>该ItemStack数组的顺序应为  鞋子、护腿、胸甲、头盔
+     * <li>当数组长度不够4个时, 将会把剩下的设置为null, 如为2时,胸甲和头盔将会自动设置为null
+     * </ul>
+     * <p>
+     * 原文: Sets the entities armor to the provided array of ItemStacks
      *
-     * @param items The items to set the armor as
+     * @param items 给定的盔甲数组
      */
     void setArmorContents(ItemStack[] items);
 
     /**
-     * Clears the entity of all armor and held items
+     * 清除实体所有的盔甲和手持的物品
+     * <p>
+     * 原文: Clears the entity of all armor and held items
      */
     void clear();
 
     /**
-     * @deprecated entities can duel wield now use the methods for the specific
-     * hand instead
+     * @return 掉落的几率
      * @see #getItemInMainHandDropChance()
      * @see #getItemInOffHandDropChance()
-     * @return drop chance
+     * @deprecated 现在实体可以双持, 所以需要特定的方法来设置指定的手
      */
     @Deprecated
     float getItemInHandDropChance();
 
     /**
-     * @deprecated entities can duel wield now use the methods for the specific
-     * hand instead
+     * @param chance 给定的掉落几率
      * @see #setItemInMainHandDropChance(float)
      * @see #setItemInOffHandDropChance(float)
-     * @param chance drop chance
+     * @deprecated 现在实体可以双持, 所以需要特定的方法来设置指定的手
      */
     @Deprecated
     void setItemInHandDropChance(float chance);
 
     /**
-     * Gets the chance of the main hand item being dropped upon this creature's
+     * 获得该生物在死亡时掉落其主手的物品的几率
+     * <p>
+     * 原文: Gets the chance of the main hand item being dropped upon this creature's
      * death.
-     *
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return chance of the currently held item being dropped (1 for players)
+     * @return 当前生物在死亡时掉落主手物品的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getItemInMainHandDropChance();
 
     /**
-     * Sets the chance of the item this creature is currently holding in their
+     * 设置该生物在死亡时掉落其主手的物品的几率
+     * <p>
+     * 原文: Sets the chance of the item this creature is currently holding in their
      * main hand being dropped upon this creature's death.
-     *
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance the chance of the main hand item being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 主手掉落物品的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setItemInMainHandDropChance(float chance);
 
     /**
-     * Gets the chance of the off hand item being dropped upon this creature's
+     * 获得该生物在死亡时掉落其副手的物品的几率
+     * <p>
+     * 原文: Gets the chance of the off hand item being dropped upon this creature's
      * death.
-     *
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return chance of the off hand item being dropped (1 for players)
+     * @return 当前生物在死亡时掉落副手物品的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getItemInOffHandDropChance();
 
     /**
-     * Sets the chance of the off hand item being dropped upon this creature's
+     * 设置该生物在死亡时掉落其副手的物品的几率
+     * <p>
+     * 原文: Sets the chance of the off hand item being dropped upon this creature's
      * death.
-     *
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance the chance of off hand item being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 副手掉落物品的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setItemInOffHandDropChance(float chance);
 
     /**
-     * Gets the chance of the helmet being dropped upon this creature's death.
-     *
+     * 获得该生物在死亡时掉落其头盔的几率
+     * <p>
+     * 原文: Gets the chance of the helmet being dropped upon this creature's death.
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return the chance of the helmet being dropped (1 for players)
+     * @return 当前生物在死亡时掉落头盔的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getHelmetDropChance();
 
     /**
-     * Sets the chance of the helmet being dropped upon this creature's death.
-     * 
+     * 设置该生物在死亡时掉落其头盔的几率
+     * <p>
+     * 原文: Sets the chance of the helmet being dropped upon this creature's death.
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance of the helmet being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 掉落头盔的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setHelmetDropChance(float chance);
 
     /**
-     * Gets the chance of the chest plate being dropped upon this creature's
+     * 获得该生物在死亡时掉落其胸甲的几率
+     * <p>
+     * 原文: Gets the chance of the chest plate being dropped upon this creature's
      * death.
-     * 
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return the chance of the chest plate being dropped (1 for players)
+     * @return 当前生物在死亡时掉落胸甲的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getChestplateDropChance();
 
     /**
-     * Sets the chance of the chest plate being dropped upon this creature's
+     * 设置该生物在死亡时掉落其胸甲的几率
+     * <p>
+     * 原文: Sets the chance of the chest plate being dropped upon this creature's
      * death.
-     * 
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance of the chest plate being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 掉落胸甲的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setChestplateDropChance(float chance);
 
     /**
-     * Gets the chance of the leggings being dropped upon this creature's
+     * 获得该生物在死亡时掉落其护腿的几率
+     * <p>
+     * 原文: Gets the chance of the leggings being dropped upon this creature's
      * death.
-     * 
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return the chance of the leggings being dropped (1 for players)
+     * @return 当前生物在死亡时掉落护腿的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getLeggingsDropChance();
 
     /**
-     * Sets the chance of the leggings being dropped upon this creature's
+     * 设置该生物在死亡时掉落其护腿的几率
+     * <p>
+     * 原文: Sets the chance of the leggings being dropped upon this creature's
      * death.
-     * 
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance chance of the leggings being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 掉落护腿的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setLeggingsDropChance(float chance);
 
     /**
-     * Gets the chance of the boots being dropped upon this creature's death.
-     * 
+     * 获得该生物在死亡时掉落其鞋子的几率
+     * <p>
+     * 原文: Gets the chance of the boots being dropped upon this creature's death.
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @return the chance of the boots being dropped (1 for players)
+     * @return 当前生物在死亡时掉落鞋子的几率(在原版里, 对于玩家来说是1.0F)
      */
     float getBootsDropChance();
 
     /**
-     * Sets the chance of the boots being dropped upon this creature's death.
-     * 
+     * 设置该生物在死亡时掉落其鞋子的几率
+     * <p>
+     * 原文: Sets the chance of the boots being dropped upon this creature's death.
+     * <p>
      * <ul>
-     * <li>A drop chance of 0.0F will never drop
-     * <li>A drop chance of 1.0F will always drop
+     * <li>当掉落几率为0.0F时, 则永远不会掉落
+     * <li>当掉落几率为1.0F时, 则总是会掉落
      * </ul>
      *
-     * @param chance of the boots being dropped
-     * @throws UnsupportedOperationException when called on players
+     * @param chance 掉落鞋子的几率
+     * @throws UnsupportedOperationException 当这一操作作用在玩家时抛出此异常
      */
     void setBootsDropChance(float chance);
 
     /**
-     * Get the entity this EntityEquipment belongs to
+     * 获取此EntityEquipment所属的实体
+     * <p>
+     * 原文: Get the entity this EntityEquipment belongs to
      *
-     * @return the entity this EntityEquipment belongs to
+     * @return 这个EntityEquipment所属的实体
      */
     Entity getHolder();
 }
