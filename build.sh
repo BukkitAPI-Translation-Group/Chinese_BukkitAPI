e16056e5e411ae16c4e1b2517b2ce1f809af1aae
hcrgm <hcrgm@qq.com>
2018-02-19 08:57:59 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ if [ -d "target" ]; then
 fi
 echo "Building javadoc..."
 mkdir target
-javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi:javadoc" -d target -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.12-R0.1-SNAPSHOT API 中文文档" -windowtitle "Bukkit 1.12-R0.1-SNAPSHOT API 中文文档" -subpackages org.bukkit
+javadoc -locale zh_CN -charset utf-8 -encoding utf-8 -docencoding utf-8 -sourcepath "BukkitApi:javadoc" -d target -stylesheetfile javadoc/stylesheet.css -overview javadoc/overview.html -docfilessubdirs -use -doctitle "Bukkit 1.12.2-R0.1-SNAPSHOT API 中文文档" -windowtitle "Bukkit 1.12.2-R0.1-SNAPSHOT API 中文文档" -subpackages org.bukkit
 cp javadoc/script.js target/
 echo 
 echo "BUILD COMPLETED"
