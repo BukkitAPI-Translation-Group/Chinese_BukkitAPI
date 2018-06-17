19cd8a3ff781e01cacb4d2ad905100103b7c3ce5
小灰灰 <1025184872@qq.com>
2018-06-17 19:37:09 +0800

    Update SimplexOctaveGenerator.java

--- BukkitApi/org/bukkit/util/noise/SimplexOctaveGenerator.java
+++ BukkitApi/org/bukkit/util/noise/SimplexOctaveGenerator.java
@@ -2,16 +2,16 @@ package org.bukkit.util.noise;
 import java.util.Random;
 import org.bukkit.World;
 /**
- * 使用倍频创建单纯形噪声。
+ * 使用倍频创建单纯形噪声.
  * <p>
- * 原文：Creates simplex noise through unbiased octaves
+ * 原文:Creates simplex noise through unbiased octaves
  */
 public class SimplexOctaveGenerator extends OctaveGenerator {
     private double wScale = 1;
     /**
-     * 使用给定的World创建一个SimplexOctaveGenerator。
+     * 使用给定的World创建一个SimplexOctaveGenerator.
      * <p>
-     * 原文：Creates a simplex octave generator for the given world
+     * 原文:Creates a simplex octave generator for the given world
      *
      * @param world 创建这个Generator的World实例
      * @param octaves 创建的倍频值
@@ -20,11 +20,11 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         this(new Random(world.getSeed()), octaves);
     }
     /**
-     * 使用给定的种子创建一个SimplexOctaveGenerator。
+     * 使用给定的种子创建一个SimplexOctaveGenerator.
      * <p>
-     * 原文：Creates a simplex octave generator for the given world
+     * 原文:Creates a simplex octave generator for the given world
      * <p>
-     * 译注:根据参数的翻译 这里given world疑似打错应该是given seed。
+     * 译注:根据参数的翻译 这里given world疑似打错应该是given seed.
      *
      * @param seed 创建这个Generator的long型种子
      * @param octaves 创建的倍频值
@@ -33,9 +33,9 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         this(new Random(seed), octaves);
     }
     /**
-     * 使用给定的{@link Random}创建一个SimplexOctaveGenerator。
+     * 使用给定的{@link Random}创建一个SimplexOctaveGenerator.
      * <p>
-     * 原文：Creates a simplex octave generator for the given {@link Random}
+     * 原文:Creates a simplex octave generator for the given {@link Random}
      *
      * @param rand 创建这个Generator的Random
      * @param octaves 创建的倍频值
@@ -49,9 +49,9 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         setWScale(scale);
     }
     /**
-     * 获取每个W坐标通过的区间大小。
+     * 获取每个W坐标通过的区间大小.
      * <p>
-     * 原文：Gets the scale used for each W-coordinates passed
+     * 原文:Gets the scale used for each W-coordinates passed
      *
      * @return W区间大小
      */
@@ -59,9 +59,9 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         return wScale;
     }
     /**
-     * 设置每个W坐标通过的区间大小。
+     * 设置每个W坐标通过的区间大小.
      * <p>
-     * 原文：Sets the scale used for each W-coordinates passed
+     * 原文:Sets the scale used for each W-coordinates passed
      *
      * @param scale 新W区间大小
      */
@@ -69,12 +69,12 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         wScale = scale;
     }
     /**
-     * 使用特殊的数个音阶和参数产生一个三维坐标的噪音。
+     * 使用特殊的数个音阶和参数产生一个三维坐标的噪音.
      * <p>
-     * 原文：Generates noise for the 3D coordinates using the specified number of
+     * 原文:Generates noise for the 3D coordinates using the specified number of
      * octaves and parameters
      * <p>
-     * 译注:根据参数的翻译 这里3D疑似打错应该是4D。
+     * 译注:根据参数的翻译 这里3D疑似打错应该是4D.
      *
      * @param x X坐标
      * @param y Y坐标
@@ -88,12 +88,12 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
         return noise(x, y, z, w, frequency, amplitude, false);
     }
     /**
-     * 使用特殊的数个音阶和参数产生一个三维坐标的噪音。
+     * 使用特殊的数个音阶和参数产生一个三维坐标的噪音.
      * <p>
-     * 原文：Generates noise for the 3D coordinates using the specified number of
+     * 原文:Generates noise for the 3D coordinates using the specified number of
      * octaves and parameters
      * <p>
-     * 译注:根据参数的翻译 这里3D疑似打错应该是4D。
+     * 译注:根据参数的翻译 这里3D疑似打错应该是4D.
      *
      * @param x X坐标
      * @param y Y坐标
@@ -101,7 +101,7 @@ public class SimplexOctaveGenerator extends OctaveGenerator {
      * @param w W坐标
      * @param frequency 每一个音阶改变多少频率
      * @param amplitude 每一个音阶改变多少振幅
-     * @param normalized 如果为true，规范这个值为[-1, 1]
+     * @param normalized 如果为true,规范这个值为[-1, 1]
      * @return 噪音结果
      */
     public double noise(double x, double y, double z, double w, double frequency, double amplitude, boolean normalized) {
