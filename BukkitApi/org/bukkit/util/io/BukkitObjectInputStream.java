4399744db4cb60e04be6bacaea1498930c768566
小灰灰 <1025184872@qq.com>
2018-06-17 09:40:34 +0800

    Update BukkitObjectInputStream.java

--- BukkitApi/org/bukkit/util/io/BukkitObjectInputStream.java
+++ BukkitApi/org/bukkit/util/io/BukkitObjectInputStream.java
@@ -5,26 +5,26 @@ import java.io.ObjectInputStream;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 /**
- * 这个类旨在被用于连接到{@link ConfigurationSerializable}的API。
- * 它把对象转换回它们在被{@link BukkitObjectInputStream}序列化之后的原始实现。
+ * 这个类旨在被用于连接到{@link ConfigurationSerializable}的API.
+ * 它把对象转换回它们在被{@link BukkitObjectInputStream}序列化之后的原始实现.
  * <p>
- * 原文：This class is designed to be used in conjunction with the {@link
+ * 原文:This class is designed to be used in conjunction with the {@link
  * ConfigurationSerializable} API. It translates objects back to their
  * original implementation after being serialized by {@link
  * BukkitObjectInputStream}.
  * <p>
- * 直接继承实现这个类的语法可能在未来版本被取消。
+ * 直接继承实现这个类的语法可能在未来版本被取消.
  * <p>
- * 原文：Behavior of implementations extending this class is not guaranteed across
+ * 原文:Behavior of implementations extending this class is not guaranteed across
  * future versions.
  */
 public class BukkitObjectInputStream extends ObjectInputStream {
     /**
-     * 构造函数提供镜像连接父类的功能。
+     * 构造函数提供镜像连接父类的功能.
      * <p>
-     * 原文：Constructor provided to mirror super functionality.
+     * 原文:Constructor provided to mirror super functionality.
      *
-     * @throws IOException 如果一个I/O连接在阅读数据流的头数据时发生错误则抛出
+     * @throws IOException 如果一个I/O连接在从数据流读取头数据时发生错误则抛出
      * @see ObjectInputStream#ObjectInputStream()
      */
     protected BukkitObjectInputStream() throws IOException, SecurityException {
@@ -32,12 +32,12 @@ public class BukkitObjectInputStream extends ObjectInputStream {
         super.enableResolveObject(true);
     }
     /**
-     * 输入流作为参数的构造函数
+     * 输入流作为参数的构造函数.
      * <p>
-     * 原文：Object input stream decoration constructor.
+     * 原文:Object input stream decoration constructor.
      *
      * @param in InputStream对象
-     * @throws 如果一个I/O连接在阅读输入流的头数据时发生错误则抛出
+     * @throws 如果一个I/O连接在从数据流读取头数据时发生错误则抛出
      * @see ObjectInputStream#ObjectInputStream(InputStream)
      */
     public BukkitObjectInputStream(InputStream in) throws IOException {
