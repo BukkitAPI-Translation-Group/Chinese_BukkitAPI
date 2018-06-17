11d5c7205778e756730eca37be3908a49b924afd
小灰灰 <1025184872@qq.com>
2018-06-17 09:42:36 +0800

    Update BukkitObjectOutputStream.java

--- BukkitApi/org/bukkit/util/io/BukkitObjectOutputStream.java
+++ BukkitApi/org/bukkit/util/io/BukkitObjectOutputStream.java
@@ -5,26 +5,26 @@ import java.io.OutputStream;
 import java.io.Serializable;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
 /**
- * 这个类旨在被用于连接到{@link ConfigurationSerializable}的API。
- * 它把对象转化为一个内部的实现，为了之后反序列化使用{@link BukkitObjectInputStream}。
+ * 这个类旨在被用于连接到{@link ConfigurationSerializable}的API.
+ * 它把对象转化为一个内部的实现，为了之后反序列化使用{@link BukkitObjectInputStream}.
  * <p>
  * 原文：This class is designed to be used in conjunction with the {@link
  * ConfigurationSerializable} API. It translates objects to an internal
  * implementation for later deserialization using {@link
  * BukkitObjectInputStream}.
  * <p>
- * 直接继承实现这个类的语法可能在未来版本被取消。
+ * 直接继承实现这个类的语法可能在未来版本被取消.
  * <p>
- * 原文：Behavior of implementations extending this class is not guaranteed across
+ * 原文:Behavior of implementations extending this class is not guaranteed across
  * future versions.
  */
 public class BukkitObjectOutputStream extends ObjectOutputStream {
     /**
-     * 构造函数提供镜像连接父类的功能。
+     * 构造函数提供镜像连接父类的功能.
      * <p>
-     * 原文：Constructor provided to mirror super functionality.
+     * 原文:Constructor provided to mirror super functionality.
      *
-     * @throws IOException 如果一个I/O连接在写出数据流的头数据时发生错误则抛出
+     * @throws IOException 如果一个I/O连接从数据流写出头数据时发生错误则抛出
      * @see ObjectOutputStream#ObjectOutputStream()
      */
     protected BukkitObjectOutputStream() throws IOException, SecurityException {
@@ -32,12 +32,12 @@ public class BukkitObjectOutputStream extends ObjectOutputStream {
         super.enableReplaceObject(true);
     }
     /**
-     * 输出流作为参数的构造函数
+     * 输出流作为参数的构造函数.
      * <p>
-     * 原文：Object output stream decoration constructor.
+     * 原文:Object output stream decoration constructor.
      *
      * @param out OutputStream对象
-     * @throws IOException 如果一个I/O连接在写出输出流的头数据时发生错误则抛出
+     * @throws IOException 如果一个I/O连接从数据流写出头数据时发生错误则抛出
      * @see ObjectOutputStream#ObjectOutputStream(OutputStream)
      */
     public BukkitObjectOutputStream(OutputStream out) throws IOException {
