eb9f765770be3788e94b9ff68a739548d380e192
小灰灰 <1025184872@qq.com>
2018-06-17 09:27:43 +0800

    Update Consumer.java

--- BukkitApi/org/bukkit/util/Consumer.java
+++ BukkitApi/org/bukkit/util/Consumer.java
@@ -1,17 +1,17 @@
 package org.bukkit.util;
 
 /**
- * 代表一个消费者的操作，它接受单独的输入参数却没有返回值
+ * 表示一个接受单独的输入参数并没有返回值的操作.
  * <p>
- * 原文：Represents an operation that accepts a single input argument and returns no
+ * 原文:Represents an operation that accepts a single input argument and returns no
  * result.
  *
- * @param <T> 输入这个操作的类型
+ * @param <T> 输入参数类型
  */
 public interface Consumer<T> {
 
     /**
-     * 通过给定的参数执行操作。
+     * 使用给定的参数执行操作.
      * <p>
      * 原文：Performs this operation on the given argument.
      *
