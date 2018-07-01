9d20682f447f3a625794cb65bb7ee8c230c425a9
MineYuanlu <38997444+MineYuanlu@users.noreply.github.com>
2018-07-01 12:35:07 +0800

    翻译了Arrow实体

--- BukkitApi/org/bukkit/entity/Arrow.java
+++ BukkitApi/org/bukkit/entity/Arrow.java
@@ -1,73 +1,94 @@
 package org.bukkit.entity;
 
 /**
- * Represents an arrow.
+ * 表示箭矢.
+ * <p>
+ * 原文: Represents an arrow.
  */
 public interface Arrow extends Projectile {
 
     /**
-     * Gets the knockback strength for an arrow, which is the
+     * 获取箭头的击退强度,这是射箭的{@link org.bukkit.enchantments.Enchantment#KNOCKBACK 力量}等级.
+     * <p>
+     * 原文:Gets the knockback strength for an arrow, which is the
      * {@link org.bukkit.enchantments.Enchantment#KNOCKBACK KnockBack} level
      * of the bow that shot it.
      *
-     * @return the knockback strength value
+     * @return 击退强度值
      */
     public int getKnockbackStrength();
 
     /**
-     * Sets the knockback strength for an arrow.
+     * 设置箭矢的击退强度.
+     * <p>
+     * 原文: Sets the knockback strength for an arrow.
      *
-     * @param knockbackStrength the knockback strength value
+     * @param knockbackStrength 击退强度值
      */
     public void setKnockbackStrength(int knockbackStrength);
 
     /**
-     * Gets whether this arrow is critical.
+     * 获取此箭头是否重要.</br>
+     * 重要的箭增加了伤害并产生粒子效果.</br>
+     * 重要的箭通常发生在玩家在射箭前拉满弓.
      * <p>
-     * Critical arrows have increased damage and cause particle effects.
-     * <p>
-     * Critical arrows generally occur when a player fully draws a bow before
-     * firing.
+     * 原文: Gets whether this arrow is critical.</br>
+     * Critical arrows have increased damage and cause particle effects.</br>
+     * Critical arrows generally occur when a player fully draws a bow before firing.
      *
-     * @return true if it is critical
+     * @return 如果是重要的就返回true
      */
     public boolean isCritical();
 
     /**
-     * Sets whether or not this arrow should be critical.
+     * 设置这个箭矢是否应该是重要的.
+     * <p>
+     * 原文:Sets whether or not this arrow should be critical.
      *
-     * @param critical whether or not it should be critical
+     * @param critical 它是否应该是重要的
      */
     public void setCritical(boolean critical);
 
     /**
-     * Gets the current pickup status of this arrow.
+     * 获取此箭矢的当前拾取状态.
+     * <p>
+     * 原文:Gets the current pickup status of this arrow.
      *
-     * @return the pickup status of this arrow.
+     * @return 此箭头的拾取状态
      */
     public PickupStatus getPickupStatus();
 
     /**
-     * Sets the current pickup status of this arrow.
+     * 设置此箭矢的当前拾取状态.
+     * <p>
+     * 原文:Sets the current pickup status of this arrow.
      *
-     * @param status new pickup status of this arrow.
+     * @param status 此箭矢新的拾取状态
      */
     public void setPickupStatus(PickupStatus status);
 
     /**
-     * Represents the pickup status of this arrow.
+     * 表示此箭头的拾取状态 .
+     * <p>
+     * 原文:Represents the pickup status of this arrow.
      */
     public enum PickupStatus {
         /**
-         * The arrow cannot be picked up.
+         * 此箭矢不能被拾取. 
+         * <p>
+         * 原文: The arrow cannot be picked up.
          */
         DISALLOWED,
         /**
-         * The arrow can be picked up.
+         * 此箭矢能被拾取. 
+         * <p>
+         * 原文: The arrow can be picked up.
          */
         ALLOWED,
         /**
-         * The arrow can only be picked up by players in creative mode.
+         * 此箭矢只能被创造玩家拾取. 
+         * <p>
+         * 原文: The arrow can only be picked up by players in creative mode.
          */
         CREATIVE_ONLY
     }
