f2dce311d92b1d7f9b556cd6631e3e3abb7af0c8
hcrgm <hcrgm@qq.com>
2018-08-13 14:28:12 +0800

    Location重新翻译

--- BukkitApi/org/bukkit/Location.java
+++ BukkitApi/org/bukkit/Location.java
@@ -9,14 +9,15 @@ import org.bukkit.util.NumberConversions;
 import org.bukkit.util.Vector;
 
 /**
- * 世界中的三维位置,包含x,y,z
- * <p>
- * 原文:Represents a 3-dimensional position in a world.
+ * 世界中的三维位置,包含x,y,z.
  * <br>
- * No constraints are placed on any angular values other than that they be
- * specified in degrees. This means that negative angles or angles of greater
- * magnitude than 360 are valid, but may be normalized to any other equivalent
- * representation by the implementation.
+ * 除了角度制只能是以度数为单位的限制外, 不对任何角度值施加任何约束.
+ * 这意味着负的角度值或大于360°的角度值也是有效的, 但可以将它们转化为其它任何等效的表示.
+ * <br>
+ * 译注:yaw和pitch分别代表自转角(又称偏航角)、旋进角(又称进动角、俯仰角).
+ * 两个值一起表示位置的朝向(与向量的概念重合, 本类有很多方法就是与{@link org.bukkit.util.Vector}重合的).
+ * 这里的翻译分别取偏航角、俯仰角.另请参见util包下的Vector(向量)、EulerAngle(欧拉角), 阅读本文档及其相关文档
+ * 建议您具备高中数学必修4相关知识.
  */
 public class Location implements Cloneable, ConfigurationSerializable {
     private World world;
@@ -27,30 +28,30 @@ public class Location implements Cloneable, ConfigurationSerializable {
     private float yaw;
 
     /**
-     * 以给定的坐标获取一个Location对象.
+     * 以给定的坐标构造一个Location对象.
      * <p>
      * 原文:Constructs a new Location with the given coordinates
      *
-     * @param world 地点所在世界的名称
-     * @param x 地点的X轴坐标
-     * @param y 地点的Y轴坐标
-     * @param z 地点的Z轴坐标
+     * @param world 位置所在世界
+     * @param x 位置x轴坐标
+     * @param y 位置y轴坐标
+     * @param z 位置z轴坐标
      */
     public Location(final World world, final double x, final double y, final double z) {
         this(world, x, y, z, 0, 0);
     }
 
     /**
-     * 给定的坐标和Direction对象获取一个Location对象
+     * 以给定的坐标和朝向构造一个Location对象.
      * <p>
      * 原文:Constructs a new Location with the given coordinates and direction
      *
-     * @param world 地点所在世界的名称
-     * @param x 地点的X轴坐标
-     * @param y 地点的Y轴坐标
-     * @param z 地点的Z轴坐标
-     * @param yaw 在X轴平面上的旋转角度的绝对值,单位为角度制
-     * @param pitch 在Y轴平面上旋转角度的绝对值,单位为角度制
+     * @param world 位置所在世界
+     * @param x 位置x轴坐标
+     * @param y 位置y轴坐标
+     * @param z 位置z轴坐标
+     * @param yaw x轴平面上的绝对的旋转角度, 以度为单位
+     * @param pitch y轴平面上的绝对的旋转角度, 以度为单位
      */
     public Location(final World world, final double x, final double y, final double z, final float yaw, final float pitch) {
         this.world = world;
@@ -62,38 +63,40 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Sets the world that this location resides in
+     * 设置本位置所在的世界.
+     * <p>
+     * 原文:Sets the world that this location resides in
      *
-     * @param world New world that this location resides in
+     * @param world 位置所在的新世界
      */
     public void setWorld(World world) {
         this.world = world;
     }
 
     /**
-     * 获取该location对象所在的世界名.
+     * 获取本位置所在的世界.
      * <p>
      * 原文:Gets the world that this location resides in
      *
-     * @return 世界名
+     * @return 世界
      */
     public World getWorld() {
         return world;
     }
 
     /**
-     * 获取该Chunk的Location对象信息.
+     * 获取此位置位于哪一区块.
      * <p>
      * 原文:Gets the chunk at the represented location
      *
-     * @return 区块Location对象信息
+     * @return 本位置所在区块
      */
     public Chunk getChunk() {
         return world.getChunkAt(this);
     }
 
     /**
-     * 获取此位置对应的方块.
+     * 获取本位置对应的方块.
      * <p>
      * 原文:Gets the block at the represented location
      *
@@ -104,108 +107,120 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * 设置该Location对象中的X坐标数据.
+     * 设置本位置的x轴坐标.
      * <p>
      * 原文:Sets the x-coordinate of this location
      *
-     * @param x 需要设置的X轴坐标
+     * @param x x轴坐标
      */
     public void setX(double x) {
         this.x = x;
     }
 
     /**
-     * 获取该Location对象中的X坐标.
+     * 获取本位置的x轴坐标.
+     * <p>
+     * 原文:Gets the x-coordinate of this location
      *
-     * @return X坐标
+     * @return x轴坐标
      */
     public double getX() {
         return x;
     }
 
     /**
-     * 获取该方块的X轴坐标,以方块为单位?
+     * 获取向下取整的x轴坐标, 表示本位置包含的方块.
      * <p>
      * 原文:Gets the floored value of the X component, indicating the block that
-     * <p>
      * this location is contained with.
      *
-     * @return 方块X坐标
+     * @return 方块x轴坐标
      */
     public int getBlockX() {
         return locToBlock(x);
     }
 
     /**
-     * 设置该Location对象中的Y轴数据.
+     * 设置本位置的y轴坐标.
+     * <p>
+     * 原文:Sets the y-coordinate of this location
      *
-     * @param y 需要设置的Y轴坐标
+     * @param y y轴坐标
      */
     public void setY(double y) {
         this.y = y;
     }
 
     /**
-     * 获取该Location对象中的Y坐标.
+     * 获取本位置的y轴坐标.
+     * <p>
+     * 原文:Gets the y-coordinate of this location
      *
-     * @return Y坐标
+     * @return y轴坐标
      */
     public double getY() {
         return y;
     }
 
     /**
-     * 获取该方块的Y轴坐标,以方块为单位?
+     * 获取向下取整的y轴坐标, 表示本位置包含的方块.
      * <p>
      * 原文:Gets the floored value of the Y component, indicating the block that
-     * <p>
      * this location is contained with.
      *
-     * @return 方块的Y轴坐标
+     * @return 方块y轴坐标
      */
     public int getBlockY() {
         return locToBlock(y);
     }
 
     /**
-     * 设置该Location对象中的Z坐标数据
+     * 设置本位置的z坐标轴.
+     * <p>
+     * 原文:Sets the z-coordinate of this location
      *
-     * @param z 需要设置的Z轴坐标
+     * @param z z坐标轴
      */
     public void setZ(double z) {
         this.z = z;
     }
 
     /**
-     * 获取该Location对象中的Z轴坐标
+     * 获取本位置的z坐标轴.
+     * <p>
+     * 原文:Gets the z-coordinate of this location
      *
-     * @return Z坐标
+     * @return z坐标轴
      */
     public double getZ() {
         return z;
     }
 
     /**
-     * 获取该Location对象中的Z轴坐标,以方块为单位
+     * 获取向下取整的z轴坐标, 表示本位置包含的方块.
+     * <p>
+     * 原文:Gets the floored value of the Z component, indicating the block that
+     * this location is contained with.
      *
-     * @return 方块的Z轴坐标
+     * @return 方块z轴坐标
      */
     public int getBlockZ() {
         return locToBlock(z);
     }
 
     /**
-     * 设置该Location对象的在X轴平面上的旋转角度, 以度为单位
-     * <p>
-     * 原文: * Sets the yaw of this location, measured in degrees.
+     * 设置本位置的偏航角(yaw), 以度为单位.
      * <ul>
-     * <li>旋转值为0或360度表示正Z轴正方向
-     * <li>旋转值为180度表示正Z轴负方向
-     * <li>旋转值为90度表示正X轴负方向
-     * <li>旋转值为270度表示正X轴正方向
+     * <li>偏航角为0或360°时表示z轴正方向.
+     * <li>偏航角为180°时表示z轴负方向.
+     * <li>偏航角为90°时表示x轴负方向.
+     * <li>偏航角为270°时表示x轴正方向.
      * </ul>
+     * 增加偏航角值相当于使你转向右侧,
+     * 增加偏向下一个坐标轴的比例并减少偏向前一个轴的比例.
      * <p>
-     * 原文:<ul>
+     * 原文:Sets the yaw of this location, measured in degrees.
+     * <ul>
      * <li>A yaw of 0 or 360 represents the positive z direction.
      * <li>A yaw of 180 represents the negative z direction.
      * <li>A yaw of 90 represents the negative x direction.
@@ -215,14 +230,24 @@ public class Location implements Cloneable, ConfigurationSerializable {
      * right-facing, increasing the scale of the next respective axis, and
      * decreasing the scale of the previous axis.
      *
-     * @param yaw X轴旋转角度
+     * @param yaw 自转角(偏航角)
      */
     public void setYaw(float yaw) {
         this.yaw = yaw;
     }
 
     /**
-     * Gets the yaw of this location, measured in degrees.
+     * 获取本位置的偏航角(yaw), 以度为单位.
+     * <ul>
+     * <li>偏航角为0或360°时表示z轴正方向.
+     * <li>偏航角为180°时表示z轴负方向.
+     * <li>偏航角为90°时表示x轴负方向.
+     * <li>偏航角为270°时表示x轴正方向.
+     * </ul>
+     * 增加偏航角值相当于使你转向右侧,
+     * 增加偏向下一个坐标轴的比例并减少偏向前一个轴的比例.
+     * <p>
+     * 原文:Gets the yaw of this location, measured in degrees.
      * <ul>
      * <li>A yaw of 0 or 360 represents the positive z direction.
      * <li>A yaw of 180 represents the negative z direction.
@@ -233,14 +258,22 @@ public class Location implements Cloneable, ConfigurationSerializable {
      * right-facing, increasing the scale of the next respective axis, and
      * decreasing the scale of the previous axis.
      *
-     * @return the rotation's yaw
+     * @return 自转角(偏航角)
      */
     public float getYaw() {
         return yaw;
     }
 
     /**
-     * Sets the pitch of this location, measured in degrees.
+     * 设置本位置的俯仰角(pitch), 以度为单位.
+     * <ul>
+     * <li>俯仰角为0时表示水平朝向.
+     * <li>俯仰角为90°时表示向下的朝向,或y轴的负方向
+     * <li>俯仰角为-90°时表示向上的朝向,或y轴的正方向
+     * </ul>
+     * 增加俯仰角值相当于俯视的效果.
+     * <p>
+     * 原文:Sets the pitch of this location, measured in degrees.
      * <ul>
      * <li>A pitch of 0 represents level forward facing.
      * <li>A pitch of 90 represents downward facing, or negative y
@@ -249,14 +282,22 @@ public class Location implements Cloneable, ConfigurationSerializable {
      * </ul>
      * Increasing pitch values the equivalent of looking down.
      *
-     * @param pitch new incline's pitch
+     * @param pitch 倾斜角(俯仰角)
      */
     public void setPitch(float pitch) {
         this.pitch = pitch;
     }
 
     /**
-     * Gets the pitch of this location, measured in degrees.
+     * 获取本位置的俯仰角(pitch), 以度为单位.
+     * <ul>
+     * <li>俯仰角为0时表示水平朝向.
+     * <li>俯仰角为90°时表示向下的朝向,或y轴的负方向
+     * <li>俯仰角为-90°时表示向上的朝向,或y轴的正方向
+     * </ul>
+     * 增加俯仰角值相当于俯视的效果.
+     * <p>
+     * 原文:Gets the pitch of this location, measured in degrees.
      * <ul>
      * <li>A pitch of 0 represents level forward facing.
      * <li>A pitch of 90 represents downward facing, or negative y
@@ -265,18 +306,20 @@ public class Location implements Cloneable, ConfigurationSerializable {
      * </ul>
      * Increasing pitch values the equivalent of looking down.
      *
-     * @return the incline's pitch
+     * @return 倾斜角(俯仰角)
      */
     public float getPitch() {
         return pitch;
     }
 
     /**
-     * Gets a unit-vector pointing in the direction that this Location is
+     * 获取本位置所面向的方向的单位向量.
+     * <p>
+     * 原文:Gets a unit-vector pointing in the direction that this Location is
      * facing.
      *
-     * @return a vector pointing the direction of this location's {@link
-     *     #getPitch() pitch} and {@link #getYaw() yaw}
+     * @return 指向此位置的{@link
+     *     #getPitch() 偏航角}和{@link #getYaw() 俯仰角}的向量
      */
     public Vector getDirection() {
         Vector vector = new Vector();
@@ -295,11 +338,13 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Sets the {@link #getYaw() yaw} and {@link #getPitch() pitch} to point
+     * 将{@link #getYaw() 偏航角}和{@link #getPitch() 俯仰角}设为指向某一向量的方向.
+     * <p>
+     * 原文:Sets the {@link #getYaw() yaw} and {@link #getPitch() pitch} to point
      * in the direction of the vector.
      * 
-     * @param vector the direction vector
-     * @return the same location
+     * @param vector 方向向量
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location setDirection(Vector vector) {
         /*
@@ -331,12 +376,14 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Adds the location by another.
+     * 将本位置的坐标加上另一个位置的坐标.
+     * <p>
+     * 原文:Adds the location by another.
      *
      * @see Vector
-     * @param vec The other location
-     * @return the same location
-     * @throws IllegalArgumentException for differing worlds
+     * @param vec 另一个位置
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
+     * @throws IllegalArgumentException 若两个位置所处世界各不相同
      */
     public Location add(Location vec) {
         if (vec == null || vec.getWorld() != getWorld()) {
@@ -350,11 +397,13 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Adds the location by a vector.
+     * 将本位置的坐标加上一个向量的坐标.
+     * <p>
+     * 原文:Adds the location by a vector.
      *
      * @see Vector
-     * @param vec Vector to use
-     * @return the same location
+     * @param vec 向量
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location add(Vector vec) {
         this.x += vec.getX();
@@ -364,13 +413,15 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Adds the location by another. Not world-aware.
+     * 将本位置的坐标加上另一个位置的坐标. 与位置所处世界无关.
+     * <p>
+     * 原文:Adds the location by another. Not world-aware.
      *
      * @see Vector
-     * @param x X coordinate
-     * @param y Y coordinate
-     * @param z Z coordinate
-     * @return the same location
+     * @param x X坐标
+     * @param y Y坐标
+     * @param z Z坐标
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location add(double x, double y, double z) {
         this.x += x;
@@ -380,12 +431,14 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Subtracts the location by another.
+     * 将本位置的坐标减去另一个位置的坐标.
+     * <p>
+     * 原文:Subtracts the location by another.
      *
      * @see Vector
-     * @param vec The other location
-     * @return the same location
-     * @throws IllegalArgumentException for differing worlds
+     * @param vec 另一个位置
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
+     * @throws IllegalArgumentException 若两个位置所处世界各不相同
      */
     public Location subtract(Location vec) {
         if (vec == null || vec.getWorld() != getWorld()) {
@@ -399,11 +452,13 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Subtracts the location by a vector.
+     * 从本位置的坐标中减去向量的坐标.
+     * <p>
+     * 原文:Subtracts the location by a vector.
      *
      * @see Vector
-     * @param vec The vector to use
-     * @return the same location
+     * @param vec 向量
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location subtract(Vector vec) {
         this.x -= vec.getX();
@@ -413,7 +468,10 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Subtracts the location by another. Not world-aware and
+     * 从本位置的坐标中减去另一个位置的坐标.
+     * 与位置所处世界无关且其方向角独立不参与运算.
+     * <p>
+     * 原文:Subtracts the location by another. Not world-aware and
      * orientation independent.
      *
      * @see Vector
@@ -430,7 +488,13 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Gets the magnitude of the location, defined as sqrt(x^2+y^2+z^2). The
+     * 获取位置的模值,定义为 sqrt(x^2+y^2+z^2).
+     * 这个方法的返回值没有被缓存,且使用了开销较大的平方以及开根函数,
+     * 所以不要反复调用这个方法来获取向量的模值.
+     * 当向量的模过大时,开根函数有可能发生溢出,并会返回{@link Double#NaN}.
+     * 与位置所处世界无关且其方向角独立不参与运算.
+     * <p>
+     * 原文:Gets the magnitude of the location, defined as sqrt(x^2+y^2+z^2). The
      * value of this method is not cached and uses a costly square-root
      * function, so do not repeatedly call this method to get the location's
      * magnitude. NaN will be returned if the inner result of the sqrt()
@@ -438,25 +502,33 @@ public class Location implements Cloneable, ConfigurationSerializable {
      * world-aware and orientation independent.
      *
      * @see Vector
-     * @return the magnitude
+     * @return 模
      */
     public double length() {
         return Math.sqrt(NumberConversions.square(x) + NumberConversions.square(y) + NumberConversions.square(z));
     }
 
     /**
-     * Gets the magnitude of the location squared. Not world-aware and
+     * 获取位置长度(又称模)的平方 (三个坐标分别平方然后加起来).
+     * 与位置所处世界无关且其方向角独立不参与运算.
+     * <p>
+     * 原文:Gets the magnitude of the location squared. Not world-aware and
      * orientation independent.
      *
      * @see Vector
-     * @return the magnitude
+     * @return 模的平方
      */
     public double lengthSquared() {
         return NumberConversions.square(x) + NumberConversions.square(y) + NumberConversions.square(z);
     }
 
     /**
-     * Get the distance between this location and another. The value of this
+     * 获取本位置与与另一个位置之间的距离. 
+     * 这个方法的返回值没有被缓存,且使用了开销较大的平方以及开根函数,
+     * 所以不要反复调用这个方法来获取位置的模值.
+     * 当位置的距离过大时,开根函数有可能发生溢出,并会返回{@link Double#NaN}.
+     * <p>
+     * 原文:Get the distance between this location and another. The value of this
      * method is not cached and uses a costly square-root function, so do not
      * repeatedly call this method to get the location's magnitude. NaN will
      * be returned if the inner result of the sqrt() function overflows, which
@@ -472,12 +544,14 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Get the squared distance between this location and another.
+     * 获取本位置与与另一个位置之间的距离的平方.
+     * <p>
+     * 原文:Get the squared distance between this location and another.
      *
      * @see Vector
-     * @param o The other location
-     * @return the distance
-     * @throws IllegalArgumentException for differing worlds
+     * @param o 另一个位置
+     * @return 距离的平方
+     * @throws IllegalArgumentException 若两个位置所处世界各不相同
      */
     public double distanceSquared(Location o) {
         if (o == null) {
@@ -492,12 +566,14 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Performs scalar multiplication, multiplying all components with a
+     * 坐标数乘, 将所有坐标轴上扩展某个倍数.
+     * <p>
+     * 原文:Performs scalar multiplication, multiplying all components with a
      * scalar. Not world-aware.
      *
-     * @param m The factor
+     * @param m 因数,即数乘的倍数,整数
      * @see Vector
-     * @return the same location
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location multiply(double m) {
         x *= m;
@@ -507,10 +583,12 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Zero this location's components. Not world-aware.
+     * 清零此位置的xyz坐标数值. 与位置所处世界无关.
+     * <p>
+     * 原文:Zero this location's components. Not world-aware.
      *
      * @see Vector
-     * @return the same location
+     * @return 相同的位置对象(不是位置相同,但位置对象相同)
      */
     public Location zero() {
         x = 0;
@@ -569,10 +647,11 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Constructs a new {@link Vector} based on this Location
+     * 基于此位置构造一个新的{@link Vector 向量}.
+     * <p>
+     * 原文:Constructs a new {@link Vector} based on this Location
      *
-     * @return New Vector containing the coordinates represented by this
-     *     Location
+     * @return 包含该位置坐标的向量表示
      */
     public Vector toVector() {
         return new Vector(x, y, z);
@@ -588,9 +667,12 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Check if each component of this Location is finite.
+     * 检测该Location的xyz坐标和偏航角、俯仰角是否合法
+     * (限制的最大值为Double.MAX_VALUE).
+     * <p>
+     * 原文:Check if each component of this Location is finite.
      *
-     * @throws IllegalArgumentException if any component is not finite
+     * @throws IllegalArgumentException 若有任意一个坐标/角度值不合法时抛出
      */
     public void checkFinite() throws IllegalArgumentException {
         NumberConversions.checkFinite(x, "x not finite");
@@ -629,11 +711,13 @@ public class Location implements Cloneable, ConfigurationSerializable {
     }
 
     /**
-     * Required method for deserialization
+     * 实现配置序列化与反序列化所需方法.
+     * <p>
+     * 原文:Required method for deserialization
      *
-     * @param args map to deserialize
-     * @return deserialized location
-     * @throws IllegalArgumentException if the world don't exists
+     * @param args 需要反序列化的map
+     * @return 反序列化后的位置
+     * @throws IllegalArgumentException 如果位置所处世界不存在
      * @see ConfigurationSerializable
      */
     public static Location deserialize(Map<String, Object> args) {
