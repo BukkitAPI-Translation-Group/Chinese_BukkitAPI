b71397baf3bd65a74d3f743ce76be6e8b7bad87e
Zoyn <602723113@qq.com>
2018-09-08 07:48:25 +0800

    :memo:更新ExperienceOrb的文档内容

--- BukkitApi/org/bukkit/entity/ExperienceOrb.java
+++ BukkitApi/org/bukkit/entity/ExperienceOrb.java
@@ -1,21 +1,30 @@
 package org.bukkit.entity;
 
 /**
+ * 代表一个经验球.
+ * <p>
+ * 原文:
  * Represents an Experience Orb.
  */
 public interface ExperienceOrb extends Entity {
 
     /**
+     * 获取此经验球中所包含的经验
+     * <p>
+     * 原文:
      * Gets how much experience is contained within this orb
      *
-     * @return Amount of experience
+     * @return 经验的数量
      */
     public int getExperience();
 
     /**
+     * 设置此经验球中所包含的经验
+     * <p>
+     * 原文:
      * Sets how much experience is contained within this orb
      *
-     * @param value Amount of experience
+     * @param value 经验的数量
      */
     public void setExperience(int value);
 }
