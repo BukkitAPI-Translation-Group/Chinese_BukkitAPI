c8f00a509f49a2539210206354e403b1d3f30c4e
Zoyn <602723113@qq.com>
2018-09-08 07:34:30 +0800

    :memo:更新PlayerItemConsumeEvent的文档内容

--- BukkitApi/org/bukkit/event/player/PlayerItemConsumeEvent.java
+++ BukkitApi/org/bukkit/event/player/PlayerItemConsumeEvent.java
@@ -7,6 +7,13 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.inventory.ItemStack;
 
 /**
+ * 当玩家消耗完物品时, 此事件将触发 例如:(食物, 药水, 牛奶桶).
+ * <br>
+ * 如果修改了ItemStack，服务器将使用新ItemStack的效果，而不是从玩家的背包中删除原始ItemStack。
+ * <br>
+ * 如果事件被取消，则不会应用效果，并且不会从玩家的背包中删除该ItemStack。
+ * <p>
+ * 原文:
  * This event will fire when a player is finishing consuming an item (food,
  * potion, milk bucket).
  * <br>
@@ -22,8 +29,8 @@ public class PlayerItemConsumeEvent extends PlayerEvent implements Cancellable {
     private ItemStack item;
 
     /**
-     * @param player the player consuming
-     * @param item the ItemStack being consumed
+     * @param player 正在消耗物品的玩家
+     * @param item   将要被消耗掉的物品
      */
     public PlayerItemConsumeEvent(final Player player, final ItemStack item) {
         super(player);
@@ -32,20 +39,27 @@ public class PlayerItemConsumeEvent extends PlayerEvent implements Cancellable {
     }
 
     /**
+     * 获取正在被消耗掉的ItemStack.
+     * 修改本方法返回的ItemStack将无效，你必须使用 {@link #setItem(org.bukkit.inventory.ItemStack)} 方法来取代.
+     * <p>
+     * 原文:
      * Gets the item that is being consumed. Modifying the returned item will
      * have no effect, you must use {@link
      * #setItem(org.bukkit.inventory.ItemStack)} instead.
      *
-     * @return an ItemStack for the item being consumed
+     * @return 正在被消耗掉的 {@link ItemStack} .
      */
     public ItemStack getItem() {
         return item.clone();
     }
 
     /**
+     * 设置正在被消耗掉的ItemStack
+     * <p>
+     * 原文:
      * Set the item being consumed
      *
-     * @param item the item being consumed
+     * @param item 正在被消耗掉的ItemStack
      */
     public void setItem(ItemStack item) {
         if (item == null) {
