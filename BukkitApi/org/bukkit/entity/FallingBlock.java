a91ad153ce688805ed8df0946a4bedf3e054bc1c
cce <cce@E420-23>
2018-10-07 08:29:34 +0800

    FallingBlock 修改

--- BukkitApi/org/bukkit/entity/FallingBlock.java
+++ BukkitApi/org/bukkit/entity/FallingBlock.java
@@ -10,77 +10,77 @@ import org.bukkit.Material;
  */
 public interface FallingBlock extends Entity {
 
-	/**
-	 * 获取下落方块的材料. 
-	 * <p>
-	 * 原文:
-	 *  Get the Material of the falling block
-	 *
-	 * @return 方块材料
-	 */
-	Material getMaterial();
+    /**
+     * 获取下落方块的类型. 
+     * <p>
+     * 原文:
+     *  Get the Material of the falling block
+     *
+     * @return 方块材料. 
+     */
+    Material getMaterial();
 
-	/**
-	 * 获取下落方块的ID. 
-	 * <p>
-	 * 原文: 
-	 * Get the ID of the falling block
-	 *
-	 * @return 方块ID
-	 * @deprecated Magic value
-	 */
-	@Deprecated
-	int getBlockId();
+    /**
+     * 获取下落方块的ID. 
+     * <p>
+     * 原文: 
+     * Get the ID of the falling block
+     *
+     * @return 方块ID. 
+     * @deprecated Magic value
+     */
+    @Deprecated
+    int getBlockId();
 
-	/**
-	 * 获取下落方块的数据值
-	 * <p>
-	 * 原文: 
-	 * Get the data for the falling block
-	 *
-	 * @return 方块数据值
-	 * @deprecated Magic value
-	 */
-	@Deprecated
-	byte getBlockData();
+    /**
+     * 获取下落方块的数据值. 
+     * <p>
+     * 原文: 
+     * Get the data for the falling block
+     *
+     * @return 方块数据值. 
+     * @deprecated Magic value
+     */
+    @Deprecated
+    byte getBlockData();
 
-	/**
-	 * 获取下落方块被阻挡时是否会变为掉落物
-	 * <p>
-	 * 原文: 
-	 * Get if the falling block will break into an item if it cannot be placed
-	 *
-	 * @return 方块在被阻挡时是否会变为掉落物
-	 */
-	boolean getDropItem();
+    /**
+     * 获取下落方块被阻挡时是否会变为掉落物. 
+     * <p>
+     * 原文: 
+     * Get if the falling block will break into an item if it cannot be placed
+     *
+     * @return 方块在被阻挡时是否会变为掉落物. 
+     */
+    boolean getDropItem();
 
-	/**
-	 * 设置下落方块被阻挡时是否会变为掉落物
-	 * <p>
-	 * 原文: 
-	 * Set if the falling block will break into an item if it cannot be placed
-	 *
-	 * @param drop 当被阻挡时变为掉落物时返回 true
-	 */
-	void setDropItem(boolean drop);
+    /**
+     * 设置下落方块被阻挡时是否会变为掉落物. 
+     * <p>
+     * 原文: 
+     * Set if the falling block will break into an item if it cannot be placed
+     *
+     * @param drop 当被阻挡时变为掉落物时返回 true. 
+     */
+    void setDropItem(boolean drop);
 
-	/**
-	 * 获取此方块伤害实体的状态
-	 * <p>
-	 * 原文: 
-	 * Get the HurtEntities state of this block.
-	 *
-	 * @return 实体是否会被此方块伤害
-	 */
-	boolean canHurtEntities();
+    /**
+     * 获取此方块能否砸伤实体. 
+     * <p>
+     * 原文: 
+     * Get the HurtEntities state of this block.
+     *
+     * @return 此方块能否砸伤实体. 
+     */
+    boolean canHurtEntities();
 
-	/**
-	 * 设置此方块伤害实体的状态
-	 * <p>
-	 * 原文: 
-	 * Set the HurtEntities state of this block.
-	 *
-	 * @param hurtEntities 实体是否会被此方块伤害
-	 */
-	void setHurtEntities(boolean hurtEntities);
+    /**
+     * 设置此方块能否砸伤实体. 
+     * <p>
+     * 原文: 
+     * Set the HurtEntities state of this block.
+     *
+     * @param hurtEntities 此方块能否砸伤实体. 
+     */
+    void setHurtEntities(boolean hurtEntities);
 }
