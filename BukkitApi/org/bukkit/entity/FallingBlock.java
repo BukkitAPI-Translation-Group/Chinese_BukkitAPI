73768bb2f763eec028123b6565568ff897b0490b
MineYuanlu <38997444+MineYuanlu@users.noreply.github.com>
2018-11-04 22:44:35 +0800

    翻译 Entity& Fallingblock  (#18)
    * 本次翻译InvalidPluginException和Plugin中的信息
    
    均位于org.bukkit.plugin
    
    * 翻译了Arrow实体
    
    * 翻译了一些实体,并且按照大佬指示修改翻译
    
    * 按照大佬指示,修改文件
    
    * 改善翻译
    
    * 缩进符修改为空格
    
    * Update InvalidPluginException.java
    
    * FallingBlock汉化
    
    * 完成Entity类的翻译！
    
    * 完成下落方块的翻译
    
    * 按照大鸭梨的指示 修改翻译
    
    * 修改Entity和FallingBlock
    
    * FallingBlock 修改

--- BukkitApi/org/bukkit/entity/FallingBlock.java
+++ BukkitApi/org/bukkit/entity/FallingBlock.java
@@ -4,11 +4,17 @@ import org.bukkit.Material;
 import org.bukkit.block.data.BlockData;
 
 /**
- * Represents a falling block
+ * 代表一个下落方块. 
+ * <p>
+ * 原文:
+ *  Represents a falling block
  */
 public interface FallingBlock extends Entity {
 
     /**
+     * 获取下落方块的类型. 
+     * <p>
+     * 原文:
      * Get the Material of the falling block
      *
      * @return Material of the block
@@ -24,30 +30,42 @@ public interface FallingBlock extends Entity {
     BlockData getBlockData();
 
     /**
+     * 获取下落方块被阻挡时是否会变为掉落物. 
+     * <p>
+     * 原文: 
      * Get if the falling block will break into an item if it cannot be placed
      *
-     * @return true if the block will break into an item when obstructed
+     * @return 方块在被阻挡时是否会变为掉落物. 
      */
     boolean getDropItem();
 
     /**
+     * 设置下落方块被阻挡时是否会变为掉落物. 
+     * <p>
+     * 原文: 
      * Set if the falling block will break into an item if it cannot be placed
      *
-     * @param drop true to break into an item when obstructed
+     * @param drop 当被阻挡时变为掉落物时返回 true. 
      */
     void setDropItem(boolean drop);
 
     /**
+     * 获取此方块能否砸伤实体. 
+     * <p>
+     * 原文: 
      * Get the HurtEntities state of this block.
      *
-     * @return whether entities will be damaged by this block.
+     * @return 此方块能否砸伤实体. 
      */
     boolean canHurtEntities();
 
     /**
+     * 设置此方块能否砸伤实体. 
+     * <p>
+     * 原文: 
      * Set the HurtEntities state of this block.
      *
-     * @param hurtEntities whether entities will be damaged by this block.
+     * @param hurtEntities 此方块能否砸伤实体. 
      */
     void setHurtEntities(boolean hurtEntities);
 }
--- BukkitApi/org/bukkit/entity/Fish.java
+++ BukkitApi/org/bukkit/entity/Fish.java
@@ -1,6 +1,9 @@
 package org.bukkit.entity;
 
 /**
- * 代表鱼.
+ * 代表一个鱼钩
+ * <p>
+ * 原文:Represents a fishing hook.
+ * @deprecated 赞成使用 {@link FishHook}
  */
 public interface Fish extends WaterMob { }
