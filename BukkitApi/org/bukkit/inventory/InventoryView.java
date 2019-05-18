9d0f3b727ba6f1cb3aab681d6acde9c1a1f2694f
hcrgm <hcrgm@qq.com>
2019-05-18 21:53:50 +0800

    翻译了InventoryView
    终于诈尸更新了...(小声)

--- BukkitApi/org/bukkit/inventory/InventoryView.java
+++ BukkitApi/org/bukkit/inventory/InventoryView.java
@@ -4,7 +4,12 @@ import org.bukkit.entity.HumanEntity;
 import org.bukkit.event.inventory.InventoryType;
 
 /**
- * Represents a view linking two inventories and a single player (whose
+ * 代表连接两个物品栏(从UI上理解即为上下各一个)与单个玩家
+ * (其物品栏可能也可能不是视图代表的两个物品栏之一)的背包视图.
+ * <p>
+ * 注意: 如果你实现了这个接口却未能满足方法的约定 (未能完全实现既定的功能), 将不保证游戏能正常工作.
+ * <p>
+ * 原文:Represents a view linking two inventories and a single player (whose
  * inventory may or may not be one of the two).
  * <p>
  * Note: If you implement this interface but fail to satisfy the expected
@@ -14,27 +19,27 @@ import org.bukkit.event.inventory.InventoryType;
 public abstract class InventoryView {
     public final static int OUTSIDE = -999;
     /**
-     * Represents various extra properties of certain inventory windows.
+     * 代表物品栏窗口视图的一些额外属性.
      */
     public enum Property {
         /**
-         * The progress of the down-pointing arrow in a brewing inventory.
+         * 酿造台燃烧进度.
          */
         BREW_TIME(0, InventoryType.BREWING),
         /**
-         * The progress of the flame in a furnace inventory.
+         * 熔炉燃料已燃烧多久 (仅计算单个燃料).
          */
         BURN_TIME(0, InventoryType.FURNACE),
         /**
-         * How many total ticks the current fuel should last.
+         * 当前正燃烧的燃料还可以燃烧多久 (时间以tick为单位, 仅计算单个燃料).
          */
         TICKS_FOR_CURRENT_FUEL(1, InventoryType.FURNACE),
         /**
-         * The progress of the right-pointing arrow in a furnace inventory.
+         * 熔炉成品(通常是食物)的烧炼进度.
          */
         COOK_TIME(2, InventoryType.FURNACE),
         /**
-         * How many total ticks the current smelting should last.
+         * 熔炉剩余燃料还可以燃烧多久.
          */
         TICKS_FOR_CURRENT_SMELTING(3, InventoryType.FURNACE),
         /**
@@ -81,19 +86,19 @@ public abstract class InventoryView {
          */
         ENCHANT_LEVEL3(9, InventoryType.ENCHANTING),
         /**
-         * In an beacon inventory, the levels of the beacon
+         * 信标等级(信标总共有 4 个等级).
          */
         LEVELS(0, InventoryType.BEACON),
         /**
-         * In an beacon inventory, the primary potion effect
+         * 信标第一增益效果(药水效果).
          */
         PRIMARY_EFFECT(1, InventoryType.BEACON),
         /**
-         * In an beacon inventory, the secondary potion effect
+         * 信标第二增益效果(药水效果).
          */
         SECONDARY_EFFECT(2, InventoryType.BEACON),
         /**
-         * The repair's cost in xp levels
+         * 修理装备需花费的经验.
          */
         REPAIR_COST(0, InventoryType.ANVIL);
         int id;
@@ -109,8 +114,8 @@ public abstract class InventoryView {
 
         /**
          *
-         * @return the id of this view
-         * @deprecated Magic value
+         * @return 此窗口视图的id
+         * @deprecated 不安全的参数
          */
         @Deprecated
         public int getId() {
@@ -118,43 +123,57 @@ public abstract class InventoryView {
         }
     }
     /**
-     * Get the upper inventory involved in this transaction.
+     * 获取此窗口视图上方的物品栏.
+     * <p>
+     * 原文:Get the upper inventory involved in this transaction.
      *
-     * @return the inventory
+     * @return 物品栏
      */
     public abstract Inventory getTopInventory();
 
     /**
-     * Get the lower inventory involved in this transaction.
+     * 获取此窗口视图下方的物品栏 (通常是玩家背包).
+     * <p>
+     * 原文:Get the lower inventory involved in this transaction.
      *
-     * @return the inventory
+     * @return 物品栏
      */
     public abstract Inventory getBottomInventory();
 
     /**
-     * Get the player viewing.
+     * 获取正查看此窗口的玩家.
+     * <p>
+     * 原文:Get the player viewing.
      *
-     * @return the player
+     * @return 玩家
      */
     public abstract HumanEntity getPlayer();
 
     /**
-     * Determine the type of inventory involved in the transaction. This
+     * 判断该事件中物品栏的类型.
+     * 这表示将被展示的窗口样式. 该方法永不会返回PLAYER值.
+     * <p>
+     * 原文:Determine the type of inventory involved in the transaction. This
      * indicates the window style being shown. It will never return PLAYER,
      * since that is common to all windows.
      *
-     * @return the inventory type
+     * @return 物品栏类型
      */
     public abstract InventoryType getType();
 
     /**
-     * Sets one item in this inventory view by its raw slot ID.
+     * 设置该物品栏指定槽位的物品.
+     * <p>
+     * 注意: 如果id是 -999, 那么该物品将被扔在地面上. 然而, 这不是必须的行为.
+     * (所以你还是通过常规方法扔物品, 毕竟写个-999不看文档的人谁看得懂呢...233).
+     * <p>
+     * 原文:Sets one item in this inventory view by its raw slot ID.
      * <p>
      * Note: If slot ID -999 is chosen, it may be expected that the item is
      * dropped on the ground. This is not required behaviour, however.
      *
-     * @param slot The ID as returned by InventoryClickEvent.getRawSlot()
-     * @param item The new item to put in the slot, or null to clear it.
+     * @param slot 槽位id (可以通过InventoryClickEvent.getRawSlot()获取)
+     * @param item 要放置的物品, null则清除
      */
     public void setItem(int slot, ItemStack item) {
         if (slot != OUTSIDE) {
@@ -169,10 +188,12 @@ public abstract class InventoryView {
     }
 
     /**
-     * Gets one item in this inventory view by its raw slot ID.
+     * 获取该物品栏指定槽位的物品.
+     * <p>
+     * 原文:Gets one item in this inventory view by its raw slot ID.
      *
-     * @param slot The ID as returned by InventoryClickEvent.getRawSlot()
-     * @return The item currently in the slot.
+     * @param slot 槽位id (可以通过InventoryClickEvent.getRawSlot()获取)
+     * @return 该槽位内的物品
      */
     public ItemStack getItem(int slot) {
         if (slot == OUTSIDE) {
@@ -186,36 +207,68 @@ public abstract class InventoryView {
     }
 
     /**
-     * Sets the item on the cursor of one of the viewing players.
+     * 设置玩家客户端鼠标光标所指位置上的物品.
+     * <p>
+     * 原文:Sets the item on the cursor of one of the viewing players.
      *
-     * @param item The item to put on the cursor, or null to remove the item
-     *     on their cursor.
+     * @param item 要放置的物品, null则清除
      */
     public final void setCursor(ItemStack item) {
         getPlayer().setItemOnCursor(item);
     }
 
     /**
-     * Get the item on the cursor of one of the viewing players.
+     * 获取玩家客户端鼠标光标所指位置上的物品.
+     * <p>
+     * 原文:Get the item on the cursor of one of the viewing players.
      *
-     * @return The item on the player's cursor, or null if they aren't holding
-     *     one.
+     * @return 光标所指物品, 如果所指槽位为空返回null
      */
     public final ItemStack getCursor() {
         return getPlayer().getItemOnCursor();
     }
 
     /**
-     * Converts a raw slot ID into its local slot ID into whichever of the two
+     * 将原始槽位id转换为本地槽位id (本地槽位id适用于当前正在查看的两个物品栏).
+     * <p>
+     * 若原始槽位id指向窗口的上半部分的物品栏, 那么返回的值不变, 此时返回值适用于getTopInventory().getItem();
+     * 若该id指向底部物品栏, 返回的值将与原始槽位id不同, 此时返回值适用于getBottomInventory().getItem().
+     * <p>
+     * 这是玩家背包的一个实例(来自代码注释):
+     
+     * <p>
+     * 原文:Converts a raw slot ID into its local slot ID into whichever of the two
      * inventories the slot points to.
      * <p>
-     * If the raw slot refers to the upper inventory, it will be returned
+     * <pre>Raw Slots(原始槽位id):
+     *
+     * 5             1  2     0
+     * 6             3  4
+     * 7
+     * 8           45
+     * 9  10 11 12 13 14 15 16 17
+     * 18 19 20 21 22 23 24 25 26
+     * 27 28 29 30 31 32 33 34 35
+     * 36 37 38 39 40 41 42 43 44
+     *
+     * 转换后:
+     *
+     * 39             1  2     0
+     * 38             3  4
+     * 37
+     * 36          40
+     * 9  10 11 12 13 14 15 16 17
+     * 18 19 20 21 22 23 24 25 26
+     * 27 28 29 30 31 32 33 34 35
+     * 0  1  2  3  4  5  6  7  8</pre>
+     * <p>
+     * 原文:If the raw slot refers to the upper inventory, it will be returned
      * unchanged and thus be suitable for getTopInventory().getItem(); if it
      * refers to the lower inventory, the output will differ from the input
      * and be suitable for getBottomInventory().getItem().
      *
-     * @param rawSlot The raw slot ID.
-     * @return The converted slot ID.
+     * @param rawSlot 原始槽位id
+     * @return 转换后的槽位id
      */
     public final int convertSlot(int rawSlot) {
         int numInTop = getTopInventory().getSize();
@@ -282,7 +335,9 @@ public abstract class InventoryView {
     }
 
     /**
-     * Closes the inventory view.
+     * 关闭该窗口视图.
+     * <p>
+     * 原文:Closes the inventory view.
      */
     public final void close() {
         getPlayer().closeInventory();
@@ -315,9 +370,11 @@ public abstract class InventoryView {
     }
 
     /**
-     * Get the title of this inventory window.
+     * 获取物品栏窗口的标题.
+     * <p>
+     * 原文:Get the title of this inventory window.
      *
-     * @return The title.
+     * @return 标题
      */
     public final String getTitle() {
         return getTopInventory().getTitle();
