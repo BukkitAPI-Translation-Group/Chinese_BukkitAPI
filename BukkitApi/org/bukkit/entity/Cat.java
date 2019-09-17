bf61fae4db4c5326bbe0e93e3665aca9fe99660e
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-17 21:15:25 +0800

    添加猫的翻译

--- BukkitApi/org/bukkit/entity/Cat.java
+++ BukkitApi/org/bukkit/entity/Cat.java
@@ -4,41 +4,58 @@ import org.bukkit.DyeColor;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * 代表猫.
+ * <p>
+ * 原文：
  * Meow.
  */
 public interface Cat extends Animals, Tameable, Sittable {
 
     /**
+     * 获取这只猫的当前类型.
+     * <p>
+     * 原文：
      * Gets the current type of this cat.
      *
-     * @return Type of the cat.
+     * @return 猫的类型.
      */
     @NotNull
     public Type getCatType();
 
     /**
+     * 设置这只猫的当前类型.
+     * <p>
+     * 原文：
      * Sets the current type of this cat.
      *
-     * @param type New type of this cat.
+     * @param type 为这只猫设置的新类型.
      */
     public void setCatType(@NotNull Type type);
 
     /**
+     * 获取这只猫的项圈颜色.
+     * <p>
+     * 原文:
      * Get the collar color of this cat
      *
-     * @return the color of the collar
+     * @return 项圈的颜色.
      */
     @NotNull
     public DyeColor getCollarColor();
 
     /**
+     * 设置这只猫的项圈颜色.
+     * <p>
+     * 原文:
      * Set the collar color of this cat
      *
-     * @param color the color to apply
+     * @param color 要设置的颜色.
      */
     public void setCollarColor(@NotNull DyeColor color);
 
     /**
+     * 代表了各种猫的类型.
+     * <p>
      * Represents the various different cat types there are.
      */
     public enum Type {
