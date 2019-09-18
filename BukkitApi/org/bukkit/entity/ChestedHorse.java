aaded8bd6a395b4c87a50064ae49c815e90a2c7c
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-18 12:46:25 +0800

    添加翻译

--- BukkitApi/org/bukkit/entity/ChestedHorse.java
+++ BukkitApi/org/bukkit/entity/ChestedHorse.java
@@ -1,22 +1,32 @@
 package org.bukkit.entity;
 
 /**
+ * 代表一个可以携带物品的类马生物. 
+ * 译注: 就是有物品栏的由马衍生的生物, 例如驴、羊驼等.
+ * <p>
+ * 原文:
  * Represents Horse-like creatures which can carry an inventory.
  */
 public interface ChestedHorse extends AbstractHorse {
 
     /**
+     * 获得此马是否有装备箱子.
+     * <p>
+     * 原文:
      * Gets whether the horse has a chest equipped.
      *
-     * @return true if the horse has chest storage
+     * @return 如果此马有存储空间返回true
      */
     public boolean isCarryingChest();
 
     /**
+     * 设置此马是否装备箱子. 移除箱子将会清空箱子内所有物品.
+     * <p>
+     * 原文:
      * Sets whether the horse has a chest equipped. Removing a chest will also
      * clear the chest's inventory.
      *
-     * @param chest true if the horse should have a chest
+     * @param 设置为true则此马将拥有箱子
      */
     public void setCarryingChest(boolean chest);
 }
