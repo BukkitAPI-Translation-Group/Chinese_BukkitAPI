8d2c6e92ee3723d3298864a1fdc565b145c6a2af
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-18 12:31:46 +0800

    添加翻译

--- BukkitApi/org/bukkit/entity/AbstractVillager.java
+++ BukkitApi/org/bukkit/entity/AbstractVillager.java
@@ -6,11 +6,18 @@ import org.bukkit.inventory.Merchant;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * 代表一个抽象村民NPC. 
+ * <p>
+ * 原文:
  * Represents a villager NPC
  */
 public interface AbstractVillager extends Ageable, NPC, InventoryHolder, Merchant {
 
     /**
+     * 获得这个村民的物品栏.
+     * 注意这个物品栏不是商人村民交易的物品, 而是村民收集的物品(收割庄稼)
+     * <p>
+     * 原文:
      * Gets this villager's inventory.
      * <br>
      * Note that this inventory is not the Merchant inventory, rather, it is the
