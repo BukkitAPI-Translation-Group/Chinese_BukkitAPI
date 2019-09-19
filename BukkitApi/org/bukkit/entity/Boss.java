67b1b9b1ea0d8ac137ab344eaf62f6f153683632
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-19 23:02:16 +0800

    添加句号

--- BukkitApi/org/bukkit/entity/Boss.java
+++ BukkitApi/org/bukkit/entity/Boss.java
@@ -11,12 +11,12 @@ import org.bukkit.boss.BossBar;
 public interface Boss extends Entity {
 
     /**
-     * 返回这个 {@link Boss} 的 {@link BossBar}
+     * 返回这个 {@link Boss} 的 {@link BossBar}.
      * <p>
      * 原文:
      * Returns the {@link BossBar} of the {@link Boss}
      *
-     * @return 此实体的 {@link BossBar} 
+     * @return 此实体的 {@link BossBar}. 
      */
     BossBar getBossBar();
 }
