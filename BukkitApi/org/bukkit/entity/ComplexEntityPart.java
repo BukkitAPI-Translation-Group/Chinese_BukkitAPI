14fce97c1e8784f48f4358ea3e26283a596f846b
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-20 08:53:18 +0800

    Update ComplexEntityPart.java

--- BukkitApi/org/bukkit/entity/ComplexEntityPart.java
+++ BukkitApi/org/bukkit/entity/ComplexEntityPart.java
@@ -1,14 +1,20 @@
 package org.bukkit.entity;
 
 /**
+ * 代表 {@link ComplexLivingEntity} 的一部分.
+ * <p>
+ * 原文:
  * Represents a single part of a {@link ComplexLivingEntity}
  */
 public interface ComplexEntityPart extends Entity {
 
     /**
+     * 获得此组件的父对象 {@link ComplexLivingEntity}
+     * <p>
+     * 原文:
      * Gets the parent {@link ComplexLivingEntity} of this part.
      *
-     * @return Parent complex entity
+     * @return Parent 复杂实体
      */
     public ComplexLivingEntity getParent();
 }
