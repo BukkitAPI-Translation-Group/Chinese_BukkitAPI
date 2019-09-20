00e542e0f81e5d8dccd1e451e59dbca2fe730996
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-20 08:48:09 +0800

    翻译AbstractHorse

--- BukkitApi/org/bukkit/entity/AbstractHorse.java
+++ BukkitApi/org/bukkit/entity/AbstractHorse.java
@@ -4,6 +4,10 @@ import org.bukkit.inventory.AbstractHorseInventory;
 import org.bukkit.inventory.InventoryHolder;
 
 /**
+ * 代表一个马基对象.
+ * 译注: 基于马的对象,比如羊驼,驴.
+ * <p>
+ * 原文:
  * Represents a Horse-like creature.
  */
 public interface AbstractHorse extends Animals, Vehicle, InventoryHolder, Tameable {
@@ -29,17 +33,30 @@ public interface AbstractHorse extends Animals, Vehicle, InventoryHolder, Tameab
     public void setVariant(Horse.Variant variant);
 
     /**
+     * 获得此马的驯化等级.
+     * <p>
+     * 更高的驯化等级表明此马更容易被驯服, 当此马的驯化等级接近最高驯化等级时,驯服的几率会增加.
+     * <p>
+     * 原文:
      * Gets the domestication level of this horse.
      * <p>
      * A higher domestication level indicates that the horse is closer to
      * becoming tame. As the domestication level gets closer to the max
      * domestication level, the chance of the horse becoming tame increases.
      *
-     * @return domestication level
+     * @return 驯化等级
      */
     public int getDomestication();
 
     /**
+     
+     * 设置此马的驯化等级.
+     * <p>
+     * 设置此马的驯化等级更高将让这匹马的驯化几率提升.
+     * <p>
+     * 驯化等级必须 大于0 且 小于最高驯化等级, 由 {@link #getMaxDomestication()} 定义.
+     * <p>
+     * 原文:
      * Sets the domestication level of this horse.
      * <p>
      * Setting the domestication level to a high value will increase the
@@ -49,21 +66,32 @@ public interface AbstractHorse extends Animals, Vehicle, InventoryHolder, Tameab
      * the max domestication level of the horse, determined with
      * {@link #getMaxDomestication()}
      *
-     * @param level domestication level
+     * @param level 驯化等级
      */
     public void setDomestication(int level);
 
     /**
+     * 获得此马的最高驯化等级.
+     * <p>
+     * 此等级越高, 驯化的时间将会更长.
+     * <p>
+     * 原文:
      * Gets the maximum domestication level of this horse.
      * <p>
      * The higher this level is, the longer it will likely take
      * for the horse to be tamed.
      *
-     * @return the max domestication level
+     * @return 最高驯化等级
      */
     public int getMaxDomestication();
 
     /**
+     * 设置此马的最高驯化等级.
+     * 设定一个更高的驯化等级将会增加需要驯化的次数(例如喂食, 骑乘), 反之减少.
+     * <p>
+     * 最高驯化等级必须大于0.
+     * <p>
+     * 原文:
      * Sets the maximum domestication level of this horse.
      * <p>
      * Setting a higher max domestication will increase the amount of
@@ -72,21 +100,33 @@ public interface AbstractHorse extends Animals, Vehicle, InventoryHolder, Tameab
      * <p>
      * Maximum domestication must be greater than zero.
      *
-     * @param level the max domestication level
+     * @param level 最高驯化等级
      */
     public void setMaxDomestication(int level);
 
     /**
+     * 获得此马的跳跃强度.
+     * <p>
+     * 跳跃强度定义这匹马能跳多高. 更高的跳跃等级将增加跳跃高度.
+     * <p>
+     * 原文:
      * Gets the jump strength of this horse.
      * <p>
      * Jump strength defines how high the horse can jump. A higher jump strength
      * increases how high a jump will go.
      *
-     * @return the horse's jump strength
+     * @return 跳跃等级
      */
     public double getJumpStrength();
 
     /**
+     * 设置此马的跳跃强度.
+     * <p>
+     * 跳跃强度定义这匹马能跳多高. 更高的跳跃等级将增加跳跃高度.
+     * 设置为0将导致马无法跳跃.
+     * 只能设置0至2之间的数值.
+     * <p>
+     * 原文:
      * Sets the jump strength of this horse.
      * <p>
      * A higher jump strength increases how high a jump will go.
@@ -94,7 +134,7 @@ public interface AbstractHorse extends Animals, Vehicle, InventoryHolder, Tameab
      * You cannot set a jump strength to a value below 0 or
      * above 2.
      *
-     * @param strength jump strength for this horse
+     * @param strength 跳跃等级
      */
     public void setJumpStrength(double strength);
 
