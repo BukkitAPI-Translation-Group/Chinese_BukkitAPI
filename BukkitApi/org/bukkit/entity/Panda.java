67cf1dd2fda44f9f92b0c097dbb63efa14cf2ce3
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-20 18:18:35 +0800

    Update Panda.java

--- BukkitApi/org/bukkit/entity/Panda.java
+++ BukkitApi/org/bukkit/entity/Panda.java
@@ -3,26 +3,37 @@ package org.bukkit.entity;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * 熊猫.
+ * <p>
+ * 原文:
  * Panda entity.
  */
 public interface Panda extends Animals {
 
     /**
+     * 获得熊猫的显性基因.
+     * <p>
      * Gets this Panda's main gene.
      *
-     * @return main gene
+     * @return 显性基因
      */
     @NotNull
     Gene getMainGene();
 
     /**
+     * 设置熊猫的显性基因.
+     * <p>
+     * 原文:
      * Sets this Panda's main gene.
      *
-     * @param gene main gene
+     * @param gene 显性基因
      */
     void setMainGene(@NotNull Gene gene);
 
     /**
+     * 获取隐性基因.
+     * <p>
+     * 原文:
      * Gets this Panda's hidden gene.
      *
      * @return hidden gene
@@ -31,9 +42,12 @@ public interface Panda extends Animals {
     Gene getHiddenGene();
 
     /**
+     * 设置隐性基因.
+     * <p>
+     * 原文:
      * Sets this Panda's hidden gene.
      *
-     * @param gene hidden gene
+     * @param gene 隐形基因
      */
     void setHiddenGene(@NotNull Gene gene);
 
@@ -54,10 +68,13 @@ public interface Panda extends Animals {
         }
 
         /**
+         * 获得此基因是否为隐性基因, 也就是说要求父母需要遗传给孩子.
+         * <p>
+         * 原文:
          * Gets whether this gene is recessive, i.e. required in both parents to
          * propagate to children.
          *
-         * @return recessive status
+         * @return 隐性状态
          */
         public boolean isRecessive() {
             return recessive;
