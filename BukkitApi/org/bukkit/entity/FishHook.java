a75fcb01966571ad3144c35017655dcf4bcaa03e
ShirosakiMieru <admin@shirosakimieru.com>
2019-09-20 09:00:18 +0800

    Update FishHook.java

--- BukkitApi/org/bukkit/entity/FishHook.java
+++ BukkitApi/org/bukkit/entity/FishHook.java
@@ -1,17 +1,26 @@
 package org.bukkit.entity;
 
 /**
+ * 代表一个鱼钩.
+ * <p>
+ * 原文:
  * Represents a fishing hook.
  */
 public interface FishHook extends Projectile {
     /**
+     * 获取鱼咬钩的几率.
+     * <p>
+     * 0.0 = 永不上钩.<br>
+     * 1.0 = 立即咬钩.
+     * <p>
+     * 原文:
      * Gets the chance of a fish biting.
      * <p>
      * 0.0 = No Chance.<br>
      * 1.0 = Instant catch.
      *
-     * @return chance the bite chance
-     * @deprecated has no effect in newer Minecraft versions
+     * @return chance 咬钩几率
+     * @deprecated Minecraft新版本中已无效果
      */
     @Deprecated
     public double getBiteChance();
