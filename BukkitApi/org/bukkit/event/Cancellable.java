373cf9e88563aae3b6f42bb1aca973d850075315
Cat & Dog <hcrgm@qq.com>
2020-02-20 21:53:59 +0800

    Update Cancellable.java

--- BukkitApi/org/bukkit/event/Cancellable.java
+++ BukkitApi/org/bukkit/event/Cancellable.java
@@ -3,7 +3,7 @@ package org.bukkit.event;
 public interface Cancellable {
 
     /**
-     * 获取这个事件是否被取消.一个被取消的事件不会在服务器里被执行，但是仍然会传递事件到其他插件。
+     * 获取这个事件是否被取消.一个被取消的事件不会在服务器里执行，但是仍然会传递事件到其他插件.
      * <p>
      * 原文：Gets the cancellation state of this event. A cancelled event will not
      * be executed in the server, but will still pass to other plugins
@@ -14,7 +14,7 @@ public interface Cancellable {
 
     /**
      * 取消这个事件. 一个被取消的事件不会在
-     * 服务器里被执行，但是仍然会传递事件到其他插件。
+     * 服务器里执行，但是仍然会传递事件到其他插件.
      * <p>
      * 原文：Sets the cancellation state of this event. A cancelled event will not
      * be executed in the server, but will still pass to other plugins.
