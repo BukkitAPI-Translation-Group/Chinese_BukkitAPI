8a78902d21d59b027afe0c360ba84339425b2702
hcrgm <hcrgm@qq.com>
2020-11-02 13:06:50 +0800

    CommandSender: 给->向,对象->实例

--- BukkitApi/org/bukkit/command/CommandSender.java
+++ BukkitApi/org/bukkit/command/CommandSender.java
@@ -7,32 +7,32 @@ import org.jetbrains.annotations.NotNull;
 public interface CommandSender extends Permissible {
 
     /**
-     * 给命令发送者发送消息.
+     * 向命令发送者发送消息.
      * <p>
      * 原文:
      * Sends this sender a message
      *
-     * @param message 要显示的消息
+     * @param message 要发送的消息
      */
     public void sendMessage(@NotNull String message);
 
     /**
-     * 给命令发送者发送多条消息.
+     * 向命令发送者发送多条消息.
      * <p>
      * 原文:
      * Sends this sender multiple messages
      *
-     * @param messages 要显示的消息(必须为数组)
+     * @param messages 要发送的消息(必须为数组)
      */
     public void sendMessage(@NotNull String[] messages);
 
     /**
-     * 返回服务器运行此命令的对象.
+     * 返回运行此命令的服务器实例.
      * <p>
      * 原文:
      * Returns the server instance that this command is running on
      *
-     * @return 服务器对象
+     * @return 服务器实例
      */
     @NotNull
     public Server getServer();
