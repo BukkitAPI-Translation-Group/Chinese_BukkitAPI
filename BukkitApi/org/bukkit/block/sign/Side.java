b228d8a83557a793e764a789b21d8836da3556ce
handy <handyplus@163.com>
2023-06-16 15:22:55 +0800

    告示牌的方向 翻译

--- BukkitApi/org/bukkit/block/sign/Side.java
+++ BukkitApi/org/bukkit/block/sign/Side.java
@@ -1,10 +1,19 @@
 package org.bukkit.block.sign;
 
 /**
+ * 表示告示牌的侧面。
+ * <p>
+ * 原文:
  * Represents the side of sign.
  */
 public enum Side {
 
+    /**
+     * 表示告示牌的前面
+     */
     FRONT,
+    /**
+     * 表示告示牌的背面
+     */
     BACK;
 }
