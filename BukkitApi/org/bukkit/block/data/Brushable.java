cab4fae9482726da783bd866fd9fdc26d30d6cd0
handy <handyplus@163.com>
2023-06-16 15:21:25 +0800

    Brushable 翻译

--- BukkitApi/org/bukkit/block/data/Brushable.java
+++ BukkitApi/org/bukkit/block/data/Brushable.java
@@ -1,11 +1,17 @@
 package org.bukkit.block.data;
 
 /**
+ * 'dusted' 表示方块被刷子清除的程度，即露出的程度。
+ * <p>
+ * 原文:
  * 'dusted' represents how far uncovered by brush the block is.
  */
 public interface Brushable extends BlockData {
 
     /**
+     * 获取 'dusted' 属性的值。
+     * <p>
+     * 原文:
      * Gets the value of the 'dusted' property.
      *
      * @return the 'dusted' value
@@ -13,6 +19,9 @@ public interface Brushable extends BlockData {
     int getDusted();
 
     /**
+     * 设置 'dusted' 属性的值。
+     * <p>
+     * 原文:
      * Sets the value of the 'dusted' property.
      *
      * @param dusted the new 'dusted' value
@@ -20,6 +29,9 @@ public interface Brushable extends BlockData {
     void setDusted(int dusted);
 
     /**
+     * 获取 'dusted' 属性允许的最大值。
+     * <p>
+     * 原文:
      * Gets the maximum allowed value of the 'dusted' property.
      *
      * @return the maximum 'dusted' value
