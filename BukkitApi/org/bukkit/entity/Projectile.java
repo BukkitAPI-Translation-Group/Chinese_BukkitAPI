e78247601e46aa6127faa4e01b8dfb03fce5d847
handy <handyplus@163.com>
2023-06-27 17:54:04 +0800

    Projectile 翻译

--- BukkitApi/org/bukkit/entity/Projectile.java
+++ BukkitApi/org/bukkit/entity/Projectile.java
@@ -4,39 +4,41 @@ import org.bukkit.projectiles.ProjectileSource;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * 表示可被射击实体的概念
+ * <p>
+ * 原文：
  * Represents a shootable entity.
  */
 public interface Projectile extends Entity {
 
     /**
-     * Retrieve the shooter of this projectile.
+     * 检索该抛射物的发射者。
      *
-     * @return the {@link ProjectileSource} that shot this projectile
+     * @return 发射该抛射物的 {@link ProjectileSource}e
      */
     @Nullable
     public ProjectileSource getShooter();
 
     /**
-     * Set the shooter of this projectile.
+     * 设置该抛射物的发射者。
      *
-     * @param source the {@link ProjectileSource} that shot this projectile
+     * @param source 发射该抛射物的 {@link ProjectileSource}
      */
     public void setShooter(@Nullable ProjectileSource source);
 
     /**
-     * Determine if this projectile should bounce or not when it hits.
+     * 确定该抛射物在击中时是否应该弹跳。
      * <p>
-     * If a small fireball does not bounce it will set the target on fire.
+     * 如果一个小型火球不弹跳，则会使目标着火。
      *
-     * @return true if it should bounce.
+     * @return 如果应该弹跳，则返回true。
      */
     public boolean doesBounce();
 
     /**
-     * Set whether or not this projectile should bounce or not when it hits
-     * something.
+     * 设置该抛射物在击中时是否应该弹跳。
      *
-     * @param doesBounce whether or not it should bounce.
+     * @param doesBounce 是否应该弹跳。
      */
     public void setBounce(boolean doesBounce);
 }
