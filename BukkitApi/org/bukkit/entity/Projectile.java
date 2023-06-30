2a4e0f15c075f4d5c108166d27fecbe081d5a607
handy <handyplus@163.com>
2023-06-30 21:08:34 +0800

    Projectile 翻译 (#48)
    * Projectile 翻译
    
    * 修正Projectile类的翻译
    方法翻译需要保留原文
    
    ---------
    
    Co-authored-by: hcrgm <hcrgm@qq.com>

--- BukkitApi/org/bukkit/entity/Projectile.java
+++ BukkitApi/org/bukkit/entity/Projectile.java
@@ -4,39 +4,52 @@ import org.bukkit.projectiles.ProjectileSource;
 import org.jetbrains.annotations.Nullable;
 
 /**
+ * 表示可被射击实体的概念
+ * <p>
+ * 原文：
  * Represents a shootable entity.
  */
 public interface Projectile extends Entity {
 
     /**
-     * Retrieve the shooter of this projectile.
+     * 获取该抛射物的发射者.
+     * <p>
+     * 原文:Retrieve the shooter of this projectile.
      *
-     * @return the {@link ProjectileSource} that shot this projectile
+     * @return 发射该抛射物的 {@link ProjectileSource}
      */
     @Nullable
     public ProjectileSource getShooter();
 
     /**
-     * Set the shooter of this projectile.
+     * 设置该抛射物的发射者.
+     * <p>
+     * 原文:Set the shooter of this projectile.
      *
-     * @param source the {@link ProjectileSource} that shot this projectile
+     * @param source 发射该抛射物的 {@link ProjectileSource}
      */
     public void setShooter(@Nullable ProjectileSource source);
 
     /**
-     * Determine if this projectile should bounce or not when it hits.
+     * 判断该抛射物在击中物体时是否应该弹跳.
+     * <p>
+     * 如果一个小型火球不弹跳,则会使目标着火.
+     * <p>
+     * 原文:Determine if this projectile should bounce or not when it hits.
      * <p>
      * If a small fireball does not bounce it will set the target on fire.
      *
-     * @return true if it should bounce.
+     * @return 如果应该弹跳，则返回true。
      */
     public boolean doesBounce();
 
     /**
-     * Set whether or not this projectile should bounce or not when it hits
+     * 设置该抛射物在击中物体时是否应该弹跳.
+     * <p>
+     * 原文:Set whether or not this projectile should bounce or not when it hits
      * something.
      *
-     * @param doesBounce whether or not it should bounce.
+     * @param doesBounce 是否应该弹跳
      */
     public void setBounce(boolean doesBounce);
 }
