e3e538061a21e17242c8c816321c7dcec6395d06
faosucvwabscvbw <137505734+faosucvwabscvbw@users.noreply.github.com>
2023-08-11 17:59:04 +0800

    Update EquipmentSlot.java

--- BukkitApi/org/bukkit/inventory/EquipmentSlot.java
+++ BukkitApi/org/bukkit/inventory/EquipmentSlot.java
@@ -1,11 +1,32 @@
 package org.bukkit.inventory;
 
+/**
+ * 一个描述玩家身体部位的枚举
+ */
 public enum EquipmentSlot {
 
+    /**
+     * 玩家主手
+     */
     HAND,
+    /**
+     * 玩家副手
+     */
     OFF_HAND,
+    /**
+     * 头部位置
+     */
     FEET,
+    /**
+     * 胸部位置
+     */
     LEGS,
+    /**
+     * 腿部位置
+     */
     CHEST,
+    /**
+     * 脚部位置
+     */
     HEAD
 }
