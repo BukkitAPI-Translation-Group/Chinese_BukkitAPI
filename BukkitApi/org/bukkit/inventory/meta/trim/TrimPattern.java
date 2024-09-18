956e16cd8b547c128009f1a40d9ac288018a4cd2
handy <handyplus@163.com>
2024-09-18 11:15:50 +0800

    完善盔甲纹饰相关翻译2

--- BukkitApi/org/bukkit/inventory/meta/trim/TrimPattern.java
+++ BukkitApi/org/bukkit/inventory/meta/trim/TrimPattern.java
@@ -14,92 +14,92 @@ import org.bukkit.Translatable;
 public interface TrimPattern extends Keyed, Translatable {
 
     /**
-     * 哨兵盔甲纹饰制作模板
+     * 哨兵盔甲纹饰
      * {@link Material#SENTRY_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern SENTRY = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("sentry"));
     /**
-     * 沙丘盔甲纹饰制作模板
+     * 沙丘盔甲纹饰
      * {@link Material#DUNE_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern DUNE = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("dune"));
     /**
-     * 海岸盔甲纹饰制作模板
+     * 海岸盔甲纹饰
      * {@link Material#COAST_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern COAST = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("coast"));
     /**
-     * 野性盔甲纹饰制作模板
+     * 荒野盔甲纹饰
      * {@link Material#WILD_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern WILD = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("wild"));
     /**
-     * 守卫盔甲纹饰制作模板
+     * 监守盔甲纹饰
      * {@link Material#WARD_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern WARD = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("ward"));
     /**
-     * 眼睛盔甲纹饰制作模板
+     * 眼眸盔甲纹饰
      * {@link Material#EYE_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern EYE = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("eye"));
     /**
-     * 恶魔盔甲纹饰制作模板
+     * 恼鬼盔甲纹饰
      * {@link Material#VEX_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern VEX = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("vex"));
     /**
-     * 潮汐盔甲纹饰制作模板
+     * 潮汐盔甲纹饰
      * {@link Material#TIDE_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern TIDE = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("tide"));
     /**
-     * 鼻子盔甲纹饰制作模板
+     * 猪鼻盔甲纹饰
      * {@link Material#SNOUT_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern SNOUT = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("snout"));
     /**
-     * 肋骨盔甲纹饰制作模板
+     * 肋骨盔甲纹饰
      * {@link Material#RIB_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern RIB = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("rib"));
     /**
-     * 尖塔盔甲纹饰制作模板
+     * 尖塔盔甲纹饰
      * {@link Material#SPIRE_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern SPIRE = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("spire"));
     /**
-     * 探路者盔甲纹饰制作模板
+     * 向导盔甲纹饰
      * {@link Material#WAYFINDER_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern WAYFINDER = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("wayfinder"));
     /**
-     * 造型师盔甲纹饰制作模板
+     * 塑形盔甲纹饰
      * {@link Material#SHAPER_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern SHAPER = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("shaper"));
     /**
-     * 沉默盔甲纹饰制作模板
+     * 幽静盔甲纹饰
      * {@link Material#SILENCE_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern SILENCE = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("silence"));
     /**
-     * 提升者盔甲纹饰制作模板
+     * 牧民盔甲纹饰
      * {@link Material#RAISER_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern RAISER = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("raiser"));
     /**
-     * 宿主盔甲纹饰制作模板
+     * 雇主盔甲纹饰
      * {@link Material#HOST_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern HOST = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("host"));
     /**
-     * 流动盔甲纹饰制作模板
+     * 涡流盔甲纹饰
      * {@link Material#FLOW_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern FLOW = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("flow"));
     /**
-     * 闪电盔甲纹饰制作模板
+     * 闪电盔甲纹饰
      * {@link Material#BOLT_ARMOR_TRIM_SMITHING_TEMPLATE}.
      */
     public static final TrimPattern BOLT = Registry.TRIM_PATTERN.get(NamespacedKey.minecraft("bolt"));
