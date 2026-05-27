745dcc350f76d46c52e18387841e6b5b7796780a
handy <handyplus@163.com>
2026-05-27 19:04:59 +0800

    翻译 BukkitApi/org/bukkit/event/inventory/InventoryClickEvent.java

--- BukkitApi/org/bukkit/event/inventory/InventoryClickEvent.java
+++ BukkitApi/org/bukkit/event/inventory/InventoryClickEvent.java
@@ -33,16 +33,13 @@ import org.jetbrains.annotations.Nullable;
  * ，这个方法将在下一个tick执行你的任务。<br>
  * 注意：上面列出来的这些方法并不全，可能还有其它的方法也存在问题。
  * <p>
- * Assuming the EntityHuman associated with this event is an instance of a
- * Player, manipulating the MaxStackSize or contents of an Inventory will
- * require an Invocation of {@link Player#updateInventory()}.
+ * 假设与此事件关联的 EntityHuman 是一个 Player 实例，那么操作物品栏的最大堆叠数量或内容
+ * 将需要调用 {@link Player#updateInventory()}。
  * <p>
- * Modifications to slots that are modified by the results of this
- * InventoryClickEvent can be overwritten. To change these slots, this event
- * should be cancelled and all desired changes to the inventory applied.
- * Alternatively, scheduling a task using
- * {@link BukkitScheduler#runTask( Plugin, Runnable)}, which would execute the
- * task on the next tick, would work as well.
+ * 对此 InventoryClickEvent 结果所修改的格子的更改可能会被覆盖。要更改这些格子，
+ * 应取消此事件并应用所有对物品栏的期望更改。
+ * 或者，使用 {@link BukkitScheduler#runTask(Plugin, Runnable)} 调度一个任务
+ * 也可以实现，该方法将在下一个 tick 执行你的任务。
  */
 public class InventoryClickEvent extends InventoryInteractEvent {
 
@@ -148,15 +145,14 @@ public class InventoryClickEvent extends InventoryInteractEvent {
     }
 
     /**
-     * Sets the item on the cursor.
+     * 设置光标上的物品.
+     * <p>
+     * 原文：Sets the item on the cursor.
      *
-     * @param stack
-     *            the new cursor item
-     * @deprecated This changes the ItemStack in their hand before any
-     *             calculations are applied to the Inventory, which has a
-     *             tendency to create inconsistencies between the Player and the
-     *             server, and to make unexpected changes in the behavior of the
-     *             clicked Inventory.
+     * @param stack 新的光标物品
+     * @deprecated 这会在对物品栏应用任何计算之前更改他们手中的 ItemStack，
+     *             这往往会导致玩家与服务器之间的不一致，
+     *             并对被点击的物品栏的行为产生意外更改。
      */
     @Deprecated(since = "1.5.2")
     public void setCursor(@Nullable ItemStack stack) {
@@ -164,10 +160,11 @@ public class InventoryClickEvent extends InventoryInteractEvent {
     }
 
     /**
-     * Sets the ItemStack currently in the clicked slot.
+     * 设置被点击格子中的物品.
+     * <p>
+     * 原文：Sets the ItemStack currently in the clicked slot.
      *
-     * @param stack
-     *            the item to be placed in the current slot
+     * @param stack 要放入当前格子的物品
      */
     public void setCurrentItem(@Nullable ItemStack stack) {
         if (slot_type == SlotType.OUTSIDE) {
@@ -178,9 +175,11 @@ public class InventoryClickEvent extends InventoryInteractEvent {
     }
 
     /**
-     * Gets the inventory corresponding to the clicked slot.
+     * 获取被点击格子对应的物品栏.
+     * <p>
+     * 原文：Gets the inventory corresponding to the clicked slot.
      *
-     * @return inventory, or null if clicked outside
+     * @return 物品栏，如果点击了外部则为 null
      * @see InventoryView#getInventory(int)
      */
     @Nullable
@@ -227,13 +226,18 @@ public class InventoryClickEvent extends InventoryInteractEvent {
     }
 
     /**
-     * Gets the InventoryAction that triggered this event.
+     * 获取触发此事件的 InventoryAction.
+     * <p>
+     * 此操作无法更改，它代表此事件的正常结果。
+     * 要更改此 InventoryClickEvent 的行为，必须手动应用更改。
+     * <p>
+     * 原文：Gets the InventoryAction that triggered this event.
      * <p>
      * This action cannot be changed, and represents what the normal outcome of
      * the event will be. To change the behavior of this InventoryClickEvent,
      * changes must be manually applied.
      *
-     * @return the InventoryAction that triggered this event.
+     * @return 触发此事件的 InventoryAction
      */
     @NotNull
     public InventoryAction getAction() {
