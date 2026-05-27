d43ab110310c37338268c72e025dc0140c8d66ce
handy <handyplus@163.com>
2026-05-27 19:01:37 +0800

    翻译 BukkitApi/org/bukkit/entity/Animals.java

--- BukkitApi/org/bukkit/entity/Animals.java
+++ BukkitApi/org/bukkit/entity/Animals.java
@@ -12,65 +12,68 @@ import org.jetbrains.annotations.Nullable;
 public interface Animals extends Breedable {
     
     /**
-     * Get the UUID of the entity that caused this entity to enter the
-     * {@link #canBreed()} state.
+     * 获取导致此实体进入 {@link #canBreed()} 状态的实体UUID.
+     * <p>
+     * 原文:Get the UUID of the entity that caused this entity to enter the {@link #canBreed()} state.
      *
-     * @return uuid if set, or null
+     * @return 如果已设置则返回UUID，否则返回null
      */
     @Nullable
     UUID getBreedCause();
 
     /**
-     * Set the UUID of the entity that caused this entity to enter the
-     * {@link #canBreed()} state.
+     * 设置导致此实体进入 {@link #canBreed()} 状态的实体UUID.
+     * <p>
+     * 原文:Set the UUID of the entity that caused this entity to enter the {@link #canBreed()} state.
      *
-     * @param uuid new uuid, or null
+     * @param uuid 新的UUID，或null
      */
     void setBreedCause(@Nullable UUID uuid);
 
     /**
-     * Get whether or not this entity is in love mode and will produce
-     * offspring with another entity in love mode. Will return true if
-     * and only if {@link #getLoveModeTicks()} is greater than 0.
+     * 获取此实体是否处于发情状态，并将与另一个处于发情状态的实体产生后代。当且仅当 {@link #getLoveModeTicks()} 大于0时返回true.
+     * <p>
+     * 原文:Get whether or not this entity is in love mode and will produce offspring with another entity in love mode.
      *
-     * @return true if in love mode, false otherwise
+     * @return 如果处于发情状态则返回true，否则返回false
      */
     boolean isLoveMode();
 
     /**
-     * Get the amount of ticks remaining for this entity in love mode.
-     * If the entity is not in love mode, 0 will be returned.
+     * 获取此实体剩余的发情状态tick数。如果实体不处于发情状态，将返回0.
+     * <p>
+     * 原文:Get the amount of ticks remaining for this entity in love mode.
      *
-     * @return the remaining love mode ticks
+     * @return 剩余的发情状态tick数
      */
     int getLoveModeTicks();
 
     /**
-     * Set the amount of ticks for which this entity should be in love mode.
-     * Setting the love mode ticks to 600 is the equivalent of a player
-     * feeding the entity their breeding item of choice.
+     * 设置此实体应处于发情状态的tick数。将发情状态tick数设置为600等同于玩家喂食该实体其选择的繁殖物品.
+     * <p>
+     * 原文:Set the amount of ticks for which this entity should be in love mode.
      *
-     * @param ticks the love mode ticks. Must be positive
+     * @param ticks 发情状态tick数，必须为正数
      */
     void setLoveModeTicks(int ticks);
 
     /**
-     * Check if the provided ItemStack is the correct item used for breeding
-     * this entity.
+     * 检查提供的ItemStack是否是用于繁殖此实体的正确物品.
+     * <p>
+     * 原文:Check if the provided ItemStack is the correct item used for breeding this entity.
      *
-     * @param stack ItemStack to check.
-     * @return if the provided ItemStack is the correct food item for this
-     * entity.
+     * @param stack 要检查的ItemStack
+     * @return 如果提供的ItemStack是此实体的正确食物则返回true
      */
     boolean isBreedItem(@NotNull ItemStack stack);
 
     /**
-     * Check if the provided ItemStack is the correct item used for breeding
-     * this entity..
+     * 检查提供的Material是否是用于繁殖此实体的正确物品.
+     * <p>
+     * 原文:Check if the provided ItemStack is the correct item used for breeding this entity.
      *
-     * @param material Material to check.
-     * @return if the provided ItemStack is the correct food item for this
-     * entity.
+     * @param material 要检查的Material
+     * @return 如果提供的Material是此实体的正确食物则返回true
      */
     boolean isBreedItem(@NotNull Material material);
 }
