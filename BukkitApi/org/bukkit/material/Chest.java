384b5f4e617bf1e88a12c4dc2f56330957785d5a
handy <handyplus@163.com>
2026-05-27 19:00:00 +0800

    翻译 BukkitApi/org/bukkit/material/Chest.java

--- BukkitApi/org/bukkit/material/Chest.java
+++ BukkitApi/org/bukkit/material/Chest.java
@@ -5,8 +5,8 @@ import org.bukkit.block.BlockFace;
 
 /**
  * 代表箱子.
- * @deprecated all usage of MaterialData is deprecated and subject to removal.
- * Use {@link org.bukkit.block.data.BlockData}.
+ * @deprecated 所有 MaterialData 的用法均已弃用且将被移除.
+ * 请使用 {@link org.bukkit.block.data.BlockData}.
  */
 @Deprecated(since = "1.14.1")
 public class Chest extends DirectionalContainer {
@@ -32,8 +32,8 @@ public class Chest extends DirectionalContainer {
     }
 
     /**
-     * @param type the type
-     * @param data the raw data value
+     * @param type 物品类型
+     * @param data 原始数据值
      * @deprecated 不安全的参数
      */
     @Deprecated(since = "1.6.2")
