291a687c5ce666dcc01623baa5b791d2c5220055
handy <handyplus@163.com>
2026-05-27 19:06:10 +0800

    翻译 BukkitApi/org/bukkit/UnsafeValues.java

--- BukkitApi/org/bukkit/UnsafeValues.java
+++ BukkitApi/org/bukkit/UnsafeValues.java
@@ -51,7 +51,20 @@ public interface UnsafeValues {
     byte[] processClass(PluginDescriptionFile pdf, String path, byte[] clazz);
 
     /**
-     * Load an advancement represented by the specified string into the server.
+     * 将指定字符串表示的进度加载到服务器中。
+     * 进度格式由 Minecraft 控制，没有指定的布局。
+     * <br>
+     * 目前是一个 JSON 对象，如 <a href="https://minecraft.wiki/w/Advancements">Minecraft wiki</a> 所述。
+     * <br>
+     * 加载的进度将被存储并在服务器重启和重载后持续存在。
+     * <br>
+     * 调用者应准备好捕获 {@link Exception}。
+     *
+     * @param key 进度的唯一键
+     * @param advancement 进度的表示形式
+     * @return 加载的进度，如果发生错误则返回 null
+     * <p>
+     * 原文：Load an advancement represented by the specified string into the server.
      * The advancement format is governed by Minecraft and has no specified
      * layout.
      * <br>
@@ -61,23 +74,23 @@ public interface UnsafeValues {
      * and reloads.
      * <br>
      * Callers should be prepared for {@link Exception} to be thrown.
-     *
-     * @param key the unique advancement key
-     * @param advancement representation of the advancement
-     * @return the loaded advancement or null if an error occurred
      */
     Advancement loadAdvancement(NamespacedKey key, String advancement);
 
     /**
-     * Delete an advancement which was loaded and saved by
+     * 删除由 {@link #loadAdvancement(org.bukkit.NamespacedKey, java.lang.String)} 加载并保存的进度。
+     * <br>
+     * 此方法只会从持久存储中删除进度。应该伴随调用 {@link Server#reloadData()} 以便从运行实例中完全删除它。
+     *
+     * @param key 进度的唯一键
+     * @return 如果找到并删除了匹配此键的文件则返回 true
+     * <p>
+     * 原文：Delete an advancement which was loaded and saved by
      * {@link #loadAdvancement(org.bukkit.NamespacedKey, java.lang.String)}.
      * <br>
      * This method will only remove advancement from persistent storage. It
      * should be accompanied by a call to {@link Server#reloadData()} in order
      * to fully remove it from the running instance.
-     *
-     * @param key the unique advancement key
-     * @return true if a file matching this key was found and deleted
      */
     boolean removeAdvancement(NamespacedKey key);
 
@@ -104,10 +117,12 @@ public interface UnsafeValues {
     FeatureFlag getFeatureFlag(@NotNull NamespacedKey key);
 
     /**
-     * Do not use, method will get removed, and the plugin won't run
+     * 不要使用，方法将被移除，插件将无法运行。
      *
-     * @param key of the potion type
-     * @return an internal potion data
+     * @param key 药水类型的键
+     * @return 内部药水数据
+     * <p>
+     * 原文：Do not use, method will get removed, and the plugin won't run
      */
     @ApiStatus.Internal
     @Deprecated(since = "1.20.2", forRemoval = true)
@@ -118,10 +133,12 @@ public interface UnsafeValues {
     DamageEffect getDamageEffect(@NotNull String key);
 
     /**
-     * Create a new {@link DamageSource.Builder}.
+     * 创建一个新的 {@link DamageSource.Builder}。
      *
-     * @param damageType the {@link DamageType} to use
-     * @return a {@link DamageSource.Builder}
+     * @param damageType 要使用的 {@link DamageType}
+     * @return 一个 {@link DamageSource.Builder}
+     * <p>
+     * 原文：Create a new {@link DamageSource.Builder}.
      */
     @ApiStatus.Internal
     @NotNull
