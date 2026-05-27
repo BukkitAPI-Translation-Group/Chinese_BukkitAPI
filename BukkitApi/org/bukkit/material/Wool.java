d229ab78f0c85ac0fe954a8d486aced1d04c98b7
handy <handyplus@163.com>
2026-05-27 18:58:46 +0800

    翻译 BukkitApi/org/bukkit/material/Wool.java

--- BukkitApi/org/bukkit/material/Wool.java
+++ BukkitApi/org/bukkit/material/Wool.java
@@ -5,8 +5,8 @@ import org.bukkit.Material;
 
 /**
  * 代表羊毛/地毯方块.
- * @deprecated all usage of MaterialData is deprecated and subject to removal.
- * Use {@link org.bukkit.block.data.BlockData}.
+ * @deprecated 所有 MaterialData 的用法均已弃用并将在未来移用.
+ * 请使用 {@link org.bukkit.block.data.BlockData}.
  */
 @Deprecated(since = "1.14.1")
 public class Wool extends MaterialData implements Colorable {
@@ -24,8 +24,8 @@ public class Wool extends MaterialData implements Colorable {
     }
 
     /**
-     * @param type the type
-     * @param data the raw data value
+     * @param type 物品/方块类型
+     * @param data 原始数据值
      * @deprecated 不安全的参数
      */
     @Deprecated(since = "1.6.2")
