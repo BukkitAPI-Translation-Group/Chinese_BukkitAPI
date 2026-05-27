ec959b9d48a9255753faeac1bebb478f00128481
handy <handyplus@163.com>
2026-05-27 19:05:53 +0800

    翻译 BukkitApi/org/bukkit/entity/Vindicator.java

--- BukkitApi/org/bukkit/entity/Vindicator.java
+++ BukkitApi/org/bukkit/entity/Vindicator.java
@@ -6,18 +6,24 @@ package org.bukkit.entity;
 public interface Vindicator extends Illager {
 
     /**
-     * Returns whether a vindicator is in "Johnny" mode.
-     *
+     * 返回卫道士是否处于"强尼"模式.
+     * <p>
+     * 当此模式激活时，卫道士将对所有生物敌对.
+     * <p>
+     * 原文：Returns whether a vindicator is in "Johnny" mode.
+     * <p>
      * When this mode is active, vindicators will be hostile to all mobs.
      *
-     * @return true if johnny
+     * @return 如果处于强尼模式则返回 true
      */
     boolean isJohnny();
 
     /**
-     * Sets the Johnny state of a vindicator.
+     * 设置卫道士的强尼状态.
+     * <p>
+     * 原文：Sets the Johnny state of a vindicator.
      *
-     * @param johnny new johnny state
+     * @param johnny 新的强尼状态
      */
     void setJohnny(boolean johnny);
 }
