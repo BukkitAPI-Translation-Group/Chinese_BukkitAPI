ec651475d2b11aa7c7eb5fbe70e929e60440dc6e
handy <handyplus@163.com>
2026-05-27 19:00:02 +0800

    翻译 BukkitApi/org/bukkit/material/Ladder.java

--- BukkitApi/org/bukkit/material/Ladder.java
+++ BukkitApi/org/bukkit/material/Ladder.java
@@ -6,8 +6,8 @@ import org.bukkit.block.BlockFace;
 /**
  * 代表梯子数据。
  *
- * @deprecated all usage of MaterialData is deprecated and subject to removal.
- * Use {@link org.bukkit.block.data.BlockData}.
+ * @deprecated 所有 MaterialData 的用法均已弃用且将被移除.
+ * 请使用 {@link org.bukkit.block.data.BlockData}.
  */
 @Deprecated(since = "1.14.1")
 public class Ladder extends SimpleAttachableMaterialData {
@@ -20,9 +20,9 @@ public class Ladder extends SimpleAttachableMaterialData {
     }
 
     /**
-     * @param type the type
-     * @param data the raw data value
-     * @deprecated Magic value
+     * @param type 物品类型
+     * @param data 原始数据值
+     * @deprecated 魔法值
      */
     @Deprecated(since = "1.6.2")
     public Ladder(final Material type, final byte data) {
