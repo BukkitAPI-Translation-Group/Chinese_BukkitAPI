ba4d3495acd1210c668bf9cd500c3fbc00cd6679
handy <handyplus@163.com>
2026-05-27 19:01:59 +0800

    翻译 BukkitApi/org/bukkit/entity/TropicalFish.java

--- BukkitApi/org/bukkit/entity/TropicalFish.java
+++ BukkitApi/org/bukkit/entity/TropicalFish.java
@@ -9,54 +9,72 @@ import org.jetbrains.annotations.NotNull;
 public interface TropicalFish extends Fish {
 
     /**
+     * 获取热带鱼的花纹颜色.
+     * <p>
+     * 原文：
      * Gets the color of the fish's pattern.
      *
-     * @return pattern color
+     * @return 花纹颜色
      */
     @NotNull
     DyeColor getPatternColor();
 
     /**
+     * 设置热带鱼的花纹颜色.
+     * <p>
+     * 原文：
      * Sets the color of the fish's pattern
      *
-     * @param color pattern color
+     * @param color 花纹颜色
      */
     void setPatternColor(@NotNull DyeColor color);
 
     /**
+     * 获取热带鱼的身体颜色.
+     * <p>
+     * 原文：
      * Gets the color of the fish's body.
      *
-     * @return pattern color
+     * @return 身体颜色
      */
     @NotNull
     DyeColor getBodyColor();
 
     /**
+     * 设置热带鱼的身体颜色.
+     * <p>
+     * 原文：
      * Sets the color of the fish's body
      *
-     * @param color body color
+     * @param color 身体颜色
      */
     void setBodyColor(@NotNull DyeColor color);
 
     /**
+     * 获取热带鱼的花纹.
+     * <p>
+     * 原文：
      * Gets the fish's pattern.
      *
-     * @return pattern
+     * @return 花纹
      */
     @NotNull
     Pattern getPattern();
 
     /**
+     * 设置热带鱼的花纹.
+     * <p>
+     * 原文：
      * Sets the fish's pattern
      *
-     * @param pattern new pattern
+     * @param pattern 新花纹
      */
     void setPattern(@NotNull Pattern pattern);
 
     /**
-     * Enumeration of all different fish patterns. Refer to the
+     * 代表所有不同热带鱼花纹的枚举。请参阅
      * <a href="https://minecraft.wiki/w/Fish">Minecraft Wiki</a>
-     * for pictures.
+     * 获取图片。
      */
     public static enum Pattern {
 
